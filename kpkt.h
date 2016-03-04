e077f12fd23e842843de16957a8d7e1110a614f3
Luke Shingles <luke.shingles@gmail.com>
2016-03-04 17:52:07 +0000

    inline kpkt routines, spacing
    Former-commit-id: 3b76492d0cccc4159706ad4b5715e78dbaa16181

--- kpkt.h
+++ kpkt.h
@@ -5,9 +5,27 @@ void calculate_kpkt_rates(int modelgridindex);
 void calculate_kpkt_rates_ion(int modelgridindex, int element, int ion, int low, double oldcoolingsum, int high);
 double do_kpkt_bb(PKT *pkt_ptr, double t1, double t2);
 double sample_planck(double T);
-double planck(double nu, double T);
+
+inline
+double planck(double nu, double T)
+/// returns intensity for frequency nu and temperature T according
+/// to the Planck distribution
+{
+  return TWOHOVERCLIGHTSQUARED * pow(nu,3) / (exp(HOVERKB*nu/T) - 1);
+}
+
 double do_kpkt(PKT *pkt_ptr, double t1, double t2, int nts);
-int get_coolinglistoffset(int element, int ion);
-int get_ncoolingterms(int element, int ion);
+
+inline
+int get_coolinglistoffset(int element, int ion)
+{
+  return elements[element].ions[ion].coolingoffset;
+}
+
+inline
+int get_ncoolingterms(int element, int ion)
+{
+  return elements[element].ions[ion].ncoolingterms;
+}
 
 #endif //KPKT_H
