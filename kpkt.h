71057a77c7544ea43e2b3ce1264e9ee43775a6bf
Luke Shingles <luke.shingles@gmail.com>
2016-03-08 01:39:52 +0000

    revert kpkt and now produces decent Fe spectrum again!

--- kpkt.h
+++ kpkt.h
@@ -5,27 +5,9 @@ void calculate_kpkt_rates(int modelgridindex);
 void calculate_kpkt_rates_ion(int modelgridindex, int element, int ion, int low, double oldcoolingsum, int high);
 double do_kpkt_bb(PKT *pkt_ptr, double t1, double t2);
 double sample_planck(double T);
-
-static inline
-double planck(double nu, double T)
-/// returns intensity for frequency nu and temperature T according
-/// to the Planck distribution
-{
-  return TWOHOVERCLIGHTSQUARED * pow(nu,3) / (exp(HOVERKB*nu/T) - 1);
-}
-
+double planck(double nu, double T);
 double do_kpkt(PKT *pkt_ptr, double t1, double t2, int nts);
-
-static inline
-int get_coolinglistoffset(int element, int ion)
-{
-  return elements[element].ions[ion].coolingoffset;
-}
-
-static inline
-int get_ncoolingterms(int element, int ion)
-{
-  return elements[element].ions[ion].ncoolingterms;
-}
+int get_coolinglistoffset(int element, int ion);
+int get_ncoolingterms(int element, int ion);
 
 #endif //KPKT_H
