e5802502958eec5051c5e28241d64b90bdcf4bab
Luke Shingles <luke.shingles@gmail.com>
2016-05-20 15:37:12 +0100

    Change loop structure of packet_prop (now slightly faster)

--- packet_prop.c
+++ packet_prop.c
@@ -8,7 +8,6 @@
 /* Master routine for moving packets around. When it called,
    it is given the time at start of inverval and at end - when it finishes,
    everything the packet does during this time should be sorted out. */
-
 int packet_prop(PKT *pkt_ptr, double t1, double t2, int nts)
 {
   double t_current = t1;
@@ -16,65 +15,49 @@ int packet_prop(PKT *pkt_ptr, double t1, double t2, int nts)
   /* 0 the scatter counter for the packet. */
   pkt_ptr->scat_count = 0;
 
-  int end_packet = 0;  //means "keep working"
-  while (end_packet == 0)
+  while (t_current >= 0)
   {
-    double t_change_type;
     /* Start by sorting out what sort of packet it is.*/
     //printout("start of packet_prop loop %d\n", pkt_ptr->type );
-    int pkt_prev_type = pkt_ptr->type; // avoid dereferencing multiple times
+    int pkt_type = pkt_ptr->type; // avoid dereferencing multiple times
 
-    if (pkt_prev_type == TYPE_GAMMA)
+    if (pkt_type == TYPE_GAMMA)
     {
       /*It's a gamma-ray packet.*/
       /* Call do_gamma. */
       //printout("gamma propagation\n");
-      t_change_type = do_gamma(pkt_ptr, t_current, t2);
+      t_current = do_gamma(pkt_ptr, t_current, t2);
 	    /* This returns a flag if the packet gets to t2 without
       changing to something else. If the packet does change it
       returns the time of change and sets everything for the
       new packet.*/
-      if (t_change_type < 0)
-      {
-        end_packet = 1;
-      }
-      else
+      if (t_current >= 0)
       {
         #ifdef _OPENMP
           #pragma omp atomic
         #endif
         time_step[nts].gamma_dep += pkt_ptr->e_cmf; ///***??
-        t_current = t_change_type;
       }
     }
-    else if (pkt_prev_type == TYPE_RPKT)
+    else if (pkt_type == TYPE_RPKT)
     {
       /*It's an r-packet. */
       //printout("r-pkt propagation\n");
-      t_change_type = do_rpkt(pkt_ptr, t_current, t2);
+      t_current = do_rpkt(pkt_ptr, t_current, t2);
 //       if (modelgrid[cell[pkt_ptr->where].modelgridindex].thick == 1)
 //         t_change_type = do_rpkt_thickcell( pkt_ptr, t_current, t2);
 //       else
 //         t_change_type = do_rpkt( pkt_ptr, t_current, t2);
 
-      if (pkt_prev_type == TYPE_ESCAPE)
+      if (pkt_ptr->type == TYPE_ESCAPE)
       {
         #ifdef _OPENMP
           #pragma omp atomic
         #endif
         time_step[nts].cmf_lum += pkt_ptr->e_cmf;
       }
-
-      if (t_change_type < 0)
-      {
-        end_packet = 1;
-      }
-      else
-      {
-        t_current = t_change_type;
-      }
     }
-    else if (pkt_prev_type == TYPE_EMINUS)
+    else if (pkt_type == TYPE_EMINUS)
     {
       /*It's an electron - convert to k-packet*/
       //printout("e-minus propagation\n");
@@ -87,51 +70,30 @@ int packet_prop(PKT *pkt_ptr, double t1, double t2, int nts)
       //if (tid == 0) k_stat_from_eminus += 1;
       k_stat_from_eminus += 1;
     }
-    else if (pkt_prev_type == TYPE_KPKT || pkt_prev_type == TYPE_PRE_KPKT || pkt_prev_type == TYPE_GAMMA_KPKT)
+    else if (pkt_type == TYPE_KPKT || pkt_type == TYPE_PRE_KPKT || pkt_type == TYPE_GAMMA_KPKT)
     {
       /*It's a k-packet - convert to r-packet (low freq).*/
       //printout("k-packet propagation\n");
 
       //t_change_type = do_kpkt(pkt_ptr, t_current, t2);
 
-      if (pkt_prev_type == TYPE_PRE_KPKT || modelgrid[cell[pkt_ptr->where].modelgridindex].thick == 1)
-        t_change_type = do_kpkt_bb(pkt_ptr, t_current, t2);
-      else if (pkt_prev_type == TYPE_KPKT)
-        t_change_type = do_kpkt(pkt_ptr, t_current, t2, nts);
+      if (pkt_type == TYPE_PRE_KPKT || modelgrid[cell[pkt_ptr->where].modelgridindex].thick == 1)
+        t_current = do_kpkt_bb(pkt_ptr, t_current, t2);
+      else if (pkt_type == TYPE_KPKT)
+        t_current = do_kpkt(pkt_ptr, t_current, t2, nts);
       else
       {
         printout("kpkt not of type TYPE_KPKT or TYPE_PRE_KPKT\n");
         abort();
         //t_change_type = do_kpkt_ffonly(pkt_ptr, t_current, t2);
       }
-
-      if (t_change_type < 0)
-      {
-        end_packet = 1;
-      }
-      else
-      {
-        t_current = t_change_type;
-      }
     }
-    else if (pkt_prev_type == TYPE_MA)
+    else if (pkt_type == TYPE_MA)
     {
       /*It's an active macroatom - apply transition probabilities*/
       //printout("MA-packet handling\n");
 
-      t_change_type = do_ma(pkt_ptr, t_current, t2, nts);
-
-      if (t_change_type < 0)
-      {
-        /// shouldn't be possible in this case
-        end_packet = 1;
-      }
-      else
-      {
-        /// should be the same as original t_current, as MA processes
-        /// happen instantaneously so far
-        t_current = t_change_type;
-      }
+      t_current = do_ma(pkt_ptr, t_current, t2, nts);
     }
     else
     {
