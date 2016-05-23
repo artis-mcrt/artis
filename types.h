624973e81bd130d28c1e01a04fad9318d29b271b
Luke Shingles <luke.shingles@gmail.com>
2016-05-23 10:17:29 +0100

    Repack structs for smaller packets

--- types.h
+++ types.h
@@ -134,58 +134,56 @@ struct packet
 {
   int where;      /// The grid cell that the packet is in.
   int type;       /// Identifies the type of packet (k-, r-, etc.)
+  int last_cross; /// To avoid rounding errors on cell crossing.
+  int interactions;/// debug: number of interactions the packet undergone
   double pos[3];  /// Position of the packet (x,y,z).
   double dir[3];  /// Direction of propagation. (x,y,z). Always a unit vector.
-  int last_cross; /// To avoid rounding errors on cell crossing.
   double tdecay;  /// Time at which pellet decays.
   double e_cmf;   /// The energy the packet carries in the co-moving frame.
   double e_rf;    /// The energy the packet carries in the rest frame.
   double nu_cmf;  /// The frequency in the co-moving frame.
   double nu_rf;   /// The frequency in the rest frame.
 
-  int escape_type; /// Flag to tell us in which form it escaped from the grid.
-  int escape_time; /// Time at which is passes out of the grid.
-                   /// Pos, dir, where, e_rf, nu_rf should all remain set at the exit point.
-
   int scat_count;  /// WHAT'S THAT???
 
   int next_trans;  /// This keeps track of the next possible line interaction of a rpkt by storing
                    /// its linelist index (to overcome numerical problems in propagating the rpkts).
-
-  int interactions;/// debug: number of interactions the packet undergone
   int last_event;  /// debug: stores information about the packets history
 
   int emissiontype;   /// records how the packet was emitted if it is a r-pkt
-  double em_pos[3]; /// Position of the packet (x,y,z).
   int absorptiontype;     /// records linelistindex of the last absorption
                           /// negative values give ff-abs (-1), bf-abs (-2), compton scattering of gammas (-3),
                           /// photoelectric effect of gammas (-4), pair production of gammas (-5)
                           /// decaying pellets of the 52Fe chain (-6) and pellets which decayed before the
                           /// onset of the simulation (-7)
   double absorptionfreq;  /// records nu_cmf of packet at last absorption
-  int nscatterings;   /// records number of electron scatterings a r-pkt undergone since it was emitted
-  int em_time;
   double absorptiondir[3]; /// Direction of propagation (x,y,z) when a packet was last absorbed in a line. Always a unit vector.
   //short timestep;
   double stokes[3]; //I, Q and U Stokes parameters
   double stokes_qu[2]; //Q and U Stokes parameters
   double pol_dir[3]; //unit vector which defines the coordinate system against which Q and U are measured; should always be perpendicular to dir
+  double em_pos[3]; /// Position of the packet (x,y,z).
+  int escape_type; /// Flag to tell us in which form it escaped from the grid.
+  int escape_time; /// Time at which is passes out of the grid.
+                   /// Pos, dir, where, e_rf, nu_rf should all remain set at the exit point.
   int number;     /// A unique number to identify which packet caused potential troubles.
+  int nscatterings;   /// records number of electron scatterings a r-pkt undergone since it was emitted
+  int em_time;
 };
 typedef struct packet PKT;
 
 typedef struct
 {
+  double nnlevel;           /// population number of the active level
+  double einstein;
+  float statweight;         /// statistical weight of the active level
   int element;              /// macro atom of type element (this is an element index)
   int ion;                  /// in ionstage ion (this is an ion index)
   int level;                /// and level=level (this is a level index)
-  double nnlevel;           /// population number of the active level
-  float statweight;         /// statistical weight of the active level
   int activatedfromlevel;   /// Helper variable for bb-activation of macro atoms due to a rpkt event
                             /// It holds information about the lower level of the bb-transition.
   int activatingline;       /// Linelistindex of the activating line for bb activated MAs, -99 else.
   int lastaction;           /// Holds information on last action performed by do_ma
-  double einstein;
 } mastate_t;
 //mastate_t mastate;
 
