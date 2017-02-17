d5a6b37810b8b33ad5cc7b6a592a91f7f6128276
Luke Shingles <luke.shingles@gmail.com>
2017-02-17 13:12:29 +0000

    Set MPKT to 200000 for 1008 core W7 on raijin

--- types.h
+++ types.h
@@ -11,7 +11,7 @@
 
 #define MGRID  125000 //125000 //1000000 //1000000//262144 //2100000 //125000 //1000000  /* Max number of grid cells.*/
 #define MMODELGRID 178 //125000 //12800 //12800 //125 //3200 //200 //200 //200 //8192 //125 //125000 //200 //125000 //8200 //200 //8200 //200 //125000
-#define MPKTS 2000000 //25000 //40000 //4000 //10000 //10000 //1250 //10000 //100000 //5000 //15625 //15625 /* Maximum number of energy packets in calculation. */
+#define MPKTS 200000 //25000 //40000 //4000 //10000 //10000 //1250 //10000 //100000 //5000 //15625 //15625 /* Maximum number of energy packets in calculation. */
 #define MELEMENTS 26 //26 //27 //9
 #define MIONS 5 //9
 #define MTHREADS 8    /// Max number of OpenMP threads
