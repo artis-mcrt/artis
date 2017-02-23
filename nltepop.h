fcf598fd629666e3cb576cc8fde5f231da3ee64f
Luke Shingles <luke.shingles@gmail.com>
2017-02-23 11:59:26 +0000

    Add save/resume for NLTE populations

--- nltepop.h
+++ nltepop.h
@@ -1,11 +1,15 @@
 #ifndef NLTEPOP_H
 #define NLTEPOP_H
 
+#include <stdio.h>
+
 void solve_nlte_pops_element(int element, int modelgridindex, int timestep);
 double solve_nlte_pops(int element, int ion, int modelgridindex, int timestep);
 double superlevel_boltzmann(int modelgridindex, int element, int ion, int level);
 void nltepop_write_to_file(int n, int timestep);
 void nltepop_open_file(int my_rank);
 void nltepop_close_file(int my_rank);
+void nltepop_write_restart_data(FILE *restart_file);
+void nltepop_read_restart_data(FILE *restart_file);
 
 #endif //NLTEPOP_H
