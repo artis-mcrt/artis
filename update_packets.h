ab7db1ba0dc68bddcd4063a6606fe01f35014b14
Luke Shingles <luke.shingles@gmail.com>
2017-03-05 23:04:08 +0000

    Fix regression in 08e7bca (still correct, but not bit exact?)

--- update_packets.h
+++ update_packets.h
@@ -5,7 +5,7 @@
 
 void update_packets(int nts);
 
-inline void update_cell(const int cellnumber)
+inline void update_cell(int cellnumber)
 ///=calculate_levelpops for non isothermal homogeneous grids
 ///
 {
