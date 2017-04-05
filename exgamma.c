8faf0ec5864f339d788d758a659839c0192cf68c
Luke Shingles <luke.shingles@gmail.com>
2017-04-05 10:49:50 +0100

    Change exgamma filename and remove unused code

--- exgamma.c
+++ exgamma.c
@@ -51,11 +51,6 @@ int main(int argc, char** argv)
   int p;
   int nnn, nn, n;
   int element;
-  #ifdef MPI_ON
-    double a, b;
-    int aa, bb;
-    int nblock, numtot, n_leftover;
-  #endif
   int nstart, nknown, ndo;
   double rho_tot, te_tot, tr_tot, w_tot, n_count;
   int position, nlp, ncl, nncl, mgi;
@@ -63,218 +58,201 @@ int main(int argc, char** argv)
   double T_e_max,T_e_min,T_e_step;
   double rho_max,rho_min,rho_step;
   char filename[100];
-  int mpi_nlte_buffer_size;
-  char *mpi_nlte_buffer;
   double nntot;
   int titer;
 
   double deltaV,deltat;
   int assoc_cells;
 
-//  int mpi_grid_buffer_size;
 
-  #ifdef MPI_ON
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
-  #else
-    my_rank = 0;
-    p = 1;
-  #endif
+  my_rank = 0;
+  p = 1;
 
   nprocs = p;              /// Global variable which holds the number of MPI processes
   rank_global = my_rank;   /// Global variable which holds the rank of the active MPI process
-  if (my_rank == 0)
+  tid = 0;
+  nthreads = 1;
+  sprintf(filename,"exgamma.txt");
+  if ((output_file = fopen(filename, "w")) == NULL)
   {
+    printf("Cannot open %s.\n",filename);
+    abort();
+  }
+  setvbuf(output_file, NULL, _IOLBF, 1);
 
-    tid = 0;
-    nthreads = 1;
-    sprintf(filename,"exgamma_%d-%d.txt",my_rank,tid);
-    if ((output_file = fopen(filename, "w")) == NULL)
+  printout("Begining do_exspec.\n");
+
+  /// Get input stuff
+  printout("time before input %d\n",time(NULL));
+  input (0);
+  printout("time after input %d\n",time(NULL));
+  nprocs = nprocs_exspec;
+
+  /// Read binary packet files and create ASCII packets files out of them
+  /*
+  npkts=MPKTS;
+  for (i = 0; i < nprocs; i++)
+  {
+    /// Read in the next bunch of packets to work on
+    sprintf(filename,"packets%d_%d.tmp",0,i);
+    printout("%s\n",filename);
+    if ((packets_file = fopen(filename, "rb")) == NULL)
+    //sprintf(filename,"packets%.2d_%.4d.out",0,i);
+    //if ((packets_file = fopen(filename, "r")) == NULL)
     {
-      printf("Cannot open %s.\n",filename);
+      printf("Cannot open packets file %s\n",filename);
       abort();
     }
-    setvbuf(output_file, NULL, _IOLBF, 1);
+    fread(&pkt[0], sizeof(PKT), npkts, packets_file);
+    //read_packets(packets_file);
+    /// Close the current file.
+    fclose(packets_file);
 
-    printout("Begining do_exspec.\n");
 
-    /// Get input stuff
-    printout("time before input %d\n",time(NULL));
-    input (0);
-    printout("time after input %d\n",time(NULL));
-    nprocs = nprocs_exspec;
+    /// Read in the next bunch of packets to work on
+    sprintf(filename,"packets%.2d_%.4d.out",0,i);
+    printout("%s\n",filename);
+    if ((packets_file = fopen(filename, "w")) == NULL)
+    {
+      printf("Cannot open packets file %s\n",filename);
+      abort();
+    }
+    write_packets(packets_file);
+    //read_packets(packets_file);
+    /// Close the current file.
+    fclose(packets_file);
+  }
+  abort();
+  */
 
-    /// Read binary packet files and create ASCII packets files out of them
-    /*
-    npkts=MPKTS;
+  /// Override some variables with values appropriate for gamma-ray spectra
+  do_emission_res = 0;         /// We don't record information on gamma packet last interactions, thus create no emission/absorption files.
+  nu_min_r = 0.05 * MEV / H;   /// Lower frequency boundary for gamma spectra
+  nu_max_r = 4 * MEV / H;      /// Upper frequency boundary for gamma spectra
+  for (outer_iteration = 0; outer_iteration < n_out_it; outer_iteration++)
+  {
+    /// Initialise the grid. Call routine that sets up the initial positions
+    /// and sizes of the grid cells.
+    //grid_init();
+    time_init();
+
+    if ((epkts = malloc((nprocs*npkts)*sizeof(EPKT))) == NULL)
+    {
+      printout("[fatal] input: not enough memory to initalise escaping packets data structure ... abort\n");
+      abort();
+    }
+
+    /// Loop over all packets in all the packets files of the simulation and check if
+    /// a packet made it out as a rpkt or not. Escaping r-packets are stored in the
+    /// epkts array, which is then used for the binning.
+    j=0;
     for (i = 0; i < nprocs; i++)
     {
       /// Read in the next bunch of packets to work on
-      sprintf(filename,"packets%d_%d.tmp",0,i);
-      printout("%s\n",filename);
-      if ((packets_file = fopen(filename, "rb")) == NULL)
-      //sprintf(filename,"packets%.2d_%.4d.out",0,i);
-      //if ((packets_file = fopen(filename, "r")) == NULL)
-      {
-        printf("Cannot open packets file %s\n",filename);
-        abort();
-      }
-      fread(&pkt[0], sizeof(PKT), npkts, packets_file);
-      //read_packets(packets_file);
-      /// Close the current file.
-      fclose(packets_file);
-
-
-      /// Read in the next bunch of packets to work on
+      //sprintf(filename,"packets%d_%d.tmp",0,i);
       sprintf(filename,"packets%.2d_%.4d.out",0,i);
-      printout("%s\n",filename);
-      if ((packets_file = fopen(filename, "w")) == NULL)
+      printout("%s, %d %d\n",filename,i,nprocs);
+      //if ((packets_file = fopen(filename, "rb")) == NULL)
+      if ((packets_file = fopen(filename, "r")) == NULL)
       {
         printf("Cannot open packets file %s\n",filename);
         abort();
       }
-      write_packets(packets_file);
-      //read_packets(packets_file);
-      /// Close the current file.
+      //fread(&pkt[0], sizeof(PKT), npkts, packets_file);
+      read_packets(packets_file);
       fclose(packets_file);
+
+      for (ii = 0; ii < npkts; ii++)
+      {
+        pkt_ptr = &pkt[ii];
+        if (pkt_ptr->type == TYPE_ESCAPE && pkt_ptr->escape_type == TYPE_GAMMA)
+        {
+          //printout("add packet %d\n",j);
+          /// We know that a packet escaped at "escape_time". However, we have
+          /// to allow for travel time. Use the formula in Leon's paper. The extra
+          /// distance to be travelled beyond the reference surface is ds = r_ref (1 - mu).
+          t_arrive = pkt_ptr->escape_time - (dot(pkt_ptr->pos, pkt_ptr->dir)/CLIGHT_PROP);
+          epkts[j].arrive_time = t_arrive;
+
+          /// Now do the cmf time.
+          t_arrive = pkt_ptr->escape_time * sqrt(1. - (vmax*vmax/CLIGHTSQUARED));
+          epkts[j].arrive_time_cmf = t_arrive;
+
+          epkts[j].dir[0] = pkt_ptr->dir[0];
+          epkts[j].dir[1] = pkt_ptr->dir[1];
+          epkts[j].dir[2] = pkt_ptr->dir[2];
+          epkts[j].nu_rf = pkt_ptr->nu_rf;
+          epkts[j].e_rf = pkt_ptr->e_rf;
+          epkts[j].e_cmf = pkt_ptr->e_cmf;
+          epkts[j].emissiontype = pkt_ptr->emissiontype;
+          epkts[j].absorptionfreq = pkt_ptr->absorptionfreq;
+          epkts[j].absorptiontype = pkt_ptr->absorptiontype;
+          j++;
+        }
+      }
     }
-    abort();
-    */
+    nepkts = j;
 
-    /// Override some variables with values appropriate for gamma-ray spectra
-    do_emission_res = 0;         /// We don't record information on gamma packet last interactions, thus create no emission/absorption files.
-    nu_min_r = 0.05 * MEV / H;   /// Lower frequency boundary for gamma spectra
-    nu_max_r = 4 * MEV / H;      /// Upper frequency boundary for gamma spectra
-    for (outer_iteration = 0; outer_iteration < n_out_it; outer_iteration++)
+
+    /// Extract angle-averaged spectra and light curves
+    if ((lc_file = fopen("gamma_light_curve.out", "w")) == NULL)
     {
-      /// Initialise the grid. Call routine that sets up the initial positions
-      /// and sizes of the grid cells.
-      //grid_init();
-      time_init();
+      printout("Cannot open gamma_light_curve.out\n");
+      abort();
+    }
+    if ((spec_file = fopen("gamma_spec.out", "w")) == NULL)
+    {
+      printout("Cannot open spec.out\n");
+      abort();
+    }
 
-      if ((epkts = malloc((nprocs*npkts)*sizeof(EPKT))) == NULL)
+    gather_spectrum(-1);
+    gather_light_curve();
+    write_spectrum(spec_file,NULL,NULL);
+    write_light_curve(lc_file,-1);
+
+    fclose(lc_file);
+    fclose(spec_file);
+
+    printout("finished angle-averaged stuff\n");
+
+    /// Extract LOS dependent spectra and light curves
+    if (model_type != RHO_1D_READ)
+    {
+      if ((lc_file = fopen("gamma_light_curve_res.out", "w")) == NULL)
       {
-        printout("[fatal] input: not enough memory to initalise escaping packets data structure ... abort\n");
+        printout("Cannot open gamma_light_curve_res.out\n");
         abort();
       }
-
-      /// Loop over all packets in all the packets files of the simulation and check if
-      /// a packet made it out as a rpkt or not. Escaping r-packets are stored in the
-      /// epkts array, which is then used for the binning.
-      j=0;
-      for (i = 0; i < nprocs; i++)
+      if ((spec_file = fopen("gamma_spec_res.out", "w")) == NULL)
       {
-        /// Read in the next bunch of packets to work on
-        //sprintf(filename,"packets%d_%d.tmp",0,i);
-        sprintf(filename,"packets%.2d_%.4d.out",0,i);
-        printout("%s, %d %d\n",filename,i,nprocs);
-        //if ((packets_file = fopen(filename, "rb")) == NULL)
-        if ((packets_file = fopen(filename, "r")) == NULL)
-        {
-          printf("Cannot open packets file %s\n",filename);
-          abort();
-        }
-        //fread(&pkt[0], sizeof(PKT), npkts, packets_file);
-        read_packets(packets_file);
-        fclose(packets_file);
-
-        for (ii = 0; ii < npkts; ii++)
-        {
-          pkt_ptr = &pkt[ii];
-          if (pkt_ptr->type == TYPE_ESCAPE && pkt_ptr->escape_type == TYPE_GAMMA)
-          {
-            //printout("add packet %d\n",j);
-            /// We know that a packet escaped at "escape_time". However, we have
-            /// to allow for travel time. Use the formula in Leon's paper. The extra
-            /// distance to be travelled beyond the reference surface is ds = r_ref (1 - mu).
-            t_arrive = pkt_ptr->escape_time - (dot(pkt_ptr->pos, pkt_ptr->dir)/CLIGHT_PROP);
-            epkts[j].arrive_time = t_arrive;
-
-            /// Now do the cmf time.
-            t_arrive = pkt_ptr->escape_time * sqrt(1. - (vmax*vmax/CLIGHTSQUARED));
-            epkts[j].arrive_time_cmf = t_arrive;
-
-            epkts[j].dir[0] = pkt_ptr->dir[0];
-            epkts[j].dir[1] = pkt_ptr->dir[1];
-            epkts[j].dir[2] = pkt_ptr->dir[2];
-            epkts[j].nu_rf = pkt_ptr->nu_rf;
-            epkts[j].e_rf = pkt_ptr->e_rf;
-            epkts[j].e_cmf = pkt_ptr->e_cmf;
-            epkts[j].emissiontype = pkt_ptr->emissiontype;
-            epkts[j].absorptionfreq = pkt_ptr->absorptionfreq;
-            epkts[j].absorptiontype = pkt_ptr->absorptiontype;
-            j++;
-          }
-        }
-      }
-      nepkts = j;
-
-
-      /// Extract angle-averaged spectra and light curves
-      if ((lc_file = fopen("gamma_light_curve.out", "w")) == NULL)
-      {
-        printout("Cannot open gamma_light_curve.out\n");
+        printout("Cannot open gamma_spec_res.out\n");
         abort();
       }
-      if ((spec_file = fopen("gamma_spec.out", "w")) == NULL)
+      for (i = 0; i < MABINS; i++)
       {
-        printout("Cannot open spec.out\n");
-        abort();
+        gather_spectrum_res(i);
+        gather_light_curve_res(i);
+
+        write_spectrum(spec_file,NULL,NULL);
+        write_light_curve(lc_file,i);
+
+        printout("Did %d of %d angle bins.\n",i+1,MABINS);
       }
-
-      gather_spectrum(-1);
-      gather_light_curve();
-      write_spectrum(spec_file,NULL,NULL);
-      write_light_curve(lc_file,-1);
-
       fclose(lc_file);
       fclose(spec_file);
-
-      printout("finished angle-averaged stuff\n");
-
-      /// Extract LOS dependent spectra and light curves
-      if (model_type != RHO_1D_READ)
-      {
-        if ((lc_file = fopen("gamma_light_curve_res.out", "w")) == NULL)
-        {
-          printout("Cannot open gamma_light_curve_res.out\n");
-          abort();
-        }
-        if ((spec_file = fopen("gamma_spec_res.out", "w")) == NULL)
-        {
-          printout("Cannot open gamma_spec_res.out\n");
-          abort();
-        }
-        for (i = 0; i < MABINS; i++)
-        {
-          gather_spectrum_res(i);
-          gather_light_curve_res(i);
-
-          write_spectrum(spec_file,NULL,NULL);
-          write_light_curve(lc_file,i);
-
-          printout("Did %d of %d angle bins.\n",i+1,MABINS);
-        }
-        fclose(lc_file);
-        fclose(spec_file);
-      }
     }
-
-    //fclose(ldist_file);
-    //fclose(output_file);
-
-    /* Spec syn. */
-    //grid_init();
-    //syn_gamma();
-
-    printout("simulation finished at %d\n",time(NULL));
-    fclose(output_file);
   }
 
-  #ifdef MPI_ON
-    MPI_Finalize();
-  #endif
+  //fclose(ldist_file);
+  //fclose(output_file);
+
+  /* Spec syn. */
+  //grid_init();
+  //syn_gamma();
+
+  printout("simulation finished at %d\n",time(NULL));
+  fclose(output_file);
 
   return 0;
 }
