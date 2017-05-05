17f500ec7f1a47ba334196b28c05cd54472aae96
Luke Shingles <luke.shingles@gmail.com>
2017-05-05 11:49:40 +0100

    Fix previous commit

--- macroatom.c
+++ macroatom.c
@@ -970,7 +970,7 @@ void macroatom_open_file(const int my_rank)
     abort();
   }
   fprintf(macroatom_file, "%8s %14s %2s %12s %12s %9s %9s %9s %11s %11s %11s %11s %9s\n",
-          "timestep", "modelgridindex", "Z", "ionstage_in", "ionstage_out", "level_in", "level_out", "activline"
+          "timestep", "modelgridindex", "Z", "ionstage_in", "ionstage_out", "level_in", "level_out", "activline",
           "nu_cmf_in", "nu_cmf_out", "nu_rf_in", "nu_rf_out", "jumps");
 }
 
