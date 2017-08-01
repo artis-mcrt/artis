f058d54f2c46af6c5d4bd9dff26f9e0bf9d6026b
Luke Shingles <luke.shingles@gmail.com>
2017-08-01 10:24:54 +0100

    Add packet property originated_from_positron

--- types.h
+++ types.h
@@ -195,6 +195,7 @@ typedef struct packet
                    /// Pos, dir, where, e_rf, nu_rf should all remain set at the exit point.
   int scat_count;  /// WHAT'S THAT???
   int number;     /// A unique number to identify which packet caused potential troubles.
+  bool originated_from_positron; // first-non-pellet packet type was positron
 } PKT;
 
 enum ma_action {
