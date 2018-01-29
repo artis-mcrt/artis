655d2dd1860fc8895f9bc4f102ac1fcbd002d252
Luke Shingles <luke.shingles@gmail.com>
2018-01-29 14:43:13 +0000

    Update movefiles.sh and add exspec.sh

---
+++ scripts/exspec.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#PBS -P fm5
+#PBS -q express
+#PBS -l walltime=1:00:00
+#PBS -l mem=4GB
+#PBS -l ncpus=1
+#PBS -l wd
+
+# ncpus must be a factor of the cores per node (16)
+# mem is total memory (all cores combined)
+# on raijin, max 128GB/node = 8GB per core and MPI task (2% of nodes)
+# max 64GB/node = 4GB per core (31% of nodes)
+# max 32GB/node = 2GB per core (66% of nodes)
+# normal queue walltime limits
+# 48 hrs for 1-255 cores
+# 24 hrs for 256-511 cores
+# 10 hrs for 512-1023 cores
+# 5 hours for 1024-56960 cores
+
+ulimit -l 2097152
+
+./exspec
+
+
+
+
+
+
+
