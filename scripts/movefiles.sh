65908a64f33f58db1d73d91304af1369e25980aa
Luke Shingles <luke.shingles@gmail.com>
2018-03-30 16:27:26 +0100

    Don't move linestat.out

--- scripts/movefiles.sh
+++ scripts/movefiles.sh
@@ -15,7 +15,6 @@ fi
 
 #cp gridsave.dat $1
 mv estimators* $1
-mv linestat.out $1
 # move the files below if the exist, but don't cause an error if they don't exist!
 find . -maxdepth 1 -name 'macroatom*' -type f -print0 | xargs -0r mv -t $1
 find . -maxdepth 1 -name 'nonthermal*' -type f -print0 | xargs -0r mv -t $1
