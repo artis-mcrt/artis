79601396f6eefb5920bd101a3acf40acd59b0e93
Luke Shingles <luke.shingles@gmail.com>
2018-08-05 14:31:00 +0100

    Input vectors as const double vec[3] for consistency

--- vectors.h
+++ vectors.h
@@ -3,9 +3,9 @@
 
 #include <math.h>
 
-void angle_ab(const double *dir1, const double *vel, double *dir2);
-double doppler(const double *dir1, const double *vel);
-void scatter_dir(const double *dir_in, double cos_theta, double *dir_out);
+void angle_ab(const double dir1[3], const double vel[3], double dir2[3]);
+double doppler(const double dir1[3], const double vel[3]);
+void scatter_dir(const double dir_in[3], double cos_theta, double dir_out[3]);
 
 inline double vec_len(const double x[3])
 // Routine for getting the magnitude of a vector.
@@ -25,14 +25,14 @@ inline void vec_norm(const double vec_in[3], double vec_out[3])
 }
 
 
-inline double dot(const double *const restrict x, const double *const restrict y)
+inline double dot(const double x[3], const double y[3])
 // Routine for taking dot product.
 {
   return (x[0] * y[0]) + (x[1] * y[1]) + (x[2] * y[2]);
 }
 
 
-inline void get_velocity(const double *const restrict x, double *restrict y, const double t)
+inline void get_velocity(const double x[3], double y[3], const double t)
 // Routine for getting velocity vector of the flow at a position with homologous expansion.
 {
   y[0] = x[0] / t;
@@ -43,15 +43,9 @@ inline void get_velocity(const double *const restrict x, double *restrict y, con
 
 inline void cross_prod(const double vec1[3], const double vec2[3], double vecout[3])
 {
-  const double vec1x = vec1[0];
-  const double vec1y = vec1[1];
-  const double vec1z = vec1[2];
-  const double vec2x = vec2[0];
-  const double vec2y = vec2[1];
-  const double vec2z = vec2[2];
-  vecout[0] = (vec1y * vec2z) - (vec2y * vec1z);
-  vecout[1] = (vec1z * vec2x) - (vec2z  *vec1x);
-  vecout[2] = (vec1x * vec2y) - (vec2x * vec1y);
+  vecout[0] = (vec1[1] * vec2[2]) - (vec2[1] * vec1[2]);
+  vecout[1] = (vec1[2] * vec2[0]) - (vec2[2] * vec1[0]);
+  vecout[2] = (vec1[0] * vec2[1]) - (vec2[0] * vec1[1]);
 }
 
 #endif //VECTORS_H
