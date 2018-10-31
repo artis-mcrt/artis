6cd2793c220a82b0f535352058c11e6a7eadf1d4
Luke Shingles <luke.shingles@gmail.com>
2018-10-31 13:40:43 +0000

    Update scripts/exspec-gzip-dirac.sh

--- scripts/exspec-gzip-dirac.sh
+++ scripts/exspec-gzip-dirac.sh
@@ -5,6 +5,11 @@
 #PBS -l nodes=1:ppn=1
 
 cd $PBS_O_WORKDIR
+
+gunzip -v phixsdata.txt.gz transitiondata.txt.gz ratecoeff.dat.gz packets*.out.gz
+
 ./exspec
 
 gzip -v packets*.out
+mkdir packets
+mv packets*.out.gz packets/
