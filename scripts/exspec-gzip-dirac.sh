cdb3a7caeea7c1254bdadae527f243e0d2c9fc6c
Luke Shingles <luke.shingles@gmail.com>
2018-11-08 16:45:03 +0000

    Update jobscripts

--- scripts/exspec-gzip-dirac.sh
+++ scripts/exspec-gzip-dirac.sh
@@ -4,11 +4,17 @@
 #PBS -l walltime=12:00:00
 #PBS -l nodes=1:ppn=1
 
+module load gsl/intel/2.4
+module load intel/compilers/18.0.3
+module load intel/mpi/18.0.3
+
 cd $PBS_O_WORKDIR
 
 gunzip -v phixsdata.txt.gz transitiondata.txt.gz ratecoeff.dat.gz packets*.out.gz
 
 ./exspec
+
+gzip -v emission*.out absorption.out
 
 gzip -v packets*.out
 mkdir packets
