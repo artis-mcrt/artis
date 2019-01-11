0a45b3e342d12bf6710af959d37e0d8ea2ec4731
Luke Shingles <luke.shingles@gmail.com>
2019-01-11 14:54:24 +0000

    Switch from gzip to lzma (.xz)

--- scripts/exspec-before.sh
+++ scripts/exspec-before.sh
@@ -1,6 +1,9 @@
+# files not in the artis folder, so move them
 if [! -f packets00_0000.out* ]; then
   mv packets/packets*.out* .
 fi
 
-gunzip packets*.out.gz || true
-gunzip -v phixsdata_v2.txt.gz transitiondata.txt.gz ratecoeff.dat.gz || true
+gzip -d packets*.out.gz || true
+xz -d -T 0 packets*.out.xz || true
+gzip -d -v phixsdata_v2.txt.gz transitiondata.txt.gz ratecoeff.dat.gz || true
+xz -d -v -T 0 phixsdata_v2.txt.xz transitiondata.txt.xz ratecoeff.dat.xz || true
