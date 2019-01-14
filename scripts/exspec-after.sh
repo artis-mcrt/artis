3f0313796c600553ff34f9a56a4b333e5ca34206
Luke Shingles <luke.shingles@gmail.com>
2019-01-14 17:32:44 +0000

    Update exspec-after.sh

--- scripts/exspec-after.sh
+++ scripts/exspec-after.sh
@@ -3,5 +3,5 @@ if [ -f spec.out ]; then
   xz -v phixsdata_v2.txt transitiondata.txt ratecoeff.dat linestat.out || true
   mkdir packets || true
   mv packets*.out* packets/
-  xz -v -T 0 packets*.out packets/packets*.out || true
+  xz -v -2 -T 0 packets*.out packets/packets*.out || true
 fi
