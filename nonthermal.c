312379e277cda7c7fe58cc5725a4f76daaa38e7c
ccollins <c_collins94@hotmail.co.uk>
2019-01-25 12:58:12 +0000

    Check nne is finite

--- nonthermal.c
+++ nonthermal.c
@@ -2667,6 +2667,7 @@ void nt_solve_spencerfano(const int modelgridindex, const int timestep, const in
   }
 
   const float nne = get_nne(modelgridindex); // electrons per cm^3
+  assert(isfinite(nne))
   const double nne_per_ion = nne / get_tot_nion(modelgridindex);
   const double nne_per_ion_last = nt_solution[modelgridindex].nneperion_when_solved;
   const double nne_per_ion_fracdiff = fabs((nne_per_ion_last / nne_per_ion) - 1.);
