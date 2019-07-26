99bbdac62853cd7041c5930e171f05b95dd0fd16
Luke Shingles <luke.shingles@gmail.com>
2019-07-26 12:11:54 +0100

    Use calculate_kappa_bf_fb_gammacontr instead of calculate_kappa_rpkt_cont

--- move.h
+++ move.h
@@ -1,7 +1,9 @@
 #ifndef MOVE_H
 #define MOVE_H
 
-void update_estimators(const PKT *restrict pkt_ptr, double distance);
+#include "types.h"
+
+void update_estimators(const PKT *restrict pkt_ptr, double distance, const double t_current);
 void move_pkt(PKT *restrict pkt_ptr, double distance, double time);
 
 #endif //MOVE_H
