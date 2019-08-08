87d9a6e317f356321c31895e33ba6ab36e7542e2
Luke Shingles <luke.shingles@gmail.com>
2019-08-08 17:05:25 +0200

    Update rpkt.c

--- rpkt.c
+++ rpkt.c
@@ -467,7 +467,6 @@ static void rpkt_event_continuum(PKT *restrict pkt_ptr, const double t_current, 
           #endif
           mastate[tid].element = element;
           mastate[tid].ion     = ion + 1;
-          // const int upper = 0; //TODO: this should come from phixsupperlevel;
           const int upper = get_phixsupperlevel(element, ion, level, phixstargetindex);
           mastate[tid].level   = upper;
           // mastate[tid].nnlevel = calculate_exclevelpop(modelgridindex,element,ion+1,upper);
@@ -538,9 +537,7 @@ static void rpkt_event_continuum(PKT *restrict pkt_ptr, const double t_current, 
         #endif
         mastate[tid].element = element;
         mastate[tid].ion     = ion;
-        // const int upper = 0; //TODO: this should come from phixsupperlevel;
         mastate[tid].level   = level;
-        // mastate[tid].nnlevel = calculate_exclevelpop(modelgridindex,element,ion+1,upper);
         mastate[tid].activatingline = -99;
         //if (element == 6) cell[pkt_ptr->where].photoion[ion] += pkt_ptr->e_cmf/pkt_ptr->nu_cmf/H;
         return;
@@ -693,6 +690,86 @@ static double closest_transition_empty(PKT *restrict pkt_ptr)
 
   /// return the transitions frequency
   return nu_trans;
+}
+
+
+static void update_estimators(const PKT *pkt_ptr, const double distance, const double t_current)
+/// Update the volume estimators J and nuJ
+/// This is done in another routine than move, as we sometimes move dummy
+/// packets which do not contribute to the radiation field.
+{
+  const int cellindex = pkt_ptr->where;
+  const int modelgridindex = cell[cellindex].modelgridindex;
+
+  /// Update only non-empty cells
+  if (modelgridindex != MMODELGRID)
+  {
+    const double distance_e_cmf = distance * pkt_ptr->e_cmf;
+    const double nu = pkt_ptr->nu_cmf;
+    //double bf = exp(-HOVERKB*nu/cell[modelgridindex].T_e);
+
+    radfield_update_estimators(modelgridindex, distance_e_cmf, nu, pkt_ptr, t_current);
+
+    #ifndef FORCE_LTE
+      ///ffheatingestimator does not depend on ion and element, so an array with gridsize is enough.
+      ///quick and dirty solution: store info in element=ion=0, and leave the others untouched (i.e. zero)
+      #ifdef _OPENMP
+        #pragma omp atomic
+      #endif
+      ffheatingestimator[modelgridindex] += distance_e_cmf * kappa_rpkt_cont[tid].ffheating;
+
+      #if (!NO_LUT_PHOTOION || !NO_LUT_BFHEATING)
+        #if (!NO_LUT_PHOTOION)
+        const double distance_e_cmf_over_nu = distance_e_cmf / nu;
+        #endif
+        for (int i = 0; i < nbfcontinua_ground; i++)
+        {
+          const double nu_edge = phixslist[tid].groundcont[i].nu_edge;
+          if (nu > nu_edge)
+          {
+            const int element = phixslist[tid].groundcont[i].element;
+            const int ion = phixslist[tid].groundcont[i].ion;
+            /// Cells with zero abundance for a specific element have zero contribution
+            /// (set in calculate_kappa_rpkt_cont and therefore do not contribute to
+            /// the estimators
+            if (get_abundance(modelgridindex, element) > 0)
+            {
+              const int ionestimindex = modelgridindex * nelements * maxion + element * maxion + ion;
+              #if (!NO_LUT_PHOTOION)
+                #ifdef _OPENMP
+                  #pragma omp atomic
+                #endif
+                gammaestimator[ionestimindex] += phixslist[tid].groundcont[i].gamma_contr * distance_e_cmf_over_nu;
+
+                #ifdef DEBUG_ON
+                if (!isfinite(gammaestimator[ionestimindex]))
+                {
+                  printout("[fatal] update_estimators: gamma estimator becomes non finite: level %d, gamma_contr %g, distance_e_cmf_over_nu %g\n", i, phixslist[tid].groundcont[i].gamma_contr, distance_e_cmf_over_nu);
+                  abort();
+                }
+                #endif
+              #endif
+              #if (!NO_LUT_BFHEATING)
+                #ifdef _OPENMP
+                  #pragma omp atomic
+                #endif
+                bfheatingestimator[ionestimindex] += phixslist[tid].groundcont[i].gamma_contr * distance_e_cmf * (1. - nu_edge/nu);
+                //bfheatingestimator[ionestimindex] += phixslist[tid].groundcont[i].bfheating_contr * distance_e_cmf * (1/nu_edge - 1/nu);
+              #endif
+            }
+          }
+          else
+            break; // because groundcont is sorted by nu_edge, nu < nu_edge for all remaining items
+        }
+      #endif
+
+    #endif
+
+    ///Heating estimators. These are only applicable for pure H. Other elements
+    ///need advanced treatment in thermalbalance calculation.
+    //cell[pkt_ptr->where].heating_ff += distance_e_cmf * kappa_rpkt_cont[tid].ffheating;
+    //cell[pkt_ptr->where].heating_bf += distance_e_cmf * kappa_rpkt_cont[tid].bfheating;
+  }
 }
 
 
