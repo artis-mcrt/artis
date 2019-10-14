58ab72aa17a3d04abbca57c1380eefc3d8a5f163
Luke Shingles <luke.shingles@gmail.com>
2019-10-14 16:41:35 +0100

    Switch xzip to gzip for packets files (much faster)

--- scripts/exspec-before.sh
+++ scripts/exspec-before.sh
@@ -1,9 +1,10 @@
-# files not in the artis folder, so move them
+# files outside the artis folder, so move them back
 if [ ! -f packets00_0000.out* ]; then
   mv packets/packets*.out* .
 fi
 
 gzip -d packets*.out.gz || true
 xz -d -T 0 packets*.out.xz || true
+
 gzip -d -v phixsdata_v2.txt.gz transitiondata.txt.gz ratecoeff.dat.gz || true
 xz -d -v -T 0 phixsdata_v2.txt.xz transitiondata.txt.xz ratecoeff.dat.xz || true
