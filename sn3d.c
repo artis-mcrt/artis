761b47d9badbf06873d83027148db4c21800cc92
Luke Shingles <luke.shingles@gmail.com>
2020-01-27 16:48:04 +0000

    Replace printout function with macro

--- sn3d.c
+++ sn3d.c
@@ -1296,7 +1296,6 @@ int main(int argc, char** argv)
 
 
 // printout should be used instead of printf throughout the whole code for output messages
-extern inline int printout(const char *format, ...);
 extern inline void gsl_error_handler_printout(const char *reason, const char *file, int line, int gsl_errno);
 extern inline FILE *fopen_required(const char *filename, const char *mode);
 
