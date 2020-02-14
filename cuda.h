23e4b1d6fa81b1f0523615422d8a6cbffea2d2c6
Luke Shingles <luke.shingles@gmail.com>
2020-02-14 17:48:09 +0000

    Replace non-working checkCudaErrors with helper_cuda.h include

--- cuda.h
+++ cuda.h
@@ -9,14 +9,32 @@
 
 #include <cuda_runtime.h>
 
+#include <helper_cuda.h>
 
-#define checkCudaErrors( call )        \
-{                                      \
-cudaError_t result = call;             \
-if (cudaSuccess != result)             \
-    std::cerr << "CUDA error " << result << " in " << __FILE__ << ":" << __LINE__ << ": " << cudaGetErrorString( result ) << " (" << #call << ")" << std::endl; abort(); \
-}
+// #include <iostream>
 
+// #define checkCudaErrors( call )\
+// {\
+//     cudaError_t result = call;\
+//     if (cudaSuccess != result)\
+//     {\
+//         std::cerr << "CUDA error " << result << " in " << __FILE__ << ":" << __LINE__ << ": " << cudaGetErrorString( result ) << " (" << #call << ")" << std::endl; \
+//     }\
+// }
+
+// #define checkCudaErrors(val) check((val), #val, __FILE__, __LINE__)
+//
+// template <typename T>
+// void check(T result, char const *const func, const char *const file,
+//            int const line) {
+//   if (result) {
+//     fprintf(stderr, "CUDA error at %s:%d code=%d(%s) \"%s\" \n", file, line,
+//             static_cast<unsigned int>(result), _cudaGetErrorEnum(result), func);
+//     // DEVICE_RESET
+//     // Make sure we call CUDA Device Reset before exiting
+//     exit(EXIT_FAILURE);
+//   }
+// }
 
 #else
 
