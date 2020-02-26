a2c321ca33fc935c043c93f56d92b1a5f713b0cc
Luke Shingles <luke.shingles@gmail.com>
2020-02-26 12:58:45 +0000

    Log cuda device properties on startup

--- rpkt.h
+++ rpkt.h
@@ -10,7 +10,7 @@ double get_rpkt_escape_prob(PKT *pkt_ptr, const double tstart, int tid);
 __host__ __device__ void calculate_kappa_bf_fb_gammacontr(const int modelgridindex, const double nu, double *kappa_bf, double *kappa_fb, rpkt_cont_opacity_struct *kappa_rpkt_cont);
 __host__ __device__ void calculate_kappa_rpkt_cont(const PKT *const pkt_ptr, const double t_current, const int modelgridindex, rpkt_cont_opacity_struct *kappa_rpkt_cont);
 void calculate_kappa_vpkt_cont(const PKT *pkt_ptr, double t_current, int tid);
-__host__ __device__ static rpkt_cont_opacity_struct *opacity_lock(void)
+__host__ __device__ rpkt_cont_opacity_struct *opacity_lock(void);
 __host__ __device__ void opacity_unlock(void);
 
 #endif //RPKT_H
