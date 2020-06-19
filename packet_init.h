c5f71b47b1a56f77e34c5e2d58894ebcfb4c999e
Luke Shingles <luke.shingles@gmail.com>
2020-06-19 10:48:02 +0100

    Enable exspec to work from temp packets files if packet*.out do not exist yet

--- packet_init.h
+++ packet_init.h
@@ -7,5 +7,6 @@
 void packet_init(int middle_iteration, int my_rank, PKT *pkt);
 void write_packets(char filename[], PKT *pkt);
 void read_packets(char filename[], PKT *pkt);
+void read_temp_packetsfile(const int timestep, const int my_rank, PKT *const pkt);
 
 #endif //PACKET_INIT_H
