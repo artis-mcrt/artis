75210d1e0558c2bbf9c00ec54ac1ce99bf0f4213
Luke Shingles <luke.shingles@gmail.com>
2020-10-09 18:25:49 +0100

    Fix compilation with VPKT_ON

--- vpkt.cc
+++ vpkt.cc
@@ -9,6 +9,83 @@
 
 
 #ifdef VPKT_ON
+// --------------------------------------------------------------------------------
+// ---------------------------  VIRTUAL PACKETS -----------------------------------
+// --------------------------------------------------------------------------------
+
+
+/* FREQUENCY */
+/* dlognu = (log(numax) - log(numin)) / VMNUBINS ~ 3.9e-4 (10'000 over 1e14-5e15 Hz) */
+#define numin_vspec (CLIGHT / 10000 * 1e8)
+#define numax_vspec (CLIGHT / 3500 * 1e8)
+#define VMNUBINS 2500
+
+/* TIME */
+/* dlogt = (log(tmin) - log(tmax)) / VMTBINS ~ 3.69e-2 (111 over 2-120 d) */
+#define tmin_vspec (10 * DAY)
+#define tmax_vspec (30 * DAY)
+#define VMTBINS 30
+
+/* Number of spectra for each observer (total + elements switched off) */
+#define MSPECTRA 12
+/* Number of observers */
+#define MOBS 5
+/* Total number of spectra */
+#define MTOT (MSPECTRA * MOBS)
+/* Total number of frequency ranges */
+#define MRANGE 1
+
+struct vspecpol
+{
+  double flux[VMNUBINS];
+  float lower_time;
+  float delta_t;
+} vstokes_i[VMTBINS][MTOT], vstokes_q[VMTBINS][MTOT], vstokes_u[VMTBINS][MTOT];
+
+float lower_freq_vspec[VMNUBINS];
+float delta_freq_vspec[VMNUBINS];
+
+// --------- INPUT PARAMETERS -----------
+
+int Nobs,Nspectra;
+double nz_obs_vpkt[MOBS] ;
+double phiobs[MOBS] ;
+double tmin_vspec_input,tmax_vspec_input;
+double Nrange;
+double lmin_vspec_input[MRANGE], lmax_vspec_input[MRANGE];
+double numin_vspec_input[MRANGE], numax_vspec_input[MRANGE];
+double cell_is_optically_thick_vpkt;
+double tau_max_vpkt;
+double exclude[MSPECTRA],tau_vpkt[MSPECTRA];
+
+// --------- VPKT GRID -----------
+
+#define MRANGE_GRID 5
+#define NY_VGRID 50
+#define NZ_VGRID 50
+
+struct vgrid
+{
+  double flux[MRANGE_GRID][MOBS];
+  double yvel[MRANGE_GRID][MOBS];
+  double zvel[MRANGE_GRID][MOBS];
+} vgrid_i[NY_VGRID][NZ_VGRID], vgrid_q[NY_VGRID][NZ_VGRID], vgrid_u[NY_VGRID][NZ_VGRID];
+
+double Nrange_grid, tmin_grid, tmax_grid, nu_grid_min[MRANGE_GRID], nu_grid_max[MRANGE_GRID] ;
+int vgrid_flag;
+double dlogt_vspec,dlognu_vspec;
+
+// --------- FLAGS -----------
+
+int realtype;
+/* number of virtual packets in a given timestep */
+int nvpkt;
+/* number of escaped virtual packet in a given timestep (with tau < tau_max) */
+int nvpkt_esc1; /* electron scattering event */
+int nvpkt_esc2; /* kpkt deactivation */
+int nvpkt_esc3; /* macroatom deactivation */
+
+
 void rlc_emiss_vpkt(PKT *pkt_ptr, double t_current, int bin, double *obs, int realtype)
 {
 
@@ -190,7 +267,7 @@ void rlc_emiss_vpkt(PKT *pkt_ptr, double t_current, int bin, double *obs, int re
 
               anumber = get_element(element);
 
-              dummypkt_ptr->next_trans = lineindex + 1;
+              dummy_ptr->next_trans = lineindex + 1;
 
               if (dummy_ptr->nu_cmf < nutrans) ldist = 0;
               else ldist = CLIGHT * t_current * (dummy_ptr->nu_cmf/nutrans - 1);
@@ -230,7 +307,7 @@ void rlc_emiss_vpkt(PKT *pkt_ptr, double t_current, int bin, double *obs, int re
           }
           else
           {
-            dummypkt_ptr->next_trans   = nlines + 1;  ///helper variable to overcome numerical problems after line scattering
+            dummy_ptr->next_trans   = nlines + 1;  ///helper variable to overcome numerical problems after line scattering
           }
 
       }
--- vpkt.h
+++ vpkt.h
@@ -21,81 +21,12 @@ int write_vpkt_grid(FILE *vpkt_grid_file);
 int read_vpkt_grid(FILE *vpkt_grid_file);
 int check_tau(double *tau, double *tau_max);
 
-// --------------------------------------------------------------------------------
-// ---------------------------  VIRTUAL PACKETS -----------------------------------
-// --------------------------------------------------------------------------------
+extern int vgrid_flag;
 
-
-/* FREQUENCY */
-/* dlognu = (log(numax) - log(numin)) / VMNUBINS ~ 3.9e-4 (10'000 over 1e14-5e15 Hz) */
-#define numin_vspec (CLIGHT / 10000 * 1e8)
-#define numax_vspec (CLIGHT / 3500 * 1e8)
-#define VMNUBINS 2500
-
-/* TIME */
-/* dlogt = (log(tmin) - log(tmax)) / VMTBINS ~ 3.69e-2 (111 over 2-120 d) */
-#define tmin_vspec (10 * DAY)
-#define tmax_vspec (30 * DAY)
-#define VMTBINS 30
-
-/* Number of spectra for each observer (total + elements switched off) */
-#define MSPECTRA 12
-/* Number of observers */
-#define MOBS 5
-/* Total number of spectra */
-#define MTOT (MSPECTRA * MOBS)
-/* Total number of frequency ranges */
-#define MRANGE 1
-
-struct vspecpol
-{
-  double flux[VMNUBINS];
-  float lower_time;
-  float delta_t;
-} vstokes_i[VMTBINS][MTOT], vstokes_q[VMTBINS][MTOT], vstokes_u[VMTBINS][MTOT];
-
-float lower_freq_vspec[VMNUBINS];
-float delta_freq_vspec[VMNUBINS];
-
-// --------- INPUT PARAMETERS -----------
-
-int Nobs,Nspectra;
-double nz_obs_vpkt[MOBS] ;
-double phiobs[MOBS] ;
-double tmin_vspec_input,tmax_vspec_input;
-double Nrange;
-double lmin_vspec_input[MRANGE], lmax_vspec_input[MRANGE];
-double numin_vspec_input[MRANGE], numax_vspec_input[MRANGE];
-double cell_is_optically_thick_vpkt;
-double tau_max_vpkt;
-double exclude[MSPECTRA],tau_vpkt[MSPECTRA];
-
-// --------- VPKT GRID -----------
-
-#define MRANGE_GRID 5
-#define NY_VGRID 50
-#define NZ_VGRID 50
-
-struct vgrid
-{
-  double flux[MRANGE_GRID][MOBS];
-  double yvel[MRANGE_GRID][MOBS];
-  double zvel[MRANGE_GRID][MOBS];
-} vgrid_i[NY_VGRID][NZ_VGRID], vgrid_q[NY_VGRID][NZ_VGRID], vgrid_u[NY_VGRID][NZ_VGRID];
-
-double Nrange_grid, tmin_grid, tmax_grid, nu_grid_min[MRANGE_GRID], nu_grid_max[MRANGE_GRID] ;
-int vgrid_flag;
-double dlogt_vspec,dlognu_vspec;
-
-// --------- FLAGS -----------
-
-int realtype;
-/* number of virtual packets in a given timestep */
-int nvpkt;
-/* number of escaped virtual packet in a given timestep (with tau < tau_max) */
-int nvpkt_esc1; /* electron scattering event */
-int nvpkt_esc2; /* kpkt deactivation */
-int nvpkt_esc3; /* macroatom deactivation */
+extern int nvpkt;
+extern int nvpkt_esc1; /* electron scattering event */
+extern int nvpkt_esc2; /* kpkt deactivation */
+extern int nvpkt_esc3; /* macroatom deactivation */
 
 #endif
 
