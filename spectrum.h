d6bed41b05dbae751ff9155b75e9cef70628b064
Luke Shingles <luke.shingles@gmail.com>
2020-10-25 06:38:28 +0000

    Refactor exspec

--- spectrum.h
+++ spectrum.h
@@ -5,12 +5,21 @@
 #include "exspec.h"
 #include "types.h"
 
-void write_spectrum(char spec_filename[], bool do_emission_res, char emission_filename[], char trueemission_filename[], char absorption_filename[], struct spec *spectra);
-void write_specpol(char spec_filename[], bool do_emission_res, char emission_filename[], char absorption_filename[]);
-void add_to_spec_res(const PKT *const pkt_ptr, int current_abin, const bool do_emission_res, struct spec *spectra, const double nu_min, const double nu_max);
+void write_spectrum(
+  char spec_filename[], char emission_filename[], char trueemission_filename[],
+  char absorption_filename[], struct spec *spectra);
+
+void write_specpol(
+  char spec_filename[], char emission_filename[], char absorption_filename[],
+  struct spec *stokes_i, struct spec *stokes_q, struct spec *stokes_u);
+
+void add_to_spec_res(
+  const PKT *const pkt_ptr, int current_abin, struct spec *spectra,
+  struct spec *stokes_i, struct spec *stokes_q, struct spec *stokes_u);
+
 struct spec *alloc_spectra(const bool do_emission_res);
 void init_spectra(struct spec *spectra, const double nu_min, const double nu_max, const bool do_emission_res);
 void init_spectrum_trace(void);
-void free_spectra(struct spec *spectra, const bool do_emission_res);
+void free_spectra(struct spec *spectra);
 
 #endif //SPECTRUM_H
