9667083baddcf07bd9832f310f383ba54031eda1
Luke Shingles <luke.shingles@gmail.com>
2021-01-03 16:17:37 +0000

    Add more __host__ __device__ flags

--- kpkt.h
+++ kpkt.h
@@ -5,11 +5,11 @@
 #include "types.h"
 
 void setup_coolinglist(void);
-void calculate_cooling_rates(int modelgridindex, heatingcoolingrates_t *heatingcoolingrates);
-double do_kpkt_bb(PKT *pkt_ptr);
-double do_kpkt(PKT *pkt_ptr, double t2, int nts);
+__host__ __device__ void calculate_cooling_rates(int modelgridindex, heatingcoolingrates_t *heatingcoolingrates);
+__host__ __device__ double do_kpkt_bb(PKT *pkt_ptr);
+__host__ __device__ double do_kpkt(PKT *pkt_ptr, double t2, int nts);
 
-__host__ __device__ 
+__host__ __device__
 inline int get_coolinglistoffset(int element, int ion)
 {
   return globals::elements[element].ions[ion].coolingoffset;
