e8034fa84bcf4ff2a3c4095da61cab7ca63e03be
lukeshingles <luke.shingles@gmail.com>
2021-01-18 14:26:34 +0000

    Move get_coolinglistoffset() into kpkt.cc

--- kpkt.h
+++ kpkt.h
@@ -8,11 +8,6 @@ void setup_coolinglist(void);
 __host__ __device__ void calculate_cooling_rates(int modelgridindex, heatingcoolingrates_t *heatingcoolingrates);
 __host__ __device__ double do_kpkt_bb(PKT *pkt_ptr);
 __host__ __device__ double do_kpkt(PKT *pkt_ptr, double t2, int nts);
-
-__host__ __device__
-inline int get_coolinglistoffset(int element, int ion)
-{
-  return globals::elements[element].ions[ion].coolingoffset;
-}
+__host__ __device__ int get_coolinglistoffset(int element, int ion);
 
 #endif //KPKT_H
