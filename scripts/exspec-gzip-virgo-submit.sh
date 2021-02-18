4fc04aa9e6c1c01a4f2aec6ffb48b164bbbabf07
lukeshingles <luke.shingles@gmail.com>
2021-02-18 11:07:56 +0000

    Use 24-hour jobs on virgo

--- scripts/exspec-gzip-virgo-submit.sh
+++ scripts/exspec-gzip-virgo-submit.sh
@@ -1,1 +1,1 @@
-sbatch -J artis --ntasks=1 --partition=long --time=02:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/exspec-gzip-virgo-slurmjob.sh
+sbatch -J artis --ntasks=1 --partition=long --time=48:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/exspec-gzip-virgo-slurmjob.sh
