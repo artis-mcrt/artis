0ff20fda2b3fdf7ebc4248d1f78a82a7cac6d967
lukeshingles <luke.shingles@gmail.com>
2021-02-18 12:32:12 +0000

    Add hashbang

--- scripts/artis-virgo-submit.sh
+++ scripts/artis-virgo-submit.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 sbatch -J artis_$(basename $(exec pwd)) --ntasks=960 --partition=long --time=24:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/artis-virgo-slurmjob.sh
