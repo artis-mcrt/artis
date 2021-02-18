9733bacb8b5b86f9a390fdf68e6b768da1b31ef4
lukeshingles <luke.shingles@gmail.com>
2021-02-18 10:44:37 +0000

    Update virgo scripts

--- scripts/artis-virgo-submit.sh
+++ scripts/artis-virgo-submit.sh
@@ -1,1 +1,1 @@
-sbatch -J artis --ntasks=960 --partition=main --time=08:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/artis-virgo-slurmjob.sh
+sbatch -J artis_$(basename $(exec pwd)) --ntasks=960 --partition=main --time=08:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/artis-virgo-slurmjob.sh
