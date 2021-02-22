fa3d50689fe851b0349197dffcd7ff0422f75e99
lukeshingles <luke.shingles@gmail.com>
2021-02-22 15:18:23 +0000

    Find submit script on virgo

--- scripts/artis-virgo-slurmjob.sh
+++ scripts/artis-virgo-slurmjob.sh
@@ -14,7 +14,13 @@ mkdir ${SLURM_JOBID}.slurm
 
 if grep -q "RESTART_NEEDED" "output_0-0.txt"
 then
-    ./artis-virgo-submit.sh
+    # check if there's a submit script in the submission directory, otherwise use the repository version
+    if [ -f ./artis-virgo-submit.sh ]
+    then
+        ./artis-virgo-submit.sh
+    else
+        ./artis/scripts/artis-virgo-submit.sh
+    fi
 fi
 
 if [ -f packets00_0000.out ]; then
