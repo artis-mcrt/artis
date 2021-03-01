c3d4a25cfd7f66b29794ce39c48f71fba782cc49
lukeshingles <luke.shingles@gmail.com>
2021-03-01 11:58:49 +0000

    Improve exspec job name on virgo

--- scripts/exspec-gzip-virgo-submit.sh
+++ scripts/exspec-gzip-virgo-submit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-sbatch -J artis --ntasks=1 --partition=long --time=48:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/exspec-gzip-virgo-slurmjob.sh
+sbatch -J exspecgzip_$(basename $(exec pwd)) --ntasks=1 --partition=long --time=48:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/exspec-gzip-virgo-slurmjob.sh
