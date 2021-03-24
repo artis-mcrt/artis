e30af54aa0e854da4a15941b4e4bbcf66a9d792b
lukeshingles <luke.shingles@gmail.com>
2021-03-24 10:13:48 +0000

    Update CI reference data

--- tests/setuptestdata.sh
+++ tests/setuptestdata.sh
@@ -6,11 +6,13 @@ if [ ! -f atomicdata_feconi.tar.xz ]; then curl -O https://psweb.mp.qub.ac.uk/ar
 
 tar -xf atomicdata_feconi.tar.xz --directory nebularonezone/
 
+tar -xf atomicdata_feconi.tar.xz --directory nebularonezone_reference/
+
 cp ../data/* nebularonezone/
+cp ../data/* nebularonezone_reference/
 
+if [ ! -f nebularonezone_reference_20210324.tar.xz ]; then curl -O https://psweb.mp.qub.ac.uk/artis/artis/nebularonezone_reference_20210324.tar.xz; fi
 
-if [ ! -f nebularonezone_reference.tar.xz ]; then curl -O https://psweb.mp.qub.ac.uk/artis/artis/nebularonezone_reference.tar.xz; fi
-
-tar -xf nebularonezone_reference.tar.xz
+tar -xf nebularonezone_reference_20210324.tar.xz
 
 set +x
