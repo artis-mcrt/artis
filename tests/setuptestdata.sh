2c3f13ec567e828439426e6dc0e3c339a9228f17
lukeshingles <luke.shingles@gmail.com>
2021-03-24 10:17:26 +0000

    Update setuptestdata.sh

--- tests/setuptestdata.sh
+++ tests/setuptestdata.sh
@@ -6,13 +6,12 @@ if [ ! -f atomicdata_feconi.tar.xz ]; then curl -O https://psweb.mp.qub.ac.uk/ar
 
 tar -xf atomicdata_feconi.tar.xz --directory nebularonezone/
 
-tar -xf atomicdata_feconi.tar.xz --directory nebularonezone_reference/
-
 cp ../data/* nebularonezone/
 cp ../data/* nebularonezone_reference/
 
 if [ ! -f nebularonezone_reference_20210324.tar.xz ]; then curl -O https://psweb.mp.qub.ac.uk/artis/artis/nebularonezone_reference_20210324.tar.xz; fi
 
 tar -xf nebularonezone_reference_20210324.tar.xz
+tar -xf atomicdata_feconi.tar.xz --directory nebularonezone_reference/
 
 set +x
