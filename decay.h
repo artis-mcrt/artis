ba58c5798dc267b5611d6b49120612ee4f04c1ed
lukeshingles <luke.shingles@gmail.com>
2021-03-24 19:59:28 +0000

    Update decay.h

--- decay.h
+++ decay.h
@@ -17,7 +17,6 @@ namespace decay
   __host__ __device__ int get_nuc_index(int atomic_number, int mass_number);
   __host__ __device__ double nucdecayenergygamma(int z, int a);
   __host__ __device__ void set_nucdecayenergygamma(int z, int a, double value);
-  __host__ __device__ double nucdecayenergypositrons(int z, int a);
   __host__ __device__ double nucdecayenergy(int z, int a);
   __host__ __device__ double get_meanlife(int z, int a);
   __host__ __device__ double nucmass(int z, int a);
