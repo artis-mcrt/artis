3c2159acaecba378b4539a77b8eff2d5a4cea3d7
lukeshingles <luke.shingles@gmail.com>
2021-03-27 09:05:18 +0000

    Update grid.cc

--- grid.cc
+++ grid.cc
@@ -1579,7 +1579,7 @@ void write_grid_restart_data(const int timestep)
 }
 
 
-static void assign_initial_temperaturesvoid)
+static void assign_initial_temperatures(void)
 /// Routine for assigning temperatures to the grid cells at the start of the simulation.
 {
   /// For a simulation started from scratch we estimate the initial temperatures
@@ -1896,7 +1896,7 @@ void grid_init(int my_rank)
   }
   else
   {
-    assign_initial_temperatures);
+    assign_initial_temperatures();
   }
 
   // scale up the radioactive abundances to account for the missing masses in
