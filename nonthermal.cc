3754d7a42832b3a340fba1fc9474dacd69e4756c
lukeshingles <luke.shingles@gmail.com>
2021-04-13 15:32:48 +0100

    Update nonthermal.cc

--- nonthermal.cc
+++ nonthermal.cc
@@ -2856,15 +2856,15 @@ void solve_spencerfano(const int modelgridindex, const int timestep, const int i
   //
   //   printout("Doing a fast initial solution without ionization or excitation in the SF equation for the first NLTE timestep.\n");
   // }
-  // else if (timestep <= globals::n_lte_timesteps + 2)
-  // {
-  //   // run the solver in a faster mode for the first couple of NLTE timesteps
-  //   // nt_solution[modelgridindex].nneperion_when_solved = -1.;
-  //   enable_sfexcitation = false;
-  //   // enable_sfionization = false;
-  //
-  //   printout("Doing a faster solution without excitation in the SF equation for the first couple of NLTE timesteps.\n");
-  // }
+  if (timestep <= globals::n_lte_timesteps + 2)
+  {
+    // run the solver in a faster mode for the first couple of NLTE timesteps
+    // nt_solution[modelgridindex].nneperion_when_solved = -1.;
+    enable_sfexcitation = false;
+    // enable_sfionization = false;
+
+    printout("Doing a faster solution without excitation in the SF equation for the first couple of NLTE timesteps.\n");
+  }
 
   gsl_matrix *const sfmatrix = gsl_matrix_calloc(SFPTS, SFPTS);
   gsl_vector *const rhsvec = gsl_vector_calloc(SFPTS); // constant term (not dependent on y func) in each equation
