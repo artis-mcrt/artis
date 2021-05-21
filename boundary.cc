46299eaa375467bc15958b63f92fa53482f86a6c
Christine <ccollins22@qub.ac.uk>
2021-05-21 15:24:21 +0200

    Update escape condition. If empty cell outside rmax then escaped

--- boundary.cc
+++ boundary.cc
@@ -383,7 +383,15 @@ void change_cell(PKT *pkt_ptr, int snext, double t_current)
     }
   #endif
 
-  if (snext == -99)
+  double cell_distance = sqrt((get_cellcoordmin(pkt_ptr->where, 0) * get_cellcoordmin(pkt_ptr->where, 0))
+                              + (get_cellcoordmin(pkt_ptr->where, 1) * get_cellcoordmin(pkt_ptr->where, 1))
+                              + (get_cellcoordmin(pkt_ptr->where, 2) * get_cellcoordmin(pkt_ptr->where, 2)));
+  if (cell_distance > globals::rmax)
+  {
+    printout("celldist %g rmax %g\n", cell_distance, globals::rmax);
+  }
+
+  if (snext == -99 || (get_cell_modelgridindex(pkt_ptr->where) == MMODELGRID && cell_distance > globals::rmax))
   {
     // Then the packet is exiting the grid. We need to record
     // where and at what time it leaves the grid.
