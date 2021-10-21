2794d8afc02208a4e14145c8077260d4dd296e62
Luke Shingles <luke.shingles@gmail.com>
2021-10-21 13:27:52 +0100

    Update movefiles.sh

--- scripts/movefiles.sh
+++ scripts/movefiles.sh
@@ -2,16 +2,21 @@
 
 # this script will move the ARTIS output files from the current run into a subfolder
 # given by the command line argument
+# e.g. ./artis/scripts/movefiles.sh 1234589.slurm
 
 if [ $# -ne 1 ]; then
   echo 1>&2 "Usage: $0 JOB_DIRECTORY"
   exit 3
 fi
 
-#mkdir $1
+# mkdir -p $1
 
 # check that the argument is a valid directory or exit
-[ -d "$1" ] || exit
+if [ ! -d "$1" ]; then
+  echo 1>&2 "JOB_DIRECTORY '$1' is not an existing directory"
+  echo 1>&2 "to create, run mkdir '$1'"
+  exit 3
+fi
 
 #cp gridsave.dat $1
 mv estimators* $1
