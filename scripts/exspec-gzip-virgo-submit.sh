eca065d5158371964a81b1adc67ebda5686696d9
Luke Shingles <luke.shingles@gmail.com>
2021-11-10 17:54:25 +0000

    Update virgo scripts

--- scripts/exspec-gzip-virgo-submit.sh
+++ scripts/exspec-gzip-virgo-submit.sh
@@ -1,2 +1,2 @@
-#!/bin/bash -x
+#!/bin/bash
 sbatch -J exspecgzip_$(basename $(exec pwd)) --ntasks=1 --partition=long --time=48:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/exspec-gzip-virgo-slurmjob.sh
