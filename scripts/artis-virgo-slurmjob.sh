f02bc484fd0ee916f065d82ed9db3f9c07f9746d
Luke Shingles <luke.shingles@gmail.com>
2021-11-11 09:39:20 +0000

    Use source (script.sh) on virgo

--- scripts/artis-virgo-slurmjob.sh
+++ scripts/artis-virgo-slurmjob.sh
@@ -12,19 +12,19 @@ echo "$(date): time after srun sn3d finished"
 echo "seconds elapsed: $SECONDS"
 
 mkdir ${SLURM_JOBID}.slurm
-./artis/scripts/movefiles.sh ${SLURM_JOBID}.slurm
+source ./artis/scripts/movefiles.sh ${SLURM_JOBID}.slurm
 
 if grep -q "RESTART_NEEDED" "output_0-0.txt"
 then
     # check if there's a submit script in the submission directory, otherwise use the repository version
     if [ -f ./artis-virgo-submit.sh ]
     then
-        ./artis-virgo-submit.sh
+        source ./artis-virgo-submit.sh
     else
-        ./artis/scripts/artis-virgo-submit.sh
+        source ./artis/scripts/artis-virgo-submit.sh
     fi
 fi
 
 if [ -f packets00_0000.out ]; then
-    ./artis/scripts/exspec-gzip-virgo-submit.sh
+    source ./artis/scripts/exspec-gzip-virgo-submit.sh
 fi
--- scripts/exspec-gzip-virgo-slurmjob.sh
+++ scripts/exspec-gzip-virgo-slurmjob.sh
@@ -5,10 +5,10 @@ spack load gsl target=$(spack arch -t)
 cd $SLURM_SUBMIT_DIR
 
 if [ ! -f spec.out ]; then
-  ./artis/scripts/exspec-before.sh
+  source ./artis/scripts/exspec-before.sh
   ./exspec
 else
   echo 'Not running exspec because spec.out was found'
 fi
 
-./artis/scripts/exspec-after.sh
+source ./artis/scripts/exspec-after.sh
