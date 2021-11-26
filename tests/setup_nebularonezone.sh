00e66aca00169997d52f19584475a71fea6e7ee7
Luke Shingles <luke.shingles@gmail.com>
2021-11-26 14:02:38 +0000

    Update CI reference model

--- tests/setup_nebularonezone.sh
+++ tests/setup_nebularonezone.sh
@@ -10,9 +10,9 @@ tar -xf atomicdata_feconi.tar.xz --directory nebularonezone_testrun/
 
 cp ../data/* nebularonezone_testrun/
 
-if [ ! -f nebularonezone_reference_20211109.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/nebularonezone_reference_20211109.tar.xz; fi
+if [ ! -f nebularonezone_reference_20211126.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/nebularonezone_reference_20211126.tar.xz; fi
 
-tar -xf nebularonezone_reference_20211109.tar.xz
+tar -xf nebularonezone_reference_20211126.tar.xz
 tar -xf atomicdata_feconi.tar.xz --directory nebularonezone_reference/
 cp ../data/* nebularonezone_reference/
 
