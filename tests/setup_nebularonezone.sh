9f066b4dd0aab26b261dfb0143eb6590bf2a7934
Luke Shingles <luke.shingles@gmail.com>
2021-11-26 13:28:04 +0000

    Place CI test run in separate folder

--- tests/setup_nebularonezone.sh
+++ tests/setup_nebularonezone.sh
@@ -2,11 +2,13 @@
 
 set -x
 
+rsync -av nebularonezone_inputfiles/ nebularonezone_testrun/
+
 if [ ! -f atomicdata_feconi.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/atomicdata_feconi.tar.xz; fi
 
-tar -xf atomicdata_feconi.tar.xz --directory nebularonezone/
+tar -xf atomicdata_feconi.tar.xz --directory nebularonezone_testrun/
 
-cp ../data/* nebularonezone/
+cp ../data/* nebularonezone_testrun/
 
 if [ ! -f nebularonezone_reference_20211109.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/nebularonezone_reference_20211109.tar.xz; fi
 
