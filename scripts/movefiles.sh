636c1064af5cb7dad08fc38d51a2068ecb393a60
Luke Shingles <luke.shingles@gmail.com>
2022-04-06 07:42:23 +0100

    Update movefiles.sh

--- scripts/movefiles.sh
+++ scripts/movefiles.sh
@@ -21,9 +21,9 @@ fi
 #cp gridsave.dat $1
 mv estimators* $1
 # move the files below if the exist, but don't cause an error if they don't exist!
-find . -maxdepth 1 -name 'macroatom*' -type f -print0 | xargs -0r mv -t $1
-find . -maxdepth 1 -name 'nonthermal*' -type f -print0 | xargs -0r mv -t $1
-find . -maxdepth 1 -name 'radfield*' -type f -print0 | xargs -0r mv -t $1
-find . -maxdepth 1 -name 'nlte*' -type f -print0 | xargs -0r mv -t $1
+find . -maxdepth 1 -name 'macroatom*' -exec mv {} $1 \;
+find . -maxdepth 1 -name 'nonthermal*' -exec mv {} $1 \;
+find . -maxdepth 1 -name 'radfield*' -exec mv {} $1 \;
+find . -maxdepth 1 -name 'nlte*' -exec mv {} $1 \;
 mv output_*.txt $1
 cp $1/output_0-0.txt .
