b7fc35caf1ea493931c7744309b3c32f089dcc28
Christine Collins <ccollins22@qub.ac.uk>
2022-07-26 16:06:55 +0200

    Add no-requeue flag

--- scripts/artis-virgo-submit.sh
+++ scripts/artis-virgo-submit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbatch -J $(basename $(exec pwd)) --ntasks=960 --ntasks-per-node=32 --partition=long --time=24:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/artis-virgo-slurmjob.sh
+sbatch -J $(basename $(exec pwd)) --ntasks=960 --ntasks-per-node=32 --partition=long --time=24:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de --no-requeue -- artis/scripts/artis-virgo-slurmjob.sh
