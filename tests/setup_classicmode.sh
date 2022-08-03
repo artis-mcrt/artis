f7bf2be3a51c00a43ef1d8148ef176cb0004c9ea
Luke Shingles <luke.shingles@gmail.com>
2022-08-03 22:34:58 +0100

    Update setup_classicmode.sh

--- tests/setup_classicmode.sh
+++ tests/setup_classicmode.sh
@@ -10,7 +10,7 @@ tar -xf atomicdata_feconi.tar.xz --directory classicmode_testrun/
 
 cp ../data/* classicmode_testrun/
 # cp ../artisoptions_classic.h classicmode_testrun/artisoptions.h
-sed 's/#define MPKTS.*/#define MPKTS 1000/g' ../artisoptions_classic.h > classicmode_testrun/artisoptions.h
+sed 's/#define MPKTS.*/#define MPKTS 5000/g' ../artisoptions_classic.h > classicmode_testrun/artisoptions.h
 
 #if [ ! -f classicmode_reference_20211126.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/classicmode_reference_20211126.tar.xz; fi
 
