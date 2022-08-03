4fe574dcad3cd9683948ec5b419c18fff546b72a
Luke Shingles <luke.shingles@gmail.com>
2022-08-03 23:05:52 +0100

    CI: Remove outdated reference model and update actions

--- tests/setup_classicmode.sh
+++ tests/setup_classicmode.sh
@@ -10,13 +10,7 @@ tar -xf atomicdata_feconi.tar.xz --directory classicmode_testrun/
 
 cp ../data/* classicmode_testrun/
 # cp ../artisoptions_classic.h classicmode_testrun/artisoptions.h
+
 sed 's/#define MPKTS.*/#define MPKTS 15000/g' ../artisoptions_classic.h > classicmode_testrun/artisoptions.h
-
-#if [ ! -f classicmode_reference_20211126.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/classicmode_reference_20211126.tar.xz; fi
-
-#tar -xf classicmode_reference_20211126.tar.xz
-#tar -xf atomicdata_feconi.tar.xz --directory classicmode_reference/
-#cp ../data/* classicmode_reference/
-#if [ ! -f classicmode_reference/input.txt ]; then cp classicmode_inputfiles/input-newrun.txt classicmode_reference/input.txt; fi
 
 set +x
--- tests/setup_nebularonezone.sh
+++ tests/setup_nebularonezone.sh
@@ -10,11 +10,4 @@ tar -xf atomicdata_feconi.tar.xz --directory nebularonezone_testrun/
 
 cp ../data/* nebularonezone_testrun/
 
-if [ ! -f nebularonezone_reference_20211126.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/nebularonezone_reference_20211126.tar.xz; fi
-
-tar -xf nebularonezone_reference_20211126.tar.xz
-tar -xf atomicdata_feconi.tar.xz --directory nebularonezone_reference/
-cp ../data/* nebularonezone_reference/
-if [ ! -f nebularonezone_reference/input.txt ]; then cp nebularonezone_inputfiles/input-newrun.txt nebularonezone_reference/input.txt; fi
-
 set +x
