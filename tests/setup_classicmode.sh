2632d476c3c5bf4978089a628bb78b3b6a9c8ef2
Luke Shingles <luke.shingles@gmail.com>
2022-08-03 16:41:50 +0100

    Update setup_classicmode.sh

--- tests/setup_classicmode.sh
+++ tests/setup_classicmode.sh
@@ -2,21 +2,21 @@
 
 set -x
 
-rsync -av nebularonezone_inputfiles/ nebularonezone_testrun/
+rsync -av classicmode_inputfiles/ classicmode_testrun/
 
 if [ ! -f atomicdata_feconi.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/atomicdata_feconi.tar.xz; fi
 
-tar -xf atomicdata_feconi.tar.xz --directory nebularonezone_testrun/
+tar -xf atomicdata_feconi.tar.xz --directory classicmode_testrun/
 
-cp ../data/* nebularonezone_testrun/
-cp ../artisoptions_classic.h nebularonezone_testrun/artisoptions.h
-sed 's/#define MPKTS.*/#define MPKTS 10000/g' nebularonezone_testrun/artisoptions.h > nebularonezone_testrun/artisoptions.h
+cp ../data/* classicmode_testrun/
+cp ../artisoptions_classic.h classicmode_testrun/artisoptions.h
+sed 's/#define MPKTS.*/#define MPKTS 10000/g' classicmode_testrun/artisoptions.h > classicmode_testrun/artisoptions.h
 
-#if [ ! -f nebularonezone_reference_20211126.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/nebularonezone_reference_20211126.tar.xz; fi
+#if [ ! -f classicmode_reference_20211126.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/classicmode_reference_20211126.tar.xz; fi
 
-#tar -xf nebularonezone_reference_20211126.tar.xz
-#tar -xf atomicdata_feconi.tar.xz --directory nebularonezone_reference/
-#cp ../data/* nebularonezone_reference/
-#if [ ! -f nebularonezone_reference/input.txt ]; then cp nebularonezone_inputfiles/input-newrun.txt nebularonezone_reference/input.txt; fi
+#tar -xf classicmode_reference_20211126.tar.xz
+#tar -xf atomicdata_feconi.tar.xz --directory classicmode_reference/
+#cp ../data/* classicmode_reference/
+#if [ ! -f classicmode_reference/input.txt ]; then cp classicmode_inputfiles/input-newrun.txt classicmode_reference/input.txt; fi
 
 set +x
