fa4649c7d0adfb996c1d8afd39e46427e6ea3afc
Luke Shingles <luke.shingles@gmail.com>
2022-08-19 11:44:12 +0100

    Squashed commit of the following:
    commit 308cbe78fe85fb4314a24af81d449b88c3853001
    Merge: 08a1cdc6 1b73ca06
    Author: Luke Shingles <luke.shingles@gmail.com>
    Date:   Fri Aug 19 11:43:47 2022 +0100
    
        Merge branch 'nebular' into nebular_cicustomartisoptions
    
    commit 08a1cdc64ae7256e6a06bfcc1ec697237f6a573e
    Author: Luke Shingles <luke.shingles@gmail.com>
    Date:   Thu Aug 18 18:50:09 2022 +0100
    
        Update setup_nebularonezone.sh
    
    commit 1df583c442b27d46079cc9af2c2c01384a3f76cf
    Author: Luke Shingles <luke.shingles@gmail.com>
    Date:   Thu Aug 18 18:21:07 2022 +0100
    
        Update ci.yml
    
    commit 07090fe24a308fb685757ba29ea61974cae13c4c
    Author: Luke Shingles <luke.shingles@gmail.com>
    Date:   Thu Aug 18 15:39:42 2022 +0100
    
        Update setup_nebularonezone.sh
    
    commit 398abbc4d38a148edbc089c23ae286982d017165
    Author: Luke Shingles <luke.shingles@gmail.com>
    Date:   Thu Aug 18 15:39:05 2022 +0100
    
        Update ci.yml
    
    commit e1e5b4f5b4420828cda90a385ca977caee9de3a9
    Author: Luke Shingles <luke.shingles@gmail.com>
    Date:   Thu Aug 18 15:35:36 2022 +0100
    
        Update ci.yml
    
    commit 0782b90ea1dd3b7897a3efa7574ca5b779ae86a6
    Author: Luke Shingles <luke.shingles@gmail.com>
    Date:   Thu Aug 18 15:30:19 2022 +0100
    
        Update setup_nebularonezone.sh

--- tests/setup_nebularonezone.sh
+++ tests/setup_nebularonezone.sh
@@ -10,4 +10,24 @@ tar -xf atomicdata_feconi.tar.xz --directory nebularonezone_testrun/
 
 cp ../data/* nebularonezone_testrun/
 
+cp ../artisoptions_nltenebular.h nebularonezone_testrun/artisoptions.h
+
+cd nebularonezone_testrun
+
+# sed -i 's/#define MPKTS.*/#define MPKTS 15000/g' artisoptions.h
+
+sed -i 's/#define TABLESIZE.*/#define TABLESIZE 20/g' artisoptions.h
+sed -i 's/#define MINTEMP.*/#define MINTEMP 2000./g' artisoptions.h
+sed -i 's/#define MAXTEMP.*/#define MAXTEMP 10000./g' artisoptions.h
+
+sed -i 's/static const int FIRST_NLTE_RADFIELD_TIMESTEP.*/static const int FIRST_NLTE_RADFIELD_TIMESTEP = 7;/g' artisoptions.h
+
+sed -i 's/#define DETAILED_BF_ESTIMATORS_USEFROMTIMESTEP.*/#define DETAILED_BF_ESTIMATORS_USEFROMTIMESTEP 7/g' artisoptions.h
+
+sed -i 's/#define SF_AUGER_CONTRIBUTION_ON.*/#define SF_AUGER_CONTRIBUTION_ON false/g' artisoptions.h
+
+sed -i 's/static bool WRITE_PARTIAL_EMISSIONABSORPTIONSPEC.*/static bool WRITE_PARTIAL_EMISSIONABSORPTIONSPEC = true;/g' artisoptions.h
+
+cd -
+
 set +x
