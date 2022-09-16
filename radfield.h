412a617ff6e2636accb1cc3caff37393ba0e27e1
Luke Shingles <luke.shingles@gmail.com>
2022-09-16 20:04:01 +0100

    Update radfield.h

--- radfield.h
+++ radfield.h
@@ -40,12 +40,13 @@ void reset_bfrate_contributions(const int modelgridindex);
 int integrate(const gsl_function *f, double nu_a, double nu_b, double epsabs, double epsrel, size_t limit, int key,
               gsl_integration_workspace *workspace, double *result, double *abserr);
 
-template <typename numtype>
-__host__ __device__ static inline double dbb(double nu, numtype T, float W)
+template <typename T_t, typename W_t>
+__host__ __device__ static inline double dbb(double nu, T_t T, W_t W)
 // returns J_nu for a dilute black body [ergs/s/sr/cm2/Hz]
 {
   return W * TWOHOVERCLIGHTSQUARED * pow(nu, 3) / expm1(HOVERKB * nu / T);
 }
+
 }  // namespace radfield
 
 #endif  // RADFIELD_H
