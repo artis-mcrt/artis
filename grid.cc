cd0f6ea76f9407d881ad9db30606a9bf5d9f1843
Luke Shingles <luke.shingles@gmail.com>
2022-10-07 12:52:00 +0100

    Fix

--- grid.cc
+++ grid.cc
@@ -1974,13 +1974,13 @@ int get_nstart(const int rank) {
   if (ranks_ndo.size() == 0) {
     setup_nstart_ndo();
   }
-  return ranks_ndo[rank];
+  return ranks_nstart[rank];
 }
 int get_ndo(const int rank) {
   if (ranks_ndo.size() == 0) {
     setup_nstart_ndo();
   }
-  return ranks_nstart[rank];
+  return ranks_ndo[rank];
 }
 int get_ndo_nonempty(const int rank) {
   if (ranks_ndo.size() == 0) {
