9627c7ee7761fcbb94db9e7e8db868d1b7cce54f
Luke Shingles <luke.shingles@gmail.com>
2022-10-10 15:09:41 +0100

    Update gammapkt.cc

--- gammapkt.cc
+++ gammapkt.cc
@@ -206,6 +206,7 @@ void init_gamma_linelist(void) {
     }
   }
   allnuc_gamma_line_list.shrink_to_fit();
+  assert_always(allnuc_gamma_line_list.size() == total_lines);
   std::sort(allnuc_gamma_line_list.begin(), allnuc_gamma_line_list.end());
 
   FILE *const line_list = fopen_required("gammalinelist.out", "w+");
