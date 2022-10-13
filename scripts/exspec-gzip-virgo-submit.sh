5d3305ef2c22a8d1bd61376d63fa7f64554c1ff2
Luke Shingles <luke.shingles@gmail.com>
2022-10-13 14:19:41 +0100

    Update job scripts

--- scripts/exspec-gzip-virgo-submit.sh
+++ scripts/exspec-gzip-virgo-submit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbatch -J ex_$(basename $(exec pwd)) --ntasks=8 --partition=long --constraint=amd --time=48:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/exspec-gzip-virgo-slurmjob.sh
+sbatch -J ex_$(basename $(exec pwd)) --ntasks=8 --ntasks-per-core=1 --partition=long --constraint=amd --time=48:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/exspec-gzip-virgo-slurmjob.sh
