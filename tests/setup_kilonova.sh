ccf008e1fcac8f7377800d8ea087b928d98ed514
Luke Shingles <luke.shingles@gmail.com>
2022-11-09 20:57:47 +0000

    Add kilonova test to CI

---
+++ tests/setup_kilonova.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+set -x
+
+rsync -av kilonova_inputfiles/ kilonova_testrun/
+
+if [ ! -f atomicdata_feconi.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/atomicdata_feconi.tar.xz; fi
+
+tar -xf atomicdata_feconi.tar.xz --directory kilonova_testrun/
+
+cp ../data/* kilonova_testrun/
+
+cp ../artisoptions_kilonova_lte.h kilonova_testrun/artisoptions.h
+
+cd kilonova_testrun
+
+xz -dv -T0 *.xz
+
+sed -i 's/#define MPKTS.*/#define MPKTS 10000/g' artisoptions.h
+
+sed -i 's/constexpr bool WRITE_PARTIAL_EMISSIONABSORPTIONSPEC.*/constexpr bool WRITE_PARTIAL_EMISSIONABSORPTIONSPEC = true;/g' artisoptions.h
+
+cd -
+
+set +x
