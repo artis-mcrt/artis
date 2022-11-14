f38e930a5369a28addafef9719cee856f980427a
Luke Shingles <luke.shingles@gmail.com>
2022-11-14 21:07:46 +0000

    constexpr lineiscommentonly

--- input.cc
+++ input.cc
@@ -1828,22 +1828,6 @@ void input(int rank)
   }
 }
 
-bool lineiscommentonly(const std::string &line)
-// return true for whitepace-only lines, and lines that are exclusively whitepace up to a '#' character
-{
-  int searchlength = line.find('#');  // ignore anything to the right of a # character
-  if (searchlength < 0) {
-    searchlength = line.length();
-  }
-
-  for (int i = 0; i < searchlength; i++) {
-    if (line[i] != ' ') {
-      return false;
-    }
-  }
-  return true;
-}
-
 static bool getline(std::istream &input, std::string &line)
 // return true if line read, false if not (EOF)
 {
--- input.h
+++ input.h
@@ -12,6 +12,21 @@ void update_parameterfile(int nts);
 void time_init(void);
 void write_timestep_file(void);
 bool get_noncommentline(std::istream &input, std::string &line);
-bool lineiscommentonly(const std::string &line);
+
+constexpr bool lineiscommentonly(const std::string &line)
+// return true for whitepace-only lines, and lines that are exclusively whitepace up to a '#' character
+{
+  int searchlength = line.find('#');  // ignore anything to the right of a # character
+  if (searchlength < 0) {
+    searchlength = line.length();
+  }
+
+  for (int i = 0; i < searchlength; i++) {
+    if (line[i] != ' ') {
+      return false;
+    }
+  }
+  return true;
+}
 
 #endif  // INPUT_H
