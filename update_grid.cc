d64701a1a2e82bf4ede5847bfb275b979cd425b1
Luke Shingles <luke.shingles@gmail.com>
2022-11-15 12:17:44 +0000

    Update update_grid.cc

--- update_grid.cc
+++ update_grid.cc
@@ -681,7 +681,8 @@ __host__ __device__ void cellhistory_reset(const int modelgridindex, const bool 
 
   //  int nlevels_with_processrates = 0;
   // const double T_e = modelgridindex >= 0 ? grid ::get_Te(modelgridindex) : 0.;
-  for (int element = 0; element < get_nelements(); element++) {
+  const int nelements = get_nelements();
+  for (int element = 0; element < nelements; element++) {
     const int nions = get_nions(element);
     for (int ion = 0; ion < nions; ion++) {
       globals::cellhistory[tid].cooling_contrib[kpkt::get_coolinglistoffset(element, ion)] = COOLING_UNDEFINED;
