c3f2ac9a251268c4c06a19867c5b7f08c7c0b4b9
Luke Shingles <luke.shingles@gmail.com>
2022-11-16 14:13:08 +0000

    Update input.h

--- input.h
+++ input.h
@@ -13,7 +13,7 @@ void time_init(void);
 void write_timestep_file(void);
 bool get_noncommentline(std::istream &input, std::string &line);
 
-constexpr bool lineiscommentonly(const std::string &line)
+static inline bool lineiscommentonly(const std::string &line)
 // return true for whitepace-only lines, and lines that are exclusively whitepace up to a '#' character
 {
   int searchlength = line.find('#');  // ignore anything to the right of a # character
