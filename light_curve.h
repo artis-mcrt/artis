b69eeb0bf265b2ef042a6fd99c664188e9b100a9
Luke Shingles <luke.shingles@gmail.com>
2022-12-02 11:51:31 +0000

    Revert "Use ofstream for write_light_curve()"
    This reverts commit eb52c9c9d355b7accda425954c7003e64844fd14.

--- light_curve.h
+++ light_curve.h
@@ -1,13 +1,13 @@
 #ifndef LIGHT_CURVE_H
 #define LIGHT_CURVE_H
 
-#include <string>
+#include <cstdio>
 
 #include "exspec.h"
 
 void add_to_lc_res(const struct packet *pkt_ptr, int current_abin, double *light_curve_lum, double *light_curve_lumcmf);
 
-void write_light_curve(const std::string &lc_filename, int current_abin, const double *light_curve_lum,
+void write_light_curve(const char *lc_filename, int current_abin, const double *light_curve_lum,
                        const double *light_curve_lumcmf, int numtimesteps);
 
 #endif  // LIGHT_CURVE_H
