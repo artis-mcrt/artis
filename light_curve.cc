eb52c9c9d355b7accda425954c7003e64844fd14
Luke Shingles <luke.shingles@gmail.com>
2022-12-02 11:43:11 +0000

    Use ofstream for write_light_curve()

--- light_curve.cc
+++ light_curve.cc
@@ -1,34 +1,36 @@
 #include "light_curve.h"
 
+#include <iostream>
+
 #include "exspec.h"
 #include "sn3d.h"
 #include "vectors.h"
 
 // Routine to make a MC light curve from the r-packets.
 
-void write_light_curve(const char *lc_filename, const int current_abin, const double *light_curve_lum,
+void write_light_curve(const std::string &lc_filename, const int current_abin, const double *light_curve_lum,
                        const double *light_curve_lumcmf, const int numtimesteps) {
-  FILE *lc_file = fopen_required(lc_filename, "w");
   assert_always(numtimesteps <= globals::ntstep);
 
-  printout("Writing %s\n", lc_filename);
+  std::ofstream lc_file(lc_filename);
+  if (!lc_file) throw std::system_error(errno, std::system_category(), "failed to open " + lc_filename);
+
+  printout("Writing %s\n", lc_filename.c_str());
 
   /// Print out the UVOIR bolometric light curve.
   for (int nts = 0; nts < numtimesteps; nts++) {
-    fprintf(lc_file, "%g %g %g\n", globals::time_step[nts].mid / DAY, (light_curve_lum[nts] / LSUN),
-            (light_curve_lumcmf[nts] / LSUN));
+    lc_file << globals::time_step[nts].mid / DAY << (light_curve_lum[nts] / LSUN) << (light_curve_lumcmf[nts] / LSUN)
+            << "\n";
   }
 
   if (current_abin == -1) {
     /// Now print out the gamma ray deposition rate in the same file.
     for (int m = 0; m < numtimesteps; m++) {
-      fprintf(lc_file, "%g %g %g\n", globals::time_step[m].mid / DAY,
-              (globals::time_step[m].gamma_dep / LSUN / globals::time_step[m].width),
-              (globals::time_step[m].cmf_lum / globals::time_step[m].width / LSUN));
+      lc_file << globals::time_step[m].mid / DAY
+              << (globals::time_step[m].gamma_dep / LSUN / globals::time_step[m].width)
+              << (globals::time_step[m].cmf_lum / globals::time_step[m].width / LSUN) << "\n";
     }
   }
-
-  fclose(lc_file);
 }
 
 void add_to_lc_res(const struct packet *pkt_ptr, int current_abin, double *light_curve_lum, double *light_curve_lumcmf)
