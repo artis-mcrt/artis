e2d07332342ae7cbd0159fca87e10d5f57090cca
Luke Shingles <luke.shingles@gmail.com>
2023-02-20 12:33:46 +0000

    Update artis-virgo-submit.sh

--- scripts/artis-virgo-submit.sh
+++ scripts/artis-virgo-submit.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# each AMD node has 192GB RAM and 2x64 core CPUs
-#--ntasks-per-node=48 will give 192/48=4GB per MPI rank
-#--ntasks-per-core=1 gives 1.5GB per rank (probably fine for 1D models)
-sbatch -J $(basename $(exec pwd)) --ntasks=960 --ntasks-per-node=48 --constraint=amd --partition=long --time=24:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de --no-requeue -- artis/scripts/artis-virgo-slurmjob.sh
+#--ntasks=960 --mem-per-cpu=1.5GB is probably fine for 1D models
+#--ntasks=1920 --mem-per-cpu=4GB for 3D kilonova models
+sbatch -J $(basename $(exec pwd)) --ntasks=960 --mem-per-cpu=1.5G --partition=long --time=24:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de --no-requeue -- artis/scripts/artis-virgo-slurmjob.sh
