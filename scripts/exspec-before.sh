228a5e84197fe60d09f2b6019e3f4aab21976f12
Luke Shingles <luke.shingles@gmail.com>
2023-02-22 20:04:04 +0000

    Update exspec-before.sh

--- scripts/exspec-before.sh
+++ scripts/exspec-before.sh
@@ -10,3 +10,4 @@ xz -d -T 0 packets*.out.xz || true
 
 gzip -d -v phixsdata_v2.txt.gz transitiondata.txt.gz ratecoeff_v2.dat.gz abundances.txt.gz model.txt.gz || true
 xz -d -v -T 0 phixsdata_v2.txt.xz transitiondata.txt.xz ratecoeff_v2.dat.xz abundances.txt.xz model.txt.xz || true
+`
