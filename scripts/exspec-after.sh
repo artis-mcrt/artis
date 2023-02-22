7535074c17784e9bbe63e9fcd5ca3c7eaf811c28
Luke Shingles <luke.shingles@gmail.com>
2023-02-22 11:36:16 +0000

    Update exspec-after.sh

--- scripts/exspec-after.sh
+++ scripts/exspec-after.sh
@@ -10,8 +10,8 @@ if [[ -f emission.out || -f emission.out.xz ]]; then
   mkdir packets || true
   mv packets*.out* packets/
 
-  # gzip -v --best packets/packets*.out || true
-  xz -v -T0 packets/packets*.out || true
+  # xz -v -T0 packets/packets*.out || true
+  find packets/ -name 'packets*.out' -size +1M -exec xz -v -T0 {} \;
 
   find . -name '*.out' -size +1M -exec xz -v -T0 {} \;
   find . -mindepth 2 -name 'output_*.txt' ! -name "output_0-0.txt" -size +1M -exec xz -v -T0 {} \;
