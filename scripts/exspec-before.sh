503bca3dcd4908e7b3038d79661add04636177cf
Luke Shingles <luke.shingles@gmail.com>
2023-02-23 13:14:10 +0000

    Update exspec-before.sh

--- scripts/exspec-before.sh
+++ scripts/exspec-before.sh
@@ -5,9 +5,11 @@ if [ ! -f packets00_0000.out* ]; then
   mv packets/packets*.out* .
 fi
 
-gzip -d packets*.out.gz || true
-xz -d -T 0 packets*.out.xz || true
+# gzip -d packets*.out.gz || true
+# xz -d -T 0 packets*.out.xz || true
 
-gzip -d -v phixsdata_v2.txt.gz transitiondata.txt.gz ratecoeff_v2.dat.gz abundances.txt.gz model.txt.gz || true
-xz -d -v -T 0 phixsdata_v2.txt.xz transitiondata.txt.xz ratecoeff_v2.dat.xz abundances.txt.xz model.txt.xz || true
-`
+#gzip -d -v phixsdata_v2.txt.gz transitiondata.txt.gz ratecoeff_v2.dat.gz || true
+#xz -d -v -T 0 phixsdata_v2.txt.xz transitiondata.txt.xz ratecoeff_v2.dat.xz || true
+
+find . -maxdepth 1 -name '*.gz' -size +10M -exec gzip -d -v {} \;
+find . -maxdepth 1 -name '*.xz' -size +10M -exec xz -d -v -T0 {} \;
