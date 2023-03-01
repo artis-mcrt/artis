710d0feb5f903c3dfe2c150a4430aee81ad64a98
Luke Shingles <luke.shingles@gmail.com>
2023-03-01 21:47:51 +0000

    Update artis-virgo-submit.sh

--- scripts/artis-virgo-submit.sh
+++ scripts/artis-virgo-submit.sh
@@ -3,4 +3,4 @@
 sbatch -J $(basename $(exec pwd)) --ntasks=1920 --mem-per-cpu=4096MB --partition=long --time=48:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de --no-requeue -- artis/scripts/artis-virgo-slurmjob.sh
 
 # 960 cores and 1.5GB per core for simple 1D models (maybe 3D Type Ia without full NLTE?)
-sbatch -J $(basename $(exec pwd)) --ntasks=960 --mem-per-cpu=1536M --partition=long --time=48:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de --no-requeue -- artis/scripts/artis-virgo-slurmjob.sh
+#sbatch -J $(basename $(exec pwd)) --ntasks=960 --mem-per-cpu=1536M --partition=long --time=48:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de --no-requeue -- artis/scripts/artis-virgo-slurmjob.sh
