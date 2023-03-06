97aae96428a1e7c31fa6eba0b03f97277ec32d34
Luke Shingles <luke.shingles@gmail.com>
2023-03-06 12:49:50 +0000

    Update artis-virgo-submit.sh

--- scripts/artis-virgo-submit.sh
+++ scripts/artis-virgo-submit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 # 1920 cores and 4GB per core for 3D LTE kilonova models (and maybe 3D NLTE Type Ia?)
 sbatch -J $(basename $(exec pwd)) --ntasks=1920 --mem-per-cpu=4096MB --partition=long --time=48:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de --no-requeue -- artis/scripts/artis-virgo-slurmjob.sh
 
