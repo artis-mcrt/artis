329b0230e740d7ee66f2a159543aea98c820dfe6
Luke Shingles <luke.shingles@gmail.com>
2023-04-06 10:03:02 +0100

    Add lz4 and lstd compression

--- scripts/exspec-after.sh
+++ scripts/exspec-after.sh
@@ -1,21 +1,33 @@
 #!/usr/bin/env bash
 
-if [[ -f emission.out || -f emission.out.xz ]]; then
+if [[ -f emission.out || -f emission.out.xz || -f emission.out.lz4 || -f emission.out.zst ]]; then
+
+  if command -v zstd > /dev/null; then
+    # zstd does decent compression at high speeds
+    cmdcompress="zstd -T0 -9 -v --rm -f"
+  elif command -v lz4 > /dev/null; then
+    # lz4 is extremely fast, but low compression ratios
+    cmdcompress="lz4 -v --best --rm -f"
+  else
+    # fall back to gzip
+    cmdcompress="gzip -v -f"
+  fi
 
   # join 3D direction files, if they exist
   ./artis/scripts/mergeangleres.py
 
-  xz -v absorption.out emission*.out || true
-  xz -v phixsdata*.txt transitiondata.txt ratecoeff_v2.dat linestat.out || true
   mkdir packets || true
-  mv packets*.out* packets/
+  mv packets*.out* packets/ || true
 
-  # xz -v -T0 packets/packets*.out || true
-  find packets/ -name 'packets*.out' -size +1M -exec xz -v -T0 {} \;
+  find packets/ -name 'packets*.out' -size +1M -exec $cmdcompress {} \;
 
-  find . -name '*.out' -size +1M -exec xz -v -T0 {} \;
-  find . -mindepth 2 -name 'output_*.txt' ! -name "output_0-0.txt" -size +1M -exec xz -v -T0 {} \;
+  # 3D kilonova model.txt and abundances.txt can be huge, so compress txt files
+  # do maxdepth 1 first in case job gets killed during run folder compression
+  find . -maxdepth 1 -name '*.txt' ! -name "output_0-0.txt" -size +2M -exec $cmdcompress {} \;
+  find . -maxdepth 1 -name '*.out' -size +1M -exec $cmdcompress {} \;
 
-  # 3D kilonova model.txt and abundances.txt can be huge
-  find . -maxdepth 1 -name '*.txt' -size +10M -exec xz -v -T0 {} \;
+  find . -maxdepth 2 -name '*.txt' ! -name "output_0-0.txt" -size +2M -exec $cmdcompress {} \;
+  find . -maxdepth 2 -name '*.out' -size +1M -exec $cmdcompress {} \;
+
 fi
+
