11889d6273adad0bce21205bd82e03665679a49c
Luke Shingles <luke.shingles@gmail.com>
2023-05-09 10:29:24 +0100

    Update setup_classicmode_3d.sh

--- tests/setup_classicmode_3d.sh
+++ tests/setup_classicmode_3d.sh
@@ -4,6 +4,8 @@ set -x
 
 rsync -av classicmode_3d_inputfiles/ classicmode_3d_testrun/
 
+xz -d -T0 -v classicmode_3d_testrun/*.xz
+
 if [ ! -f atomicdata_feconi.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/atomicdata_feconi.tar.xz; fi
 
 tar -xf atomicdata_feconi.tar.xz --directory classicmode_3d_testrun/
