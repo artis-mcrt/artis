d107dc9a52e7d315b121a72ca266ffb6f33fd4b4
Luke Shingles <luke.shingles@gmail.com>
2023-05-09 10:22:39 +0100

    Add classicmode 3D test

---
+++ tests/setup_classicmode_3d.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+set -x
+
+rsync -av classicmode_3d_inputfiles/ classicmode_3d_testrun/
+
+if [ ! -f atomicdata_feconi.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/atomicdata_feconi.tar.xz; fi
+
+tar -xf atomicdata_feconi.tar.xz --directory classicmode_3d_testrun/
+
+cp ../data/* classicmode_3d_testrun/
+
+cp ../artisoptions_classic.h classicmode_3d_testrun/artisoptions.h
+
+sed -i'' -e 's/constexpr int MPKTS.*/constexpr int MPKTS = 15000;/g' classicmode_3d_testrun/artisoptions.h
+
+sed -i'' -e 's/constexpr bool WRITE_PARTIAL_EMISSIONABSORPTIONSPEC.*/constexpr bool WRITE_PARTIAL_EMISSIONABSORPTIONSPEC = true;/g' classicmode_3d_testrun/artisoptions.h
+
+
+set +x
