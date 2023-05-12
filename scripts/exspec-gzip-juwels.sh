13cf6b80147c83af31d80f19cdbb6b7132443c06
Luke Shingles <luke.shingles@gmail.com>
2023-05-12 09:52:00 +0100

    Update exspec-gzip-juwels.sh

--- scripts/exspec-gzip-juwels.sh
+++ scripts/exspec-gzip-juwels.sh
@@ -2,7 +2,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --partition=batch
 ##SBATCH --account=rtsn22
 #SBATCH --account=knrt23
