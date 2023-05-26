5aa9c4f1b6fe82d71be6d2c5aeaf76b38e92a3dc
Luke Shingles <luke.shingles@gmail.com>
2023-05-26 14:47:41 +0100

    Revert "Update spectrum.cc"
    This reverts commit 85889645bf91b1ea5191e3fa1e76890b5508ff4c.

--- spectrum.cc
+++ spectrum.cc
@@ -11,7 +11,7 @@
 #include "sn3d.h"
 #include "vectors.h"
 
-constexpr bool TRACE_EMISSION_ABSORPTION_REGION_ON = false;
+bool TRACE_EMISSION_ABSORPTION_REGION_ON = false;
 
 constexpr double traceemissabs_lambdamin = 1000.;  // in Angstroms
 constexpr double traceemissabs_lambdamax = 25000.;
