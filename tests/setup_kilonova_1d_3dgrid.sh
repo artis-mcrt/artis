01586de10dfceec78e179b6f2908a445dc67b0a2
Luke Shingles <luke.shingles@gmail.com>
2023-09-12 15:53:18 +0100

    rename kilonova to kilonva_1d_3dgrid

--- tests/setup_kilonova_1d_3dgrid.sh
+++ tests/setup_kilonova_1d_3dgrid.sh
@@ -2,19 +2,21 @@
 
 set -x
 
-mkdir -p kilonova_testrun
+runfolder=kilonova_2d_2dgrid_testrun
+
+mkdir -p $runfolder
 
 if [ ! -f atomicdata_feconi.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/atomicdata_feconi.tar.xz; fi
 
-tar -xf atomicdata_feconi.tar.xz --directory kilonova_testrun/
+tar -xf atomicdata_feconi.tar.xz --directory $runfolder/
 
-rsync -av kilonova_inputfiles/ kilonova_testrun/
+rsync -av kilonova_inputfiles/ $runfolder/
 
-cp ../data/* kilonova_testrun/
+cp ../data/* $runfolder/
 
-cp ../artisoptions_kilonova_lte.h kilonova_testrun/artisoptions.h
+cp ../artisoptions_kilonova_lte.h $runfolder/artisoptions.h
 
-cd kilonova_testrun
+cd $runfolder
 
 xz -dv -T0 *.xz
 
