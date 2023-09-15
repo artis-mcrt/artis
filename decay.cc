11d6630e54eb1a9b6efaecb59aee5ba4b7bb7d72
Luke Shingles <luke.shingles@gmail.com>
2023-09-15 13:18:45 +0100

    Update decay.cc

--- decay.cc
+++ decay.cc
@@ -22,8 +22,7 @@
 
 namespace decay {
 
-constexpr int Z_MAX = 118;
-const char *elsymbols[1 + Z_MAX] = {
+constexpr std::array<const char *, 119> elsymbols = {
     "n",  "H",  "He", "Li", "Be", "B",  "C",  "N",  "O",  "F",  "Ne", "Na",  "Mg", "Al",  "Si", "P",   "S",
     "Cl", "Ar", "K",  "Ca", "Sc", "Ti", "V",  "Cr", "Mn", "Fe", "Co", "Ni",  "Cu", "Zn",  "Ga", "Ge",  "As",
     "Se", "Br", "Kr", "Rb", "Sr", "Y",  "Zr", "Nb", "Mo", "Tc", "Ru", "Rh",  "Pd", "Ag",  "Cd", "In",  "Sn",
@@ -31,6 +30,7 @@ const char *elsymbols[1 + Z_MAX] = {
     "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W",  "Re", "Os", "Ir", "Pt", "Au",  "Hg", "Tl",  "Pb", "Bi",  "Po",
     "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U",  "Np", "Pu", "Am", "Cm",  "Bk", "Cf",  "Es", "Fm",  "Md",
     "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Uut", "Fl", "Uup", "Lv", "Uus", "Uuo"};
+constexpr int Z_MAX = elsymbols.size() - 1;
 
 struct nuclide {
   int z = -1;                    // atomic number
