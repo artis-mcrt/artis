de064e76b0fda4cc145e6a9ddc66339bf23d8afe
Luke Shingles <luke.shingles@gmail.com>
2023-09-18 17:08:18 +0100

    Update vpkt.cc

--- vpkt.cc
+++ vpkt.cc
@@ -477,6 +477,11 @@ void init_vspecpol() {
     vstokes_u[p] = static_cast<struct vspecpol *>(malloc(indexmax * sizeof(struct vspecpol)));
   }
 
+  for (int m = 0; m < VMNUBINS; m++) {
+    lower_freq_vspec[m] = exp(log(VSPEC_NUMIN) + (m * (dlognu_vspec)));
+    delta_freq_vspec[m] = exp(log(VSPEC_NUMIN) + ((m + 1) * (dlognu_vspec))) - lower_freq_vspec[m];
+  }
+
   for (int ind_comb = 0; ind_comb < indexmax; ind_comb++) {
     // start by setting up the time and frequency bins.
     // it is all done interms of a logarithmic spacing in both t and nu - get the
@@ -489,11 +494,6 @@ void init_vspecpol() {
       vstokes_i[n][ind_comb].lower_time = exp(log(VSPEC_TIMEMIN) + (n * (dlogt_vspec)));
       vstokes_i[n][ind_comb].delta_t =
           exp(log(VSPEC_TIMEMIN) + ((n + 1) * (dlogt_vspec))) - vstokes_i[n][ind_comb].lower_time;
-
-      for (int m = 0; m < VMNUBINS; m++) {
-        lower_freq_vspec[m] = exp(log(VSPEC_NUMIN) + (m * (dlognu_vspec)));
-        delta_freq_vspec[m] = exp(log(VSPEC_NUMIN) + ((m + 1) * (dlognu_vspec))) - lower_freq_vspec[m];
-      }
     }
   }
 }
