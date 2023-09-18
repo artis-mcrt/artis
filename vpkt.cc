d3991650d74e25dc2b4da56798921e138c3a18b9
Luke Shingles <luke.shingles@gmail.com>
2023-09-18 12:41:15 +0100

    Update vpkt.cc

--- vpkt.cc
+++ vpkt.cc
@@ -568,7 +568,7 @@ void init_vpkt_grid() {
       vgrid_u[n][m].yvel = yvel;
       vgrid_u[n][m].zvel = zvel;
 
-      vgrid_i[n][m].flux.resize(Nrange_grid);
+      vgrid_i[n][m].flux.resize(Nrange_grid, {});
       for (int bin_range = 0; bin_range < Nrange_grid; bin_range++) {
         vgrid_i[n][m].flux[bin_range].resize(Nobs, 0.);
         vgrid_q[n][m].flux[bin_range].resize(Nobs, 0.);
