f9eec434d3590406985e852b3bfe2564cf18acc6
Luke Shingles <luke.shingles@gmail.com>
2023-09-29 14:08:50 +0100

    Fix lineiscommentonly()

--- input.h
+++ input.h
@@ -16,11 +16,11 @@ static inline bool lineiscommentonly(const std::string &line)
 // return true for whitepace-only lines, and lines that are exclusively whitepace up to a '#' character
 {
   for (size_t i = 0; i < line.length(); i++) {
-    if (line[i] != ' ') {
-      return false;
-    }
     if (line[i] == '#') {  // anything to the right of a # character doesn't count
       return true;
+    }
+    if (line[i] != ' ') {
+      return false;
     }
   }
   return true;
