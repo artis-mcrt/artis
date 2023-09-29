86baeb911ad4abc620ef87782563a6c23d0c09fe
Luke Shingles <luke.shingles@gmail.com>
2023-09-29 14:03:30 +0100

    Speed up lineiscommentonly

--- input.h
+++ input.h
@@ -15,14 +15,12 @@ bool get_noncommentline(std::fstream &input, std::string &line);
 static inline bool lineiscommentonly(const std::string &line)
 // return true for whitepace-only lines, and lines that are exclusively whitepace up to a '#' character
 {
-  int searchlength = line.find('#');  // ignore anything to the right of a # character
-  if (searchlength < 0) {
-    searchlength = line.length();
-  }
-
-  for (int i = 0; i < searchlength; i++) {
+  for (size_t i = 0; i < line.length(); i++) {
     if (line[i] != ' ') {
       return false;
+    }
+    if (line[i] == '#') {  // anything to the right of a # character doesn't count
+      return true;
     }
   }
   return true;
