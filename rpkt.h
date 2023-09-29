86e1764306dc527be0599be785b86b9ba2f745d7
Luke Shingles <luke.shingles@gmail.com>
2023-09-29 12:22:34 +0100

    Revert "Rename emitt_rpkt() -> emit_rpkt()"
    This reverts commit 234c51688ff3e6480697856592b97140b16a2b7b.

--- rpkt.h
+++ rpkt.h
@@ -5,7 +5,7 @@
 #include "constants.h"
 
 void do_rpkt(struct packet *pkt_ptr, double t2);
-void emit_rpkt((struct packet *pkt_ptr);
+void emitt_rpkt(struct packet *pkt_ptr);
 int closest_transition(double nu_cmf, int next_trans);
 double get_rpkt_escape_prob(struct packet *pkt_ptr, double tstart);
 double calculate_kappa_bf_gammacontr(int modelgridindex, double nu);
