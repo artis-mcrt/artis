cb68ad37b9a8b070cc6e7fad388381545a9f96b5
Luke Shingles <luke.shingles@gmail.com>
2023-10-07 14:22:37 +0100

    Update update_grid.cc

--- update_grid.cc
+++ update_grid.cc
@@ -1413,18 +1413,20 @@ static auto find_uppermost_ion(const int modelgridindex, const int element, cons
   }
 
   double factor = 1.;
-  for (int ion = 0; ion < uppermost_ion; ion++) {
+  int ion = 0;
+  for (ion = 0; ion < uppermost_ion; ion++) {
     factor *= nne_hi * phi(element, ion, modelgridindex);
-
+    // printout("element %d, ion %d, factor %g\n",element,i,factor);
     if (!std::isfinite(factor)) {
       printout(
           "[info] calculate_ion_balance: uppermost_ion limited by phi factors for element "
           "Z=%d, ionstage %d in "
           "cell %d\n",
           get_atomicnumber(element), get_ionstage(element, ion), modelgridindex);
-      return ion;
+      break;
     }
   }
+  uppermost_ion = ion;
   return uppermost_ion;
 }
 
