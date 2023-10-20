49dbbd3193684980b5dfca2cf41ee070f5c6994a
Luke Shingles <luke.shingles@gmail.com>
2023-10-20 22:34:35 +0200

    Update rpkt.cc

--- rpkt.cc
+++ rpkt.cc
@@ -556,6 +556,8 @@ static void update_estimators(const struct packet *pkt_ptr, const double distanc
 
   if constexpr (USE_LUT_PHOTOION || USE_LUT_BFHEATING) {
     const double distance_e_cmf_over_nu = distance_e_cmf / nu;
+    const int nelements = get_nelements();
+    const int max_nions = get_max_nions();
 
     for (int i = 0; i < globals::nbfcontinua_ground; i++) {
       const double nu_edge = globals::groundcont[i].nu_edge;
@@ -566,7 +568,7 @@ static void update_estimators(const struct packet *pkt_ptr, const double distanc
         /// the estimators
         if (grid::get_elem_abundance(modelgridindex, element) > 0) {
           const int ion = globals::groundcont[i].ion;
-          const int ionestimindex = get_ionestimindex(modelgridindex, element, ion);
+          const int ionestimindex = modelgridindex * nelements * max_nions + element * max_nions + ion;
 
           if constexpr (USE_LUT_PHOTOION) {
             safeadd(globals::gammaestimator[ionestimindex],
