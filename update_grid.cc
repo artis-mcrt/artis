9ef75f833e43c2ec36a05974c9fdcb99b77811a7
Luke Shingles <luke.shingles@gmail.com>
2023-11-15 08:38:37 +0100

    Update update_grid.cc

--- update_grid.cc
+++ update_grid.cc
@@ -653,7 +653,7 @@ static void write_to_estimators_file(FILE *estimators_file, const int mgi, const
 
   fflush(estimators_file);
 
-  const int write_estim_duration = time(nullptr) - sys_time_start_write_estimators;
+  const auto write_estim_duration = time(nullptr) - sys_time_start_write_estimators;
   if (write_estim_duration >= 1) {
     printout("writing estimators for timestep %d cell %d took %d seconds\n", timestep, mgi, write_estim_duration);
   }
