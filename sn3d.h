4bac67e7625e7a36bbd5de5c5ae22d29ba3eb526
Luke Shingles <luke.shingles@gmail.com>
2023-11-25 10:30:06 +0000

    Update sn3d.h

--- sn3d.h
+++ sn3d.h
@@ -241,6 +241,7 @@ inline void check_already_running() {
 }
 
 [[nodiscard]] inline auto get_ionestimindex(const int mgi, const int element, const int ion) -> int {
+  assert_testmodeonly(ion < get_nions(element) - 1);
   return mgi * get_includedions() + get_uniqueionindex(element, ion);
 }
 #endif  // SN3D_H
