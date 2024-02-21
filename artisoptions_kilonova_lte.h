75d19b18e97061d1e659a34dc336f0707a2ac69f
jpollin98 <joshuapollin222@gmail.com>
2024-02-21 22:08:06 +0000

    bfest

--- artisoptions_kilonova_lte.h
+++ artisoptions_kilonova_lte.h
@@ -82,7 +82,7 @@ constexpr bool LEVEL_HAS_BFEST(int element_z, int ionstage, int level) {
     }
     return (level <= 80);
   } else {
-    return true;  // Always treat (element, ion, level) with bf estimator
+    return true;
   }
 }
 
