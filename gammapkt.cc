1eafb08a4df9aa4a26bd59dd84d40b63529b6814
Gerrit Leck <gleck@lxbuild07.gsi.de>
2024-03-01 16:24:40 +0100

    Fix XCOM file not found

--- gammapkt.cc
+++ gammapkt.cc
@@ -213,7 +213,7 @@ void init_photoion_data() {
   for (int Z = 0; Z < numb_xcom_elements; Z++) {
     photoion_data[Z].reserve(100);
   }
-  const std::string filepath{"xcom_photoion_data.txt"};
+  const std::string filepath{"data/xcom_photoion_data.txt"};
   assert_always(std::filesystem::exists(filepath));
 
   std::ifstream data_fs(filepath);
