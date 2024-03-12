0ebbe19d4487d9aa16479c2c7a720b6e9d3331b9
Luke Shingles <luke.shingles@gmail.com>
2024-03-12 12:56:03 +0000

    Update jobscripts

--- scripts/artis-juwels.sh
+++ scripts/artis-juwels.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 #SBATCH --ntasks=960
-##SBATCH --ntasks=1920
-#SBATCH --ntasks-per-node=48
+#SBATCH --nodes=20
+#SBATCH --exclusive
 #SBATCH --time=24:00:00
 #SBATCH --partition=batch
 ##SBATCH --partition=mem192
@@ -10,11 +10,11 @@
 #SBATCH --mail-type=ALL
 ##SBATCH --mail-user=luke.shingles@gmail.com
 
-module load Stages/2024 GCC ParaStationMPI GSL
+module load ParaStationMPI
+module load GSL
+module load UCX-settings/RC
 
 module list
-
-export SLURM_HINT=nomultithread
 
 cd $SLURM_SUBMIT_DIR
 
@@ -22,7 +22,7 @@ echo "CPU type: $(c++ -march=native -Q --help=target | grep -- '-march=  ' | cut
 
 hoursleft=$(python3 ./artis/scripts/slurmjobhoursleft.py ${SLURM_JOB_ID})
 echo "$(date): before srun sn3d. hours left: $hoursleft"
-time srun --cpus-per-task=1 --threads-per-core=1 --cpu-bind=rank_ldom --hint=nomultithread -- ./sn3d -w $hoursleft > out.txt
+time srun --mpi=pspmix --tasks-per-node=48 --threads-per-core=1 -- ./sn3d -w $hoursleft > out.txt
 hoursleftafter=$(python3 ./artis/scripts/slurmjobhoursleft.py ${SLURM_JOB_ID})
 echo "$(date): after srun sn3d finished. hours left: $hoursleftafter"
 hourselapsed=$(python3 -c "print($hoursleft - $hoursleftafter)")
