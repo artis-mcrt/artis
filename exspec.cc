2cfe5c0e109150cc4d61b47cc9ae617ff76d4d4b
Luke Shingles <luke.shingles@gmail.com>
2024-03-12 16:07:34 +0000

    Condense node rank log messages

--- exspec.cc
+++ exspec.cc
@@ -205,9 +205,8 @@ auto main(int argc, char *argv[]) -> int {
   printout("process id (pid): %d\n", getpid());
   printout("MPI enabled:\n");
   printout("  rank %d of [0..%d] in MPI_COMM_WORLD\n", globals::rank_global, globals::nprocs - 1);
-  printout("  node %d of [0..%d]\n", globals::node_id, globals::node_count - 1);
-  printout("  rank %d of [0..%d] within this node (MPI_COMM_WORLD_SHARED)\n", globals::rank_in_node,
-           globals::node_nprocs - 1);
+  printout("  rank %d of [0..%d] in MPI_COMM_WORLD_SHARED on node %d of [0..%d]\n", globals::rank_in_node,
+           globals::node_nprocs - 1, globals::node_id, globals::node_count - 1);
 #else
   printout("MPI is disabled in this build\n");
 #endif
