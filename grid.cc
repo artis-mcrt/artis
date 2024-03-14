a767c1a90ce340dd5c9c3e61c9a38458864003fc
Luke Shingles <luke.shingles@gmail.com>
2024-03-14 15:48:55 +0000

    Update grid.cc

--- grid.cc
+++ grid.cc
@@ -1743,10 +1743,6 @@ static void read_grid_restart_data(const int timestep) {
   assert_always(fscanf(gridsave_file, "%d ", &nprocs_in) == 1);
   assert_always(nprocs_in == globals::nprocs);
 
-  int nthreads_in = -1;
-  assert_always(fscanf(gridsave_file, "%d ", &nthreads_in) == 1);
-  assert_always(nthreads_in == get_max_threads());
-
   for (int nts = 0; nts < globals::ntimesteps; nts++) {
     int pellet_decays = 0.;
     assert_always(fscanf(gridsave_file, "%la %la %la %la %la %la %la %la %la %la %la %la %la %la %la %d ",
@@ -1824,7 +1820,6 @@ void write_grid_restart_data(const int timestep) {
 
   fprintf(gridsave_file, "%d ", globals::ntimesteps);
   fprintf(gridsave_file, "%d ", globals::nprocs);
-  fprintf(gridsave_file, "%d ", get_max_threads());
 
   for (int nts = 0; nts < globals::ntimesteps; nts++) {
     fprintf(gridsave_file, "%la %la %la %la %la %la %la %la %la %la %la %la %la %la %la %d ",
