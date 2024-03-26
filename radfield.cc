6b246b4f19723ef1a5fb9cd2c57955cc632c527e
Luke Shingles <luke.shingles@gmail.com>
2024-03-26 09:40:08 +0000

    Update radfield.cc

--- radfield.cc
+++ radfield.cc
@@ -1068,9 +1068,10 @@ auto get_bfrate_estimator(const int element, const int lowerion, const int lower
   if constexpr (DETAILED_BF_ESTIMATORS_ON) {
     const int allcontindex = get_bfcontindex(element, lowerion, lower, phixstargetindex);
     if (allcontindex >= 0) {
-      if (globals::allcont[allcontindex].bfestimindex >= 0) {
+      const auto bfestimindex = globals::allcont[allcontindex].bfestimindex;
+      if (bfestimindex >= 0) {
         const ptrdiff_t nonemptymgi = grid::get_modelcell_nonemptymgi(modelgridindex);
-        return prev_bfrate_normed[nonemptymgi * globals::bfestimcount + allcontindex];
+        return prev_bfrate_normed[nonemptymgi * globals::bfestimcount + bfestimindex];
       }
     }
   }
