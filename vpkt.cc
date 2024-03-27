b636706ffd47b214e7c308c8441df234387a37c1
Luke Shingles <luke.shingles@gmail.com>
2024-03-27 17:09:07 +0000

    make rot_angle() constexpr

--- vpkt.cc
+++ vpkt.cc
@@ -955,51 +955,6 @@ auto vpkt_call_estimators(Packet &pkt, const enum packet_type type_before_rpkt) 
   }
 }
 
-[[nodiscard]] auto rot_angle(std::span<const double, 3> n1, std::span<const double, 3> n2, std::span<double, 3> ref1,
-                             std::span<double, 3> ref2) -> double {
-  // Rotation angle from the scattering plane
-  // We need to rotate Stokes Parameters to (or from) the scattering plane from (or to)
-  // the meridian frame such that Q=1 is in the scattering plane and along ref1
-
-  // ref1_sc is the ref1 axis in the scattering plane ref1 = n1 x ( n1 x n2 )
-  const double n1_dot_n2 = dot(n1, n2);
-  auto ref1_sc = std::array<double, 3>{n1[0] * n1_dot_n2 - n2[0], n1[1] * n1_dot_n2 - n2[1], n1[2] * n1_dot_n2 - n2[2]};
-  ref1_sc = vec_norm(ref1_sc);
-
-  double cos_stokes_rot_1 = dot(ref1_sc, ref1);
-  const double cos_stokes_rot_2 = dot(ref1_sc, ref2);
-
-  if (cos_stokes_rot_1 < -1) {
-    cos_stokes_rot_1 = -1;
-  }
-  if (cos_stokes_rot_1 > 1) {
-    cos_stokes_rot_1 = 1;
-  }
-
-  double i = 0;
-  if ((cos_stokes_rot_1 > 0) && (cos_stokes_rot_2 > 0)) {
-    i = acos(cos_stokes_rot_1);
-  } else if ((cos_stokes_rot_1 < 0) && (cos_stokes_rot_2 > 0)) {
-    i = PI - acos(fabs(cos_stokes_rot_1));
-  } else if ((cos_stokes_rot_1 > 0) && (cos_stokes_rot_2 < 0)) {
-    i = 2 * PI - acos(cos_stokes_rot_1);
-  } else if ((cos_stokes_rot_1 < 0) && (cos_stokes_rot_2 < 0)) {
-    i = PI + acos(fabs(cos_stokes_rot_1));
-  }
-  if (cos_stokes_rot_1 == 0) {
-    i = PI / 2.;
-  }
-  if (cos_stokes_rot_2 == 0) {
-    i = 0.;
-  }
-
-  if (!std::isfinite(i)) {
-    printout("Warning NaN: %3.6f \t %3.6f \t %3.6f \n", cos_stokes_rot_1, cos_stokes_rot_2, acos(cos_stokes_rot_1));
-  }
-
-  return i;
-}
-
 // Routine to compute the meridian frame axes ref1 and ref2
 [[nodiscard]] auto meridian(std::span<const double, 3> n, std::span<double, 3> ref1) -> std::array<double, 3> {
   // for ref_1 use (from triple product rule)
--- vpkt.h
+++ vpkt.h
@@ -2,14 +2,15 @@
 #ifndef VPKT_H
 #define VPKT_H
 
+#include <cmath>
 #include <span>
 
 #include "artisoptions.h"
 #include "constants.h"
 #include "packet.h"
+#include "sn3d.h"
+#include "vectors.h"
 
-[[nodiscard]] auto rot_angle(std::span<const double, 3> n1, std::span<const double, 3> n2, std::span<double, 3> ref1,
-                             std::span<double, 3> ref2) -> double;
 [[nodiscard]] auto meridian(std::span<const double, 3> n, std::span<double, 3> ref1) -> std::array<double, 3>;
 auto frame_transform(std::span<const double, 3> n_rf, double *Q, double *U,
                      std::span<const double, 3> v) -> std::array<double, 3>;
@@ -43,5 +44,48 @@ inline int nvpkt_esc2{0};  // kpkt deactivation
 inline int nvpkt_esc3{0};  // macroatom deactivation
 
 inline double cell_is_optically_thick_vpkt;
+
+[[nodiscard]] constexpr auto rot_angle(std::span<const double, 3> n1, std::span<const double, 3> n2,
+                                       std::span<double, 3> ref1, std::span<double, 3> ref2) -> double {
+  // Rotation angle from the scattering plane
+  // We need to rotate Stokes Parameters to (or from) the scattering plane from (or to)
+  // the meridian frame such that Q=1 is in the scattering plane and along ref1
+
+  // ref1_sc is the ref1 axis in the scattering plane ref1 = n1 x ( n1 x n2 )
+  const double n1_dot_n2 = dot(n1, n2);
+  auto ref1_sc = std::array<double, 3>{n1[0] * n1_dot_n2 - n2[0], n1[1] * n1_dot_n2 - n2[1], n1[2] * n1_dot_n2 - n2[2]};
+  ref1_sc = vec_norm(ref1_sc);
+
+  double cos_stokes_rot_1 = dot(ref1_sc, ref1);
+  const double cos_stokes_rot_2 = dot(ref1_sc, ref2);
+
+  if (cos_stokes_rot_1 < -1) {
+    cos_stokes_rot_1 = -1;
+  }
+  if (cos_stokes_rot_1 > 1) {
+    cos_stokes_rot_1 = 1;
+  }
+
+  double i = 0;
+  if ((cos_stokes_rot_1 > 0) && (cos_stokes_rot_2 > 0)) {
+    i = acos(cos_stokes_rot_1);
+  } else if ((cos_stokes_rot_1 < 0) && (cos_stokes_rot_2 > 0)) {
+    i = PI - acos(fabs(cos_stokes_rot_1));
+  } else if ((cos_stokes_rot_1 > 0) && (cos_stokes_rot_2 < 0)) {
+    i = 2 * PI - acos(cos_stokes_rot_1);
+  } else if ((cos_stokes_rot_1 < 0) && (cos_stokes_rot_2 < 0)) {
+    i = PI + acos(fabs(cos_stokes_rot_1));
+  }
+  if (cos_stokes_rot_1 == 0) {
+    i = PI / 2.;
+  }
+  if (cos_stokes_rot_2 == 0) {
+    i = 0.;
+  }
+
+  assert_always(std::isfinite(i));
+
+  return i;
+}
 
 #endif  // VPKT_H
