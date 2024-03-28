e0e909799844175e559439bf21c41709157d7d16
Luke Shingles <luke.shingles@gmail.com>
2024-03-28 09:33:26 +0000

    Update macroatom.cc

--- macroatom.cc
+++ macroatom.cc
@@ -420,7 +420,7 @@ void do_macroatom(Packet &pkt, const MacroAtomState &pktmastate)
     const double randomrate = rng_uniform() * cumulative_transitions[MA_ACTION_COUNT - 1];
 
     // first cumulative_transitions[i] such that cumulative_transitions[i] > randomrate
-    const int selected_action =
+    const auto selected_action =
         std::distance(cumulative_transitions.cbegin(),
                       std::upper_bound(cumulative_transitions.cbegin(), cumulative_transitions.cend(), randomrate));
 
@@ -642,7 +642,7 @@ void do_macroatom(Packet &pkt, const MacroAtomState &pktmastate)
 
       default:
         if constexpr (TESTMODE) {
-          printout("ERROR: Unknown macroatom selected_action type %d\n", selected_action);
+          printout("ERROR: Unknown macroatom selected_action type %ld\n", selected_action);
           assert_testmodeonly(false);
         } else {
           __builtin_unreachable();
