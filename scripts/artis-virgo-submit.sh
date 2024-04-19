2c241bfcbcaa5b7af041f0f6c257c43d7692b9ad
Luke Shingles <luke.shingles@gmail.com>
2024-04-19 05:45:17 +0100

    Enable APPTAINER_SHARENS

--- scripts/artis-virgo-submit.sh
+++ scripts/artis-virgo-submit.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -x
+export APPTAINER_SHARENS=true
+export APPTAINER_CONFIGDIR=/tmp/$USER
 sbatch -J $(basename $(exec pwd)) --ntasks=1920 --ntasks-per-node=128 --mem-per-cpu=2000MB --partition=long --time=24:00:00 --constraint=amd,epyc,7713 --mail-type=ALL --mail-user=${USER}@gsi.de --no-requeue -- artis/scripts/artis-virgo-slurmjob.sh
