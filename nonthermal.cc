7978eea638d49c790fa7153e0c9487141f8fcaab
jpollin98 <joshuapollin222@gmail.com>
2024-05-06 16:44:52 +0100

    Update nonthermal.cc

--- nonthermal.cc
+++ nonthermal.cc
@@ -2725,11 +2725,11 @@ void nt_MPI_Bcast(const int modelgridindex, const int root) {
 
     for (size_t excitationindex = 0; excitationindex < frac_excitations_list_size; excitationindex++) {
       MPI_Bcast(&nt_solution[modelgridindex].frac_excitations_list[excitationindex].frac_deposition, 1, MPI_DOUBLE,
-                root, MPI_COMM_WORLD);
+                root, globals::mpi_comm_internode);
       MPI_Bcast(&nt_solution[modelgridindex].frac_excitations_list[excitationindex].ratecoeffperdeposition, 1,
-                MPI_DOUBLE, root, MPI_COMM_WORLD);
+                MPI_DOUBLE, root, globals::mpi_comm_internode);
       MPI_Bcast(&nt_solution[modelgridindex].frac_excitations_list[excitationindex].lineindex, 1, MPI_INT, root,
-                MPI_COMM_WORLD);
+                globals::mpi_comm_internode);
     }
 
     if (STORE_NT_SPECTRUM) {
