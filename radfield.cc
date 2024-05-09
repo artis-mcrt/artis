855a7dfca66c0bd234bd7b1724d8429c58f3779c
jpollin98 <joshuapollin222@gmail.com>
2024-05-09 09:51:10 +0100

    Update radfield.cc

--- radfield.cc
+++ radfield.cc
@@ -1149,9 +1149,8 @@ void reduce_estimators()
 
   if constexpr (DETAILED_BF_ESTIMATORS_ON) {
     for (ptrdiff_t nonemptymgi = 0; nonemptymgi < nonempty_npts_model; nonemptymgi++) {
-      MPI_Reduce(globals::rank_in_node == 0 ? MPI_IN_PLACE : &bfrate_raw[nonemptymgi * globals::bfestimcount],
-                 &bfrate_raw[nonemptymgi * globals::bfestimcount], globals::bfestimcount, MPI_DOUBLE, MPI_SUM, 0,
-                 globals::mpi_comm_node);
+      MPI_Allreduce(MPI_IN_PLACE, &bfrate_raw[nonemptymgi * globals::bfestimcount], globals::bfestimcount, MPI_DOUBLE,
+                    MPI_SUM, globals::mpi_comm_node);
     }
 
     if (globals::rank_in_node == 0) {
