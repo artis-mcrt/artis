997fb86a5c79dab3abd4aefc5a3d1f97f64187da
Luke Shingles <luke.shingles@gmail.com>
2024-05-15 11:26:36 +0200

    Fix clang-formatting

--- grid.cc
+++ grid.cc
@@ -2024,7 +2024,7 @@ static void setup_grid_cartesian_3d()
   // have a higher expansion velocity than the sides
   const double vmax_corner = sqrt(3 * pow(globals::vmax, 2));
   printout("corner vmax %g [cm/s] (%.2fc)\n", vmax_corner, vmax_corner / CLIGHT);
-  if (!FORCE_SPHERICAL_ESCAPE_SURFACE){
+  if (!FORCE_SPHERICAL_ESCAPE_SURFACE) {
     assert_always(vmax_corner < CLIGHT);
   }
 
