067de004b4d231ec8875fae3e200af57fb11e3be
jpollin98 <152508849+jpollin98@users.noreply.github.com>
2024-05-29 09:40:12 +0100

    Normalise bound-free estimators on each rank independently to eliminate MPI_Bcast and skip NLTE solver if Te=MINTEMP (#54)
    **Normalise bound-free estimators independently on all ranks**
    The normalisation of the bf estimators has been changed to occur across
    all cells instead of the rank-assigned cells. This means that we no
    longer need to broadcast the prev_bfratenormed array.
    
    **Skip NLTE solver if Te=MINTEMP:**
    The temperature check was added due to populations in some grid cells
    with a low temperature, as populations being unphysical (i.e producing
    nne outsude of the limits of a float). These grid cells typically had a
    lower amount of 56Ni.

--- radfield.cc
+++ radfield.cc
@@ -1040,14 +1040,23 @@ void normalise_J(const int modelgridindex, const double estimator_normfactor_ove
   }
 }
 
-void normalise_bf_estimators(const int modelgridindex, const ptrdiff_t nonemptymgi,
-                             const double estimator_normfactor_over_H) {
-  if constexpr (DETAILED_BF_ESTIMATORS_ON) {
-    printout("normalise_bf_estimators for cell %d with factor %g\n", modelgridindex, estimator_normfactor_over_H);
-    assert_always(nonemptymgi >= 0);
-    for (int i = 0; i < globals::bfestimcount; i++) {
-      const auto mgibfindex = nonemptymgi * globals::bfestimcount + i;
-      prev_bfrate_normed[mgibfindex] = bfrate_raw[mgibfindex] * estimator_normfactor_over_H;
+void normalise_bf_estimators(const int nts, const int nts_prev, const int titer, double deltat) {
+  if (nts != globals::timestep_initial || titer != 0) {
+    for (int mgi = 0; mgi < grid::get_npts_model(); mgi++) {
+      const ptrdiff_t nonemptymgi = grid::get_modelcell_nonemptymgi(mgi);
+      if (!globals::lte_iteration && grid::modelgrid[mgi].thick != 1) {
+        int assoc_cells = grid::get_numassociatedcells(mgi);
+        if (assoc_cells > 0) {
+          double deltaV =
+              grid::get_modelcell_assocvolume_tmin(mgi) * pow(globals::timesteps[nts_prev].mid / globals::tmin, 3);
+          double estimator_normfactor = 1 / deltaV / deltat / globals::nprocs;
+          assert_always(nonemptymgi >= 0);
+          for (int i = 0; i < globals::bfestimcount; i++) {
+            const auto mgibfindex = nonemptymgi * globals::bfestimcount + i;
+            prev_bfrate_normed[mgibfindex] = bfrate_raw[mgibfindex] * (estimator_normfactor / H);
+          }
+        }
+      }
     }
   }
 }
@@ -1200,13 +1209,6 @@ void do_MPI_Bcast(const int modelgridindex, const int root, int root_node_id)
         MPI_Bcast(&radfieldbin_solutions[mgibinindex].W, 1, MPI_FLOAT, root_node_id, globals::mpi_comm_internode);
         MPI_Bcast(&radfieldbin_solutions[mgibinindex].T_R, 1, MPI_FLOAT, root_node_id, globals::mpi_comm_internode);
       }
-    }
-  }
-
-  if constexpr (DETAILED_BF_ESTIMATORS_ON) {
-    if (globals::rank_in_node == 0) {
-      MPI_Bcast(&prev_bfrate_normed[nonemptymgi * globals::bfestimcount], globals::bfestimcount, MPI_FLOAT,
-                root_node_id, globals::mpi_comm_internode);
     }
   }
 
