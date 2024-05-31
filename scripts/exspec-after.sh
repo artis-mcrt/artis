51911a07e5528b2018453a3f4c8a64bb5f08b9a3
Luke Shingles <luke.shingles@gmail.com>
2024-05-31 08:30:15 +0100

    Enable estimators conversion to parquet

--- scripts/exspec-after.sh
+++ scripts/exspec-after.sh
@@ -46,8 +46,8 @@ if [ -f emission.out* ]; then
   # convert virtual packets to parquet
   artistools lc --frompackets -plotvspecpol 0 || true
 
-  # convert estimators to parquet. commented because python multiprocessing hangs on JUWELS
-  #python3 -c 'import artistools as at; at.estimators.scan_estimators()' || true
+  # convert estimators to parquet. On JUWELS, you might need to limit the number of processes to 16 in artistools/artistools/configuration.py
+  python3 -c 'import artistools as at; at.estimators.scan_estimators()' || true
 
 fi
 
