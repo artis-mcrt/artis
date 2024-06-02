560b09da24218e7e5d271cdaf7df502eec6de6b3
Luke Shingles <luke.shingles@gmail.com>
2024-06-03 00:01:02 +0100

    Update rpkt.cc

--- rpkt.cc
+++ rpkt.cc
@@ -121,10 +121,10 @@ auto closest_transition(const double nu_cmf, const int next_trans) -> int
 
   // will find the highest frequency (lowest index) line with nu_line <= nu_cmf
   // lower_bound matches the first element where the comparison function is false
-  const auto *matchline =
-      std::lower_bound(globals::linelist, globals::linelist + globals::nlines, nu_cmf,
-                       [](const auto &line, const double nu_cmf) -> bool { return line.nu > nu_cmf; });
-  const int matchindex = std::distance(globals::linelist, matchline);
+  const int matchindex =
+      std::distance(globals::linelist,
+                    std::lower_bound(globals::linelist, globals::linelist + globals::nlines, nu_cmf,
+                                     [](const auto &line, const double nu_cmf) -> bool { return line.nu > nu_cmf; }));
   if (matchindex >= globals::nlines) [[unlikely]] {
     return -1;
   }
