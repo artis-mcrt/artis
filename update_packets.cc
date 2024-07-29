962bc78e4287662450ffa503a7e74f4b2ac657d4
Luke Shingles <luke.shingles@gmail.com>
2024-07-29 13:34:55 +0100

    Update update_packets.cc

--- update_packets.cc
+++ update_packets.cc
@@ -218,7 +218,7 @@ void do_packet(Packet &pkt, const double t2, const int nts)
     }
 
     case TYPE_RPKT: {
-      do_rpkt(pkt, t2);
+      // do_rpkt(pkt, t2);
 
       if (pkt.type == TYPE_ESCAPE) {
         atomicadd(globals::timesteps[nts].cmf_lum, pkt.e_cmf);
