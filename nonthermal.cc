42e764e1f4c7f710effde8677e1a8271509a159b
Luke Shingles <luke.shingles@gmail.com>
2024-08-07 12:04:31 +0100

    Update nonthermal.cc

--- nonthermal.cc
+++ nonthermal.cc
@@ -1574,7 +1574,7 @@ void analyse_sf_solution(const int modelgridindex, const int timestep, const boo
     // sort by descending frac_deposition
     std::sort(EXEC_PAR_UNSEQ nt_solution[modelgridindex].frac_excitations_list.begin(),
               nt_solution[modelgridindex].frac_excitations_list.end(),
-              [](const auto &a, const auto &b) { return static_cast<bool>(a.frac_deposition > b.frac_deposition); });
+              [](const auto &a, const auto &b) { return a.frac_deposition > b.frac_deposition; });
 
     // the excitation list is now sorted by frac_deposition descending
     const double deposition_rate_density = get_deposition_rate_density(modelgridindex);
@@ -1632,7 +1632,6 @@ void analyse_sf_solution(const int modelgridindex, const int timestep, const boo
 
     // sort the excitation list by ascending lineindex for fast lookup with a binary search
     std::ranges::sort(EXEC_PAR_UNSEQ nt_solution[modelgridindex].frac_excitations_list,
-
                       [](const auto &a, const auto &b) { return static_cast<bool>(a.lineindex < b.lineindex); });
 
   }  // NT_EXCITATION_ON
