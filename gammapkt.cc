c3aa6264ad7486c041570b44fb1764809914125f
Luke Shingles <luke.shingles@gmail.com>
2024-08-07 11:46:20 +0100

    Update gammapkt.cc

--- gammapkt.cc
+++ gammapkt.cc
@@ -49,20 +49,6 @@ struct NucGammaLine {
   int nucindex;       // is it a Ni56, Co56, a fake line, etc
   int nucgammaindex;  // which of the lines of that nuclide is it
   double energy;      // in erg
-
-  auto operator<(const NucGammaLine &g2) const -> bool {
-    // true if d1 < d2
-    if (energy < g2.energy) {
-      return true;
-    }
-    if (energy == g2.energy && nucindex < g2.nucindex) {
-      return true;
-    }
-    if (energy == g2.energy && nucindex == g2.nucindex && nucgammaindex < g2.nucgammaindex) {
-      return true;
-    }
-    return false;
-  }
 };
 
 static std::vector<NucGammaLine> allnuc_gamma_line_list;
@@ -177,8 +163,7 @@ static void init_gamma_linelist() {
   }
   printout("total gamma-ray lines %td\n", total_lines);
 
-  allnuc_gamma_line_list = std::vector<NucGammaLine>();
-  allnuc_gamma_line_list.reserve(total_lines);
+  allnuc_gamma_line_list = std::vector<NucGammaLine>(total_lines);
 
   for (int nucindex = 0; nucindex < decay::get_num_nuclides(); nucindex++) {
     for (std::ptrdiff_t j = 0; j < std::ssize(gamma_spectra[nucindex]); j++) {
@@ -188,7 +173,18 @@ static void init_gamma_linelist() {
   }
   allnuc_gamma_line_list.shrink_to_fit();
   assert_always(static_cast<int>(allnuc_gamma_line_list.size()) == total_lines);
-  std::sort(allnuc_gamma_line_list.begin(), allnuc_gamma_line_list.end());
+  std::ranges::sort(allnuc_gamma_line_list, [](const NucGammaLine &lhs, const NucGammaLine &rhs) {
+    if (rhs.energy < lhs.energy) {
+      return true;
+    }
+    if (rhs.energy == lhs.energy && rhs.nucindex < lhs.nucindex) {
+      return true;
+    }
+    if (rhs.energy == lhs.energy && rhs.nucindex == lhs.nucindex && rhs.nucgammaindex < lhs.nucgammaindex) {
+      return true;
+    }
+    return false;
+  });
 
   FILE *const line_list = fopen_required("gammalinelist.out", "w");
 
