1c8281670d6fdff0288ab8e801b70ccf9caa5a5d
Luke Shingles <luke.shingles@gmail.com>
2024-08-24 07:19:17 +0100

    Update atomic.h

--- atomic.h
+++ atomic.h
@@ -414,6 +414,9 @@ inline void set_nuptrans(const int element, const int ion, const int level, cons
     }
   }
   assert_testmodeonly(false);
+  if constexpr (!TESTMODE) {
+    __builtin_unreachable();
+  }
   return -1;
 }
 
