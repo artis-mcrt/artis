989085a8003e196b87e7073b6f59b582cf5ac545
Luke Shingles <luke.shingles@gmail.com>
2024-09-08 17:04:40 +0100

    Update macroatom.cc

--- macroatom.cc
+++ macroatom.cc
@@ -655,7 +655,7 @@ void macroatom_close_file() {
 auto rad_deexcitation_ratecoeff(const int modelgridindex, const int element, const int ion, const int upper,
                                 const int lower, const double epsilon_trans, const float A_ul,
                                 const double upperstatweight, const double t_current) -> double {
-  assert_always(upper > lower);
+  assert_testmodeonly(upper > lower);
 
   const double n_u = get_levelpop(modelgridindex, element, ion, upper);
   const double n_l = get_levelpop(modelgridindex, element, ion, lower);
