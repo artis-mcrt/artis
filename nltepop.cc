9f168ae465c7c1481784207beb151a2dc663caad
Luke Shingles <luke.shingles@gmail.com>
2024-10-30 11:36:55 +0000

    Update nltepop.cc

--- nltepop.cc
+++ nltepop.cc
@@ -1265,6 +1265,7 @@ void nltepop_read_restart_data(FILE *restart_file) {
 }
 
 auto get_nlte_levelpop_over_rho(int modelgridindex, int element, int ion, int level) -> double {
+  assert_testmodeonly(level <= get_nlevels_nlte(element, ion));
   return grid::modelgrid[modelgridindex].nlte_pops[globals::elements[element].ions[ion].first_nlte + level - 1];
 }
 
@@ -1275,6 +1276,7 @@ auto get_nlte_superlevelpop_over_rho(int modelgridindex, int element, int ion) -
 }
 
 void set_nlte_levelpop_over_rho(int modelgridindex, int element, int ion, int level, double value) {
+  assert_testmodeonly(level <= get_nlevels_nlte(element, ion));
   grid::modelgrid[modelgridindex].nlte_pops[globals::elements[element].ions[ion].first_nlte + level - 1] = value;
 }
 
