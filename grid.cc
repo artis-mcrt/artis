19c050f3bd84cc7d6a8312aa9d08223e6dd5aec2
Luke Shingles <luke.shingles@gmail.com>
2024-10-31 12:07:05 +0000

    Update grid.cc

--- grid.cc
+++ grid.cc
@@ -119,7 +119,7 @@ void set_npts_model(const int new_npts_model) {
   npts_model = new_npts_model;
 
   assert_always(modelgrid.data() == nullptr);
-  modelgrid = std::span(static_cast<ModelGridCell *>(calloc(npts_model + 1, sizeof(ModelGridCell))), npts_model + 1);
+  modelgrid = std::span(static_cast<ModelGridCell *>(malloc((npts_model + 1) * sizeof(ModelGridCell))), npts_model + 1);
   assert_always(modelgrid.data() != nullptr);
   mg_associated_cells.resize(npts_model + 1, 0);
   nonemptymgi_of_mgi.resize(npts_model + 1, -1);
