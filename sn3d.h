1f0662b12c58ae9e84e4452200b31983c2380bb1
Luke Shingles <luke.shingles@gmail.com>
2024-11-01 21:01:37 +0000

    Update sn3d.h

--- sn3d.h
+++ sn3d.h
@@ -137,7 +137,6 @@ __attribute__((__format__(__printf__, 1, 2))) inline auto printout(const char *f
       }                                                                                                                \
       std::cerr << "\n[rank " << globals::my_rank << "] " << __FILE__ << ":" << __LINE__ << ": failed assertion `"     \
                 << #e << "` in function " << __PRETTY_FUNCTION__ << "\n";                                              \
-      std::abort();                                                                                                    \
     }                                                                                                                  \
     assert(assertpass);                                                                                                \
   }
