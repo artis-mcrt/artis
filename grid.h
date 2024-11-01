59329fe8b87660ceb1f4be5d108c034d33b90d41
Luke Shingles <luke.shingles@gmail.com>
2024-11-01 10:29:58 +0000

    Update grid.h

--- grid.h
+++ grid.h
@@ -147,10 +147,10 @@ inline void change_cell(Packet &pkt, const int snext)
 
 inline auto get_ejecta_kinetic_energy() {
   double E_kin = 0.;
-  for (int nonemptymgi = 0; nonemptymgi < grid::get_nonempty_npts_model(); nonemptymgi++) {
-    const int mgi = grid::get_mgi_of_nonemptymgi(nonemptymgi);
-    const int assoc_cells = grid::get_numpropcells(mgi);
-    double M_cell = grid::get_rho_tmin(mgi) * grid::get_modelcell_assocvolume_tmin(mgi);
+  for (int nonemptymgi = 0; nonemptymgi < get_nonempty_npts_model(); nonemptymgi++) {
+    const int mgi = get_mgi_of_nonemptymgi(nonemptymgi);
+    const int assoc_cells = get_numpropcells(mgi);
+    double M_cell = get_rho_tmin(mgi) * grid::get_modelcell_assocvolume_tmin(mgi);
     const double radial_pos = get_initial_radial_pos_sum(mgi) / assoc_cells;
     E_kin += 0.5 * M_cell * std::pow(radial_pos / globals::tmin, 2);
   }
