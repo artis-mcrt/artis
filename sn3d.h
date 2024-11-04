2cfc9b10357d21047fcf9ffd21ba7adac1ef832f
Luke Shingles <luke.shingles@gmail.com>
2024-11-04 09:44:59 +0000

    Update sn3d.h

--- sn3d.h
+++ sn3d.h
@@ -134,6 +134,7 @@ __attribute__((__format__(__printf__, 1, 2))) inline auto printout(const char *f
     }                                                                                                                  \
     assert(assertpass);                                                                                                \
   }
+
 #endif
 
 #define assert_always(e) __artis_assert(e)
