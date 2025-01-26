9ab7000f2ced425c889941fda943e3e9b444a477
Luke Shingles <luke.shingles@gmail.com>
2025-01-26 09:59:15 +0100

    JUWELS scripts: Use Stages/2025

--- scripts/artis-juwels.sh
+++ scripts/artis-juwels.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=ALL
 ##SBATCH --mail-user=luke.shingles@gmail.com
 
-module load Stages/2024 GCC ParaStationMPI GSL
+module load Stages/2025 GCC ParaStationMPI GSL
 module load UCX-settings/plain
 
 module list
