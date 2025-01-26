b149ee11a803431a3afc387bb48028b8b1317cd2
Luke Shingles <luke.shingles@gmail.com>
2025-01-26 09:59:34 +0100

    JUWELS scripts: Use Stages/2025 (#164)

--- scripts/exspec-gzip-juwels.sh
+++ scripts/exspec-gzip-juwels.sh
@@ -9,15 +9,15 @@
 #SBATCH --mail-type=ALL
 ##SBATCH --mail-user=luke.shingles@gmail.com
 
-module load Stages/2024 ParaStationMPI GSL zstd/.1.5.5
+module load Stages/2025 ParaStationMPI GSL
 
 cd $SLURM_SUBMIT_DIR
 
-if [ ! -f emission.out.zst ]; then
+if [[ -f emission.out || -f emission.out.zst ]]; then
   ./artis/scripts/exspec-before.sh
   ./exspec
 else
-  echo 'Not running exspec because emission.out.zst was found'
+  echo 'Not running exspec because emission.out[.zst] was found'
 fi
 
 ./artis/scripts/exspec-after.sh
