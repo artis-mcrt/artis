artis-mcrt/artis

commit cb565d54892a4a581fe382e0829fe2841ebf13fc

Use more nonemptymgi indicies and remove more pointers from ModelGridCell (#140)

nltepop.cc
@@ -10,6 +10,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
@@ -383,10 +384,10 @@ void print_level_rates(const int modelgridindex, const int timestep, const int e
 }
 
 void nltepop_reset_element(const int modelgridindex, const int element) {
+  const ptrdiff_t nonemptymgi = grid::get_nonemptymgi_of_mgi(modelgridindex);
   const int nions = get_nions(element);
   for (int ion = 0; ion < nions; ion++) {
-    const int nlte_start = globals::elements[element].ions[ion].first_nlte;
-    std::fill_n(&grid::modelgrid[modelgridindex].nlte_pops[nlte_start],
+    std::fill_n(&grid::nltepops_allcells[(nonemptymgi * globals::total_nlte_levels)],
                 get_nlevels_nlte(element, ion) + (ion_has_superlevel(element, ion) ? 1 : 0), -1.);
   }
 }
@@ -1023,12 +1024,11 @@ void solve_nlte_pops_element(const int element, const int modelgridindex, const 
       //          gsl_vector_get(x, index_gs), gsl_vector_get(pop_norm_factor_vec, index_gs));
 
       // store the NLTE level populations
-      const int nlte_start = globals::elements[element].ions[ion].first_nlte;
       // double solution_ion_pop = 0.;
       for (int level = 1; level <= nlevels_nlte; level++) {
         const int index = get_nlte_vector_index(element, ion, level);
-        grid::modelgrid[modelgridindex].nlte_pops[nlte_start + level - 1] =
-            gsl_vector_get(&popvec, index) / grid::get_rho(modelgridindex);
+        set_nlte_levelpop_over_rho(modelgridindex, element, ion, level,
+                                   gsl_vector_get(&popvec, index) / grid::get_rho(modelgridindex));
         // solution_ion_pop += gsl_vector_get(popvec, index);
       }
 
@@ -1036,8 +1036,9 @@ void solve_nlte_pops_element(const int element, const int modelgridindex, const 
       if (ion_has_superlevel(element, ion))  // a superlevel exists
       {
         const int index_sl = get_nlte_vector_index(element, ion, nlevels_nlte + 1);
-        grid::modelgrid[modelgridindex].nlte_pops[nlte_start + nlevels_nlte] =
-            (gsl_vector_get(&popvec, index_sl) / grid::modelgrid[modelgridindex].rho / superlevel_partfunc[ion]);
+        set_nlte_superlevelpop_over_rho(
+            modelgridindex, element, ion,
+            gsl_vector_get(&popvec, index_sl) / grid::get_rho(modelgridindex) / superlevel_partfunc[ion]);
       }
 
       // store the ground level population
@@ -1142,7 +1143,6 @@ void nltepop_write_to_file(const int modelgridindex, const int timestep) {
 
     for (int ion = 0; ion < nions; ion++) {
       const int nlevels_nlte = get_nlevels_nlte(element, ion);
-      const int ion_first_nlte = globals::elements[element].ions[ion].first_nlte;
       const int ionstage = get_ionstage(element, ion);
 
       const int nsuperlevels = ion_has_superlevel(element, ion) ? 1 : 0;
@@ -1158,13 +1158,13 @@ void nltepop_write_to_file(const int modelgridindex, const int timestep) {
           if (level == 0) {
             nnlevelnlte = get_groundlevelpop(modelgridindex, element, ion);
           } else {
-            nnlevelnlte = (grid::modelgrid[modelgridindex].nlte_pops[ion_first_nlte + level - 1] *
-                           grid::modelgrid[modelgridindex].rho);
+            nnlevelnlte =
+                get_nlte_levelpop_over_rho(modelgridindex, element, ion, level) * grid::modelgrid[modelgridindex].rho;
           }
         } else {
           // superlevel, so add the populations of all other levels in the superlevel
-          const double slpopfactor = (grid::modelgrid[modelgridindex].nlte_pops[ion_first_nlte + nlevels_nlte] *
-                                      grid::modelgrid[modelgridindex].rho);
+          const double slpopfactor =
+              get_nlte_superlevelpop_over_rho(modelgridindex, element, ion) * grid::modelgrid[modelgridindex].rho;
 
           nnlevellte = 0;
           double superlevel_partfunc = 0;
@@ -1207,7 +1207,7 @@ void nltepop_write_restart_data(FILE *restart_file) {
       }
     }
     for (int nlteindex = 0; nlteindex < globals::total_nlte_levels; nlteindex++) {
-      fprintf(restart_file, "%la ", grid::modelgrid[modelgridindex].nlte_pops[nlteindex]);
+      fprintf(restart_file, "%la ", grid::nltepops_allcells[(nonemptymgi * globals::total_nlte_levels) + nlteindex]);
     }
   }
 }
@@ -1260,7 +1260,37 @@ void nltepop_read_restart_data(FILE *restart_file) {
         assert_always(fscanf(restart_file, "%*a ") == 0);  // discard value (master rank of this node will set it)
       } else
 #endif
-        assert_always(fscanf(restart_file, "%la ", &grid::modelgrid[modelgridindex].nlte_pops[nlteindex]) == 1);
+        assert_always(fscanf(restart_file, "%la ",
+                             &grid::nltepops_allcells[(nonemptymgi * globals::total_nlte_levels) + nlteindex]) == 1);
     }
   }
+}
+
+auto get_nlte_levelpop_over_rho(const int modelgridindex, const int element, const int ion, const int level) -> double {
+  assert_testmodeonly(level <= get_nlevels_nlte(element, ion));
+  const ptrdiff_t nonemptymgi = grid::get_nonemptymgi_of_mgi(modelgridindex);
+  return grid::nltepops_allcells[(nonemptymgi * globals::total_nlte_levels) +
+                                 globals::elements[element].ions[ion].first_nlte + level - 1];
+}
+
+auto get_nlte_superlevelpop_over_rho(const int modelgridindex, const int element, const int ion) -> double {
+  assert_testmodeonly(ion_has_superlevel(element, ion));
+  const ptrdiff_t nonemptymgi = grid::get_nonemptymgi_of_mgi(modelgridindex);
+  const int sl_nlte_index = globals::elements[element].ions[ion].first_nlte + get_nlevels_nlte(element, ion);
+  return grid::nltepops_allcells[(nonemptymgi * globals::total_nlte_levels) + sl_nlte_index];
+}
+
+void set_nlte_levelpop_over_rho(const int modelgridindex, const int element, const int ion, const int level,
+                                const double value) {
+  assert_testmodeonly(level <= get_nlevels_nlte(element, ion));
+  const ptrdiff_t nonemptymgi = grid::get_nonemptymgi_of_mgi(modelgridindex);
+  grid::nltepops_allcells[(nonemptymgi * globals::total_nlte_levels) + globals::elements[element].ions[ion].first_nlte +
+                          level - 1] = value;
+}
+
+void set_nlte_superlevelpop_over_rho(const int modelgridindex, const int element, const int ion, const double value) {
+  assert_testmodeonly(ion_has_superlevel(element, ion));
+  const ptrdiff_t nonemptymgi = grid::get_nonemptymgi_of_mgi(modelgridindex);
+  const int sl_nlte_index = globals::elements[element].ions[ion].first_nlte + get_nlevels_nlte(element, ion);
+  grid::nltepops_allcells[(nonemptymgi * globals::total_nlte_levels) + sl_nlte_index] = value;
 }

.github/workflows/ci-checks.yml
@@ -26,7 +26,7 @@ jobs:
             - name: run cppcheck
               run: |
                   cppcheck --version
-                  cppcheck --force --error-exitcode=1 --output-format=sarif --language=c++ --std=c++20 --enable=all --inconclusive --suppress=knownConditionTrueFalse --suppress=constParameterCallback --suppress=redundantAssignment --suppress=knownArgument --suppress=missingIncludeSystem --suppress=unusedFunction --check-level=exhaustive .
+                  cppcheck --force --error-exitcode=1 --language=c++ --std=c++20 --enable=all --inconclusive --suppress=knownConditionTrueFalse --suppress=constParameterCallback --suppress=redundantAssignment --suppress=knownArgument --suppress=missingIncludeSystem --suppress=unusedFunction --check-level=exhaustive .
 
     clang-format:
         runs-on: macos-14

.github/workflows/ci.yml
@@ -171,21 +171,10 @@ jobs:
               working-directory: tests/${{ matrix.testname }}_testrun/
               run: cat job1/output_0-0.txt
 
-            - name: Set up Python
-              if: always() && matrix.testmode == 'OFF'
-              uses: actions/setup-python@v5
-
-            - name: Install artistools
-              if: always() && matrix.testmode == 'OFF'
-              run: |
-                  python3 -m pip install --upgrade pip uv
-                  uv pip install --system -U artistools
-
             - name: show deposition.out
               if: always() && matrix.testmode == 'OFF'
               working-directory: tests/${{ matrix.testname }}_testrun/
               run: |
-                  echo -e "import artistools as at; import polars as pl;\nwith pl.Config(tbl_rows=-1,tbl_cols=-1,tbl_width_chars=300,tbl_formatting='NOTHING'):\n    df = at.get_deposition('.')\n    print(df.select(['timestep', 'tmid_days'] + [c for c in sorted(df.columns, key=lambda x: x.lower().lstrip('eps_')) if c not in ['timestep', 'tmid_days', 'tmid_s']]))" | python3 -
                   cat deposition.out
 
             - name: Run exspec
@@ -231,6 +220,16 @@ jobs:
               with:
                   name: ${{ matrix.testname }}_inputfiles
                   path: tests/${{ matrix.testname }}_inputfiles/results_md5*
+
+            - name: Set up Python
+              if: always() && matrix.testmode == 'OFF'
+              uses: actions/setup-python@v5
+
+            - name: Install artistools
+              if: always() && matrix.testmode == 'OFF'
+              run: |
+                  python3 -m pip install --upgrade pip uv
+                  uv pip install --system -U artistools
 
             - name: Plot light curve
               if: always() && matrix.testmode == 'OFF'

decay.cc
@@ -652,12 +652,11 @@ auto get_endecay_to_tinf_per_ejectamass_at_time(const int modelgridindex, const 
   return endecay;
 }
 
-auto get_endecay_per_ejectamass_t0_to_time_withexpansion_chain_numerical(const int modelgridindex,
+// Simple Euler integration as a check on the analytic result from get_endecay_per_ejectamass_t0_to_time_withexpansion()
+auto get_endecay_per_ejectamass_t0_to_time_withexpansion_chain_numerical(const int nonemptymgi,
                                                                          const int decaypathindex, const double tstart)
-    -> double
-// just here as as check on the analytic result from get_endecay_per_ejectamass_t0_to_time_withexpansion()
-// this version does an Euler integration
-{
+    -> double {
+  const auto modelgridindex = grid::get_mgi_of_nonemptymgi(nonemptymgi);
   double min_meanlife = -1;
   for (int i = 0; i < get_decaypathlength(decaypathindex); i++) {
     const double meanlife = get_meanlife(decaypaths[decaypathindex].nucindex[i]);
@@ -665,9 +664,9 @@ auto get_endecay_per_ejectamass_t0_to_time_withexpansion_chain_numerical(const i
       min_meanlife = meanlife;
     }
   }
+  // min steps across the meanlifetime
+  const int nsteps = static_cast<int>(ceil((tstart - grid::get_t_model()) / min_meanlife) * 100000);
 
-  const int nsteps = static_cast<int>(ceil((tstart - grid::get_t_model()) / min_meanlife) *
-                                      100000);  // min steps across the meanlifetime
   double chain_endecay = 0.;
   double last_chain_endecay = -1.;
   double last_t = -1.;
@@ -722,10 +721,9 @@ auto calculate_simtime_endecay_per_ejectamass(const int mgi, const int decaypath
   }
 }
 
-auto get_simtime_endecay_per_ejectamass(const int mgi, const int decaypathindex) -> double
+auto get_simtime_endecay_per_ejectamass(const int nonemptymgi, const int decaypathindex) -> double
 // get the decay energy released during the simulation time per unit mass [erg/g]
 {
-  const ptrdiff_t nonemptymgi = grid::get_modelcell_nonemptymgi(mgi);
   const double chainendecay = decaypath_energy_per_mass[(nonemptymgi * get_num_decaypaths()) + decaypathindex];
   assert_testmodeonly(chainendecay >= 0.);
   assert_testmodeonly(std::isfinite(chainendecay));
@@ -771,7 +769,7 @@ auto get_decaypath_power_per_ejectamass(const int decaypathindex, const int mode
 }
 }  // anonymous namespace
 
-[[nodiscard]] auto get_num_nuclides() -> int { return static_cast<int>(nuclides.size()); }
+[[nodiscard]] auto get_num_nuclides() -> ptrdiff_t { return std::ssize(nuclides); }
 
 [[nodiscard]] auto get_elname(const int z) -> std::string {
   assert_testmodeonly(z <= Z_MAX);
@@ -987,11 +985,11 @@ void init_nuclides(const std::vector<int> &custom_zlist, const std::vector<int> 
     }
   }
 
-  printout("Number of nuclides before filtering: %d\n", get_num_nuclides());
+  printout("Number of nuclides before filtering: %td\n", get_num_nuclides());
   find_decaypaths(custom_zlist, custom_alist, standard_nuclides);
   filter_unused_nuclides(custom_zlist, custom_alist, standard_nuclides);
 
-  printout("Number of nuclides:  %d\n", get_num_nuclides());
+  printout("Number of nuclides:  %td\n", get_num_nuclides());
 
   const int maxdecaypathlength = std::accumulate(
       decaypaths.cbegin(), decaypaths.cend(), 0,
@@ -1015,7 +1013,8 @@ void init_nuclides(const std::vector<int> &custom_zlist, const std::vector<int> 
 
 // calculate the decay energy per unit mass [erg/g] released from time t_model to tstart, accounting for
 // the photon energy loss due to expansion between time of decays and tstart (equation 18 of Lucy 2005)
-auto get_endecay_per_ejectamass_t0_to_time_withexpansion(const int modelgridindex, const double tstart) -> double {
+auto get_endecay_per_ejectamass_t0_to_time_withexpansion(const int nonemptymgi, const double tstart) -> double {
+  const auto modelgridindex = grid::get_mgi_of_nonemptymgi(nonemptymgi);
   double tot_endecay = 0.;
   for (const auto &decaypath : decaypaths) {
     const int decaypathlength = get_decaypathlength(decaypath);
@@ -1038,10 +1037,10 @@ auto get_endecay_per_ejectamass_t0_to_time_withexpansion(const int modelgridinde
 }
 
 // get the density at time tmin of decay energy that will be released during the simulation time range [erg/cm3]
-auto get_modelcell_simtime_endecay_per_mass(const int mgi) -> double {
+auto get_modelcell_simtime_endecay_per_mass(const int nonemptymgi) -> double {
   double endecay_per_mass = 0.;
   for (int decaypathindex = 0; decaypathindex < get_num_decaypaths(); decaypathindex++) {
-    endecay_per_mass += get_simtime_endecay_per_ejectamass(mgi, decaypathindex);
+    endecay_per_mass += get_simtime_endecay_per_ejectamass(nonemptymgi, decaypathindex);
   }
   return endecay_per_mass;
 }
@@ -1109,8 +1108,8 @@ void free_decaypath_energy_per_mass() {
 }
 
 // energy release rate in form of kinetic energy of positrons, electrons, and alpha particles in [erg/s/g]
-[[nodiscard]] auto get_particle_injection_rate(const int modelgridindex, const double t, const int decaytype)
-    -> double {
+[[nodiscard]] auto get_particle_injection_rate(const int nonemptymgi, const double t, const int decaytype) -> double {
+  const auto modelgridindex = grid::get_mgi_of_nonemptymgi(nonemptymgi);
   double dep_sum = 0.;
   const auto num_nuclides = get_num_nuclides();
   for (int nucindex = 0; nucindex < num_nuclides; nucindex++) {
@@ -1135,7 +1134,8 @@ void free_decaypath_energy_per_mass() {
 }
 
 // energy release rate in form of gamma-rays in [erg/s/g]
-[[nodiscard]] auto get_gamma_emission_rate(const int modelgridindex, const double t) -> double {
+[[nodiscard]] auto get_gamma_emission_rate(const int nonemptymgi, const double t) -> double {
+  const auto modelgridindex = grid::get_mgi_of_nonemptymgi(nonemptymgi);
   double eps_gamma_sum = 0.;
   const auto num_nuclides = get_num_nuclides();
   for (int nucindex = 0; nucindex < num_nuclides; nucindex++) {
@@ -1159,8 +1159,9 @@ void free_decaypath_energy_per_mass() {
 }
 
 // energy release rate [erg/s/g] including everything (even neutrinos that are ignored elsewhere)
-[[nodiscard]] auto get_qdot_modelcell(const int modelgridindex, const double t, const int decaytype) -> double {
+[[nodiscard]] auto get_qdot_modelcell(const int nonemptymgi, const double t, const int decaytype) -> double {
   double qdot = 0.;
+  const int modelgridindex = grid::get_mgi_of_nonemptymgi(nonemptymgi);
   const auto num_nuclides = get_num_nuclides();
   for (int nucindex = 0; nucindex < num_nuclides; nucindex++) {
     const auto [z, a] = get_nuc_z_a(nucindex);
@@ -1194,7 +1195,8 @@ auto get_global_etot_t0_tinf() -> double {
 }
 
 // Update the mass fractions of elements using the current abundances of nuclides
-void update_abundances(const int modelgridindex, const int timestep, const double t_current) {
+void update_abundances(const int nonemptymgi, const int timestep, const double t_current) {
+  const auto modelgridindex = grid::get_mgi_of_nonemptymgi(nonemptymgi);
   printout("update_abundances for cell %d timestep %d\n", modelgridindex, timestep);
 
   for (int element = get_nelements() - 1; element >= 0; element--) {
@@ -1243,16 +1245,16 @@ void update_abundances(const int modelgridindex, const int timestep, const doubl
       isomassfrac_on_nucmass_sum += nuc_massfrac / nucmass(2, 4);
     }
 
-    const double stable_init_massfrac = grid::get_stable_initabund(modelgridindex, element);
+    const double stable_init_massfrac = grid::get_stable_initabund(nonemptymgi, element);
     isomassfracsum += stable_init_massfrac;
     isomassfrac_on_nucmass_sum += stable_init_massfrac / globals::elements[element].initstablemeannucmass;
 
-    grid::set_elem_abundance(modelgridindex, element, isomassfracsum);
+    grid::set_elem_abundance(nonemptymgi, element, isomassfracsum);
     if (isomassfrac_on_nucmass_sum > 0.) {
-      grid::set_element_meanweight(modelgridindex, element, isomassfracsum / isomassfrac_on_nucmass_sum);
+      grid::set_element_meanweight(nonemptymgi, element, isomassfracsum / isomassfrac_on_nucmass_sum);
     } else {
       // avoid a divide by zero
-      grid::set_element_meanweight(modelgridindex, element, globals::elements[element].initstablemeannucmass);
+      grid::set_element_meanweight(nonemptymgi, element, globals::elements[element].initstablemeannucmass);
     }
   }
 
@@ -1298,7 +1300,8 @@ void update_abundances(const int modelgridindex, const int timestep, const doubl
   // assert_always(fabs(nucfracsum - initnucfracsum) < 0.001); // decays shouldn't change nuclear mass fraction sum
 }
 
-void fprint_nuc_abundances(FILE *estimators_file, const int modelgridindex, const double t_current, const int element) {
+void fprint_nuc_abundances(FILE *estimators_file, const int nonemptmgi, const double t_current, const int element) {
+  const auto modelgridindex = grid::get_mgi_of_nonemptymgi(nonemptmgi);
   const double rho = grid::get_rho(modelgridindex);
 
   const int atomic_number = get_atomicnumber(element);
@@ -1338,7 +1341,7 @@ void fprint_nuc_abundances(FILE *estimators_file, const int modelgridindex, cons
   }
 
   // factor to convert convert mass fraction to number density
-  const double otherstablemassfrac = grid::get_stable_initabund(modelgridindex, element);
+  const double otherstablemassfrac = grid::get_stable_initabund(nonemptmgi, element);
   if (otherstablemassfrac > 0) {
     const double meannucmass = globals::elements[element].initstablemeannucmass;
     const double otherstable_numberdens = otherstablemassfrac / meannucmass * grid::get_rho(modelgridindex);
@@ -1348,6 +1351,7 @@ void fprint_nuc_abundances(FILE *estimators_file, const int modelgridindex, cons
 }
 
 void setup_radioactive_pellet(const double e0, const int mgi, Packet &pkt) {
+  const auto nonemptymgi = grid::get_nonemptymgi_of_mgi(mgi);
   const int num_decaypaths = get_num_decaypaths();
 
   // decay channels include all radioactive decay paths, and possibly also an initial cell energy channel
@@ -1358,7 +1362,7 @@ void setup_radioactive_pellet(const double e0, const int mgi, Packet &pkt) {
 
   // add the radioactive decay paths
   for (int decaypathindex = 0; decaypathindex < num_decaypaths; decaypathindex++) {
-    energysum += get_simtime_endecay_per_ejectamass(mgi, decaypathindex);
+    energysum += get_simtime_endecay_per_ejectamass(nonemptymgi, decaypathindex);
     cumulative_en_sum[decaypathindex] = energysum;
   }
 
@@ -1414,7 +1418,8 @@ void setup_radioactive_pellet(const double e0, const int mgi, Packet &pkt) {
     // we need to scale the packet energy up or down according to decay rate at the randomly selected time.
     // e0 is the average energy per packet for this cell and decaypath, so we scale this up or down
     // according to: decay power at this time relative to the average decay power
-    const double avgpower = get_simtime_endecay_per_ejectamass(mgi, decaypathindex) / (globals::tmax - tdecaymin);
+    const double avgpower =
+        get_simtime_endecay_per_ejectamass(nonemptymgi, decaypathindex) / (globals::tmax - tdecaymin);
     assert_always(avgpower > 0.);
     assert_always(std::isfinite(avgpower));
     pkt.e_cmf = e0 * get_decaypath_power_per_ejectamass(decaypathindex, mgi, pkt.tdecay) / avgpower;

decay.h
@@ -26,7 +26,7 @@ constexpr std::array<enum decaytypes, 5> all_decaytypes = {
 void init_nuclides(const std::vector<int> &custom_zlist, const std::vector<int> &custom_alist);
 [[nodiscard]] auto get_nucstring_z(const std::string &strnuc) -> int;
 [[nodiscard]] auto get_nucstring_a(const std::string &strnuc) -> int;
-[[nodiscard]] auto get_num_nuclides() -> int;
+[[nodiscard]] auto get_num_nuclides() -> ptrdiff_t;
 [[nodiscard]] auto get_elname(int z) -> std::string;
 [[nodiscard]] auto get_nuc_z(int nucindex) -> int;
 [[nodiscard]] auto get_nuc_a(int nucindex) -> int;
@@ -35,16 +35,16 @@ void init_nuclides(const std::vector<int> &custom_zlist, const std::vector<int> 
 [[nodiscard]] auto nucdecayenergygamma(int nucindex) -> double;
 [[nodiscard]] auto nucdecayenergygamma(int z, int a) -> double;
 void set_nucdecayenergygamma(int nucindex, double value);
-void update_abundances(int modelgridindex, int timestep, double t_current);
-[[nodiscard]] auto get_endecay_per_ejectamass_t0_to_time_withexpansion(int modelgridindex, double tstart) -> double;
-[[nodiscard]] auto get_modelcell_simtime_endecay_per_mass(int mgi) -> double;
+void update_abundances(int nonemptymgi, int timestep, double t_current);
+[[nodiscard]] auto get_endecay_per_ejectamass_t0_to_time_withexpansion(int nonemptymgi, double tstart) -> double;
+[[nodiscard]] auto get_modelcell_simtime_endecay_per_mass(int nonemptymgi) -> double;
 void setup_decaypath_energy_per_mass();
 void free_decaypath_energy_per_mass();
-[[nodiscard]] auto get_qdot_modelcell(int modelgridindex, double t, int decaytype) -> double;
-[[nodiscard]] auto get_particle_injection_rate(int modelgridindex, double t, int decaytype) -> double;
-[[nodiscard]] auto get_gamma_emission_rate(int modelgridindex, double t) -> double;
+[[nodiscard]] auto get_qdot_modelcell(int nonemptymgi, double t, int decaytype) -> double;
+[[nodiscard]] auto get_particle_injection_rate(int nonemptymgi, double t, int decaytype) -> double;
+[[nodiscard]] auto get_gamma_emission_rate(int nonemptymgi, double t) -> double;
 [[nodiscard]] auto get_global_etot_t0_tinf() -> double;
-void fprint_nuc_abundances(FILE *estimators_file, int modelgridindex, double t_current, int element);
+void fprint_nuc_abundances(FILE *estimators_file, int nonemptmgi, double t_current, int element);
 void setup_radioactive_pellet(double e0, int mgi, Packet &pkt);
 void cleanup();
 

exspec.cc
@@ -73,7 +73,7 @@ void do_angle_bin(const int a, Packet *pkts, bool load_allrank_packets, Spectra 
     MPI_Barrier(MPI_COMM_WORLD);
 #endif
 
-    if (p % globals::nprocs != globals::rank_global) {
+    if (p % globals::nprocs != globals::my_rank) {
       printout("skipping packets file %d %d\n", p + 1, globals::nprocs);
       continue;
     }
@@ -173,7 +173,7 @@ auto main(int argc, char *argv[]) -> int {  // NOLINT(misc-unused-parameters)
   check_already_running();
 
   char filename[MAXFILENAMELENGTH];
-  if (globals::rank_global == 0) {
+  if (globals::my_rank == 0) {
     snprintf(filename, MAXFILENAMELENGTH, "exspec.txt");
     output_file = std::ofstream(filename);
     assert_always(output_file.is_open());
@@ -194,7 +194,7 @@ auto main(int argc, char *argv[]) -> int {  // NOLINT(misc-unused-parameters)
 #ifdef MPI_ON
   printout("process id (pid): %d\n", getpid());
   printout("MPI enabled:\n");
-  printout("  rank_global %d of [0..%d] in MPI_COMM_WORLD\n", globals::rank_global, globals::nprocs - 1);
+  printout("  rank_global %d of [0..%d] in MPI_COMM_WORLD\n", globals::my_rank, globals::nprocs - 1);
   printout("  rank_in_node %d of [0..%d] in node %d of [0..%d]\n", globals::rank_in_node, globals::node_nprocs - 1,
            globals::node_id, globals::node_count - 1);
 #else
@@ -202,14 +202,14 @@ auto main(int argc, char *argv[]) -> int {  // NOLINT(misc-unused-parameters)
 #endif
 
   // single rank only for now
-  assert_always(globals::rank_global == 0);
+  assert_always(globals::my_rank == 0);
   assert_always(globals::nprocs == 1);
 
   printout("Beginning exspec.\n");
 
   // Get input stuff
   printout("time before input %ld\n", std::time(nullptr));
-  input(globals::rank_global);
+  input(globals::my_rank);
   printout("time after input %ld\n", std::time(nullptr));
 
   // nprocs_exspec is the number of rank output files to process with expec

gammapkt.cc
@@ -274,7 +274,8 @@ auto get_chi_compton_rf(const Packet &pkt) -> double {
   const double sigma_cmf = (xx < THOMSON_LIMIT) ? SIGMA_T : sigma_compton_partial(xx, 1 + (2 * xx));
 
   // Now need to multiply by the electron number density.
-  const double chi_cmf = sigma_cmf * grid::get_nnetot(grid::get_cell_modelgridindex(pkt.where));
+  const auto mgi = grid::get_cell_modelgridindex(pkt.where);
+  const double chi_cmf = sigma_cmf * grid::get_nnetot(mgi);
 
   // convert between frames
   const double chi_rf = chi_cmf * calculate_doppler_nucmf_on_nurf(pkt.pos, pkt.dir, pkt.prop_time);
@@ -782,7 +783,7 @@ void transport_gamma(Packet &pkt, const double t2) {
 
     // Move it into the new cell.
     const int mgi = grid::get_cell_modelgridindex(pkt.where);
-    const int nonemptymgi = (mgi < grid::get_npts_model()) ? grid::get_modelcell_nonemptymgi(mgi) : -1;
+    const int nonemptymgi = (mgi < grid::get_npts_model()) ? grid::get_nonemptymgi_of_mgi(mgi) : -1;
     if (chi_tot > 0 && nonemptymgi >= 0) {
       update_gamma_dep(pkt, sdist, mgi, nonemptymgi);
     }
@@ -796,7 +797,7 @@ void transport_gamma(Packet &pkt, const double t2) {
     // Doesn't reach boundary.
     move_pkt_withtime(pkt, tdist / 2.);
     const int mgi = grid::get_cell_modelgridindex(pkt.where);
-    const int nonemptymgi = (mgi < grid::get_npts_model()) ? grid::get_modelcell_nonemptymgi(mgi) : -1;
+    const int nonemptymgi = (mgi < grid::get_npts_model()) ? grid::get_nonemptymgi_of_mgi(mgi) : -1;
 
     if (chi_tot > 0 && nonemptymgi >= 0) {
       update_gamma_dep(pkt, tdist, mgi, nonemptymgi);
@@ -806,7 +807,7 @@ void transport_gamma(Packet &pkt, const double t2) {
   } else if ((edist < sdist) && (edist < tdist)) {
     move_pkt_withtime(pkt, edist / 2.);
     const int mgi = grid::get_cell_modelgridindex(pkt.where);
-    const int nonemptymgi = (mgi < grid::get_npts_model()) ? grid::get_modelcell_nonemptymgi(mgi) : -1;
+    const int nonemptymgi = (mgi < grid::get_npts_model()) ? grid::get_nonemptymgi_of_mgi(mgi) : -1;
     if (chi_tot > 0 && nonemptymgi >= 0) {
       update_gamma_dep(pkt, edist, mgi, nonemptymgi);
     }
@@ -1075,7 +1076,7 @@ __host__ __device__ void do_gamma(Packet &pkt, const int nts, const double t2) {
                   GAMMA_THERMALISATION_SCHEME != ThermalisationScheme::DETAILEDWITHGAMMAPRODUCTS) {
       // no transport, so the path-based gamma deposition estimator won't get updated unless we do it here
       const int mgi = grid::get_cell_modelgridindex(pkt.where);
-      const int nonemptymgi = grid::get_modelcell_nonemptymgi(mgi);
+      const int nonemptymgi = grid::get_nonemptymgi_of_mgi(mgi);
       atomicadd(globals::dep_estimator_gamma[nonemptymgi], pkt.e_cmf);
     }
   }

commit 2e5e0cfc0317b05ef2c62b6d162d812d746b1402

Fix indexing error in nltepop_reset_element() (#166)
Bug was introduced by  #140 cb565d54 (not released to main)

nltepop.cc
@@ -390,7 +390,8 @@ void print_level_rates(const int modelgridindex, const int timestep, const int e
 void nltepop_reset_element(const int nonemptymgi, const int element) {
   const int nions = get_nions(element);
   for (int ion = 0; ion < nions; ion++) {
-    std::fill_n(&grid::nltepops_allcells[(nonemptymgi * globals::total_nlte_levels)],
+    const int nlte_start = globals::elements[element].ions[ion].first_nlte;
+    std::fill_n(&grid::nltepops_allcells[(nonemptymgi * globals::total_nlte_levels) + nlte_start],
                 get_nlevels_nlte(element, ion) + (ion_has_superlevel(element, ion) ? 1 : 0), -1.);
   }
 }