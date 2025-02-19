8f72dd1911de3fbe949953d262b368a3fb429c6d
Luke Shingles <luke.shingles@gmail.com>
2025-02-19 10:51:13 +0000

    Virgo: use znver4 nodes

--- scripts/exspec-gzip-virgo-submit.sh
+++ scripts/exspec-gzip-virgo-submit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbatch -J exspec_$(basename $(exec pwd)) --ntasks=1 --cpus-per-task 16 --mem-per-cpu=4096MB --partition=long --time=48:00:00 --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/exspec-gzip-virgo-slurmjob.sh
+sbatch -J exspec_$(basename $(exec pwd)) --ntasks=1 --cpus-per-task 48 --mem-per-cpu=2000MB --partition=long --time=48:00:00 --constraint=amd,epyc,9654 --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/exspec-gzip-virgo-slurmjob.sh
