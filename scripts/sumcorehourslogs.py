3435bc9bc818349002a6016afe3c7b65ea74b16c
Luke Shingles <luke.shingles@gmail.com>
2025-02-20 11:12:20 +0000

    Create sumcorehourslogs.py

---
+++ scripts/sumcorehourslogs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+
+def main() -> None:
+    sn3dlogfiles = sorted(Path().glob("**/output_0-0.txt"))
+
+    last_line = ""
+    total_core_hours = 0.0
+    for logfile in sn3dlogfiles:
+        prev_last_line = last_line
+        with logfile.open("rt", encoding="utf-8") as flog:
+            last_line = flog.readlines()[-1].strip()
+        print(f"{str(logfile):10s} ", end="")
+        if prev_last_line != last_line:
+            print(f"{last_line:>10s}")
+            if "CPU hours" in last_line:
+                job_core_hours = float(last_line.split("CPU hours")[0].split()[-1])
+                total_core_hours += job_core_hours
+                print(f"   finished with {job_core_hours} core-h")
+        else:
+            print("ignored duplicate log")
+
+    print(f"CPU time [k core-h]: {total_core_hours / 1000:.2f}")
+
+
+if __name__ == "__main__":
+    main()
