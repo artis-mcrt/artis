59c601e2d21bc7b08c00ecf7f2c203022a35a98a
Luke Shingles <luke.shingles@gmail.com>
2025-03-12 09:08:21 +0000

    Update virgo scripts to fix container creation failed
    Co-Authored-By: Christine Collins <ccollins22@qub.ac.uk>

--- scripts/artis-virgo-submit.sh
+++ scripts/artis-virgo-submit.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -x
-export APPTAINER_SHARENS=true
-export APPTAINER_CONFIGDIR=/tmp/$USER
-sbatch -J $(basename $(exec pwd)) --ntasks=1920 --ntasks-per-node=192 --mem-per-cpu=2000MB --partition=long --time=24:00:00 --constraint=amd,epyc,9654 --mail-type=ALL --mail-user=${USER}@gsi.de --no-requeue -- artis/scripts/artis-virgo-slurmjob.sh
+sbatch -J $(basename $(exec pwd)) --ntasks=960 --ntasks-per-node=120 --mem-per-cpu=2000MB --partition=long --time=48:00:00 --constraint=amd,epyc,9654 --mail-type=ALL --mail-user=${USER}@gsi.de --no-requeue -- artis/scripts/artis-virgo-slurmjob.sh
 
 # AMD EPYC 9654 nodes have 192 real cores per node and 4 GB/core. march=znver4
 # However, you might need to reduce --ntasks-per-node to around 120 because entire nodes are rarely available
--- scripts/exspec-gzip-virgo-slurmjob.sh
+++ scripts/exspec-gzip-virgo-slurmjob.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -x
 
+export APPTAINER_CONTAINER="/cvmfs/vae.gsi.de/vae24/containers/vae24-user_container_20240418T1037.sif"
+export APPTAINER_NAME="vae24-user_container"
+export APPTAINER_SHARENS=true
+export APPTAINER_CONFIGDIR=/tmp/$USER
+
 eval `spack load --sh gsl%gcc arch=linux-debian11-x86_64`
 eval `spack load --sh python arch=linux-debian11-x86_64`
 
