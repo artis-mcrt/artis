b0ebec3ae774c77125066495c8d02d5ccf8fa8f9
Luke Shingles <luke.shingles@gmail.com>
2025-04-01 12:49:50 +0100

    Replace vector with unique_ptr in Phixslist

--- rpkt.cc
+++ rpkt.cc
@@ -469,9 +469,9 @@ void rpkt_event_continuum(Packet &pkt, const Rpkt_continuum_absorptioncoeffs &ch
     const double chi_bf_rand = rng_uniform() * chi_bf_inrest;
 
     // first chi_bf_sum[i] such that chi_bf_sum[i] > chi_bf_rand
-    const auto allcontindex = std::upper_bound(phixslist.chi_bf_sum.data() + phixslist.allcontbegin,
-                                               phixslist.chi_bf_sum.data() + phixslist.allcontend - 1, chi_bf_rand) -
-                              phixslist.chi_bf_sum.data();
+    const auto allcontindex = std::upper_bound(phixslist.chi_bf_sum.get() + phixslist.allcontbegin,
+                                               phixslist.chi_bf_sum.get() + phixslist.allcontend - 1, chi_bf_rand) -
+                              phixslist.chi_bf_sum.get();
     assert_always(allcontindex < phixslist.allcontend);
 
     const double nu_edge = globals::allcont[allcontindex].nu_edge;
@@ -782,12 +782,12 @@ auto calculate_chi_ffheating(const int nonemptymgi, const double nu) -> double {
 // get bound-free opacity
 template <bool USECELLHISTANDUPDATEPHIXSLIST>
 auto calculate_chi_bf_gammacontr(const int nonemptymgi, const double nu, Phixslist &phixslist) -> double {
-  assert_always(!USECELLHISTANDUPDATEPHIXSLIST || !phixslist.chi_bf_sum.empty());
+  assert_always(!USECELLHISTANDUPDATEPHIXSLIST || !(phixslist.chi_bf_sum.get() == nullptr));
 
   double chi_bf_sum = 0.;
   if constexpr (USECELLHISTANDUPDATEPHIXSLIST) {
     if constexpr (USE_LUT_PHOTOION || USE_LUT_BFHEATING) {
-      std::ranges::fill(phixslist.groundcont_gamma_contr, 0.);
+      std::fill_n(phixslist.groundcont_gamma_contr.get(), globals::nbfcontinua_ground, 0.);
     }
   }
 
@@ -990,7 +990,7 @@ void calculate_chi_rpkt_cont(const double nu_cmf, Rpkt_continuum_absorptioncoeff
     chi_escat = SIGMA_T * nne;
 
     // Third contribution: bound-free absorption
-    chi_bf = chi_rpkt_cont.phixslist.chi_bf_sum.empty()
+    chi_bf = chi_rpkt_cont.phixslist.chi_bf_sum == nullptr
                  ? calculate_chi_bf_gammacontr<false>(nonemptymgi, nu_cmf, chi_rpkt_cont.phixslist)
                  : calculate_chi_bf_gammacontr<true>(nonemptymgi, nu_cmf, chi_rpkt_cont.phixslist);
 
