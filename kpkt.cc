7a435daf7a7720094d1fd19f62eb167d8a1d70a5
Luke Shingles <luke.shingles@gmail.com>
2025-04-28 13:26:02 +0100

    Revert "Update kpkt.cc"
    This reverts commit 0335b66c27a8e26a239307b71fc325520a2a2688.

--- kpkt.cc
+++ kpkt.cc
@@ -89,23 +89,25 @@ auto calculate_cooling_rates_ion(const int nonemptymgi, const int element, const
     const double statweight = stat_weight(element, ion, level);
 
     const auto uptranslist = get_uptransspan(element, ion, level);
-    for (const LevelTransition &transition : uptranslist) {
-      const int upper = transition.targetlevelindex;
-      const double epsilon_trans = epsilon(element, ion, upper) - epsilon_current;
-      const double C = nnlevel *
-                       col_excitation_ratecoeff(T_e, nne, element, ion, transition, epsilon_trans, statweight) *
-                       epsilon_trans;
-      C_ion += C;
-      if constexpr (!update_cooling_contrib_list) {
-        *C_exc += C;
+    if (!uptranslist.empty()) {
+      for (const auto &transition : uptranslist) {
+        const int upper = transition.targetlevelindex;
+        const double epsilon_trans = epsilon(element, ion, upper) - epsilon_current;
+        const double C = nnlevel *
+                         col_excitation_ratecoeff(T_e, nne, element, ion, transition, epsilon_trans, statweight) *
+                         epsilon_trans;
+        C_ion += C;
+        if constexpr (!update_cooling_contrib_list) {
+          *C_exc += C;
+        }
       }
-    }
-    if constexpr (update_cooling_contrib_list) {
-      globals::cellcache[cellcacheslotid].cooling_contrib[i] = C_ion;
+      if constexpr (update_cooling_contrib_list) {
+        globals::cellcache[cellcacheslotid].cooling_contrib[i] = C_ion;
 
-      assert_testmodeonly(coolinglist[i].type == CoolingType::COLLEXC);
+        assert_testmodeonly(coolinglist[i].type == CoolingType::COLLEXC);
 
-      i++;
+        i++;
+      }
     }
   }
 
