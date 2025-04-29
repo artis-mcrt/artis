c3374696f68461cb3e710d0a1c560680bb3634de
Luke Shingles <luke.shingles@gmail.com>
2025-04-29 16:04:23 +0100

    Update atomic.h

--- atomic.h
+++ atomic.h
@@ -54,6 +54,7 @@ inline auto get_nions(const int element) -> int {
 [[nodiscard]] inline auto get_ion_levels(const int element, const int ion) -> EnergyLevel * {
   assert_testmodeonly(element < get_nelements());
   assert_testmodeonly(ion < get_nions(element));
+  assert_testmodeonly(globals::elements[element].ions[ion].uniquelevelindexstart >= 0);
   return globals::alllevels.data() + globals::elements[element].ions[ion].uniquelevelindexstart;
 }
 
