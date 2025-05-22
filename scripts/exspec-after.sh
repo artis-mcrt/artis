de71c59a624eac6e5eb879a5c092044b9ab571f5
Luke Shingles <luke.shingles@gmail.com>
2025-05-22 10:47:57 +0100

    Update exspec-after.sh

--- scripts/exspec-after.sh
+++ scripts/exspec-after.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # only compress the files if we successfully ran exspec
-if [[ -f emission.out || -f emission.out.zst ]]; then
+if [[ -f emission.out || -f emission.out.zst || -f emissionpol.out ]]; then
 
   # zstd does decent compression at high speeds
   cmdcompress="zstd -T0 -16 -v --rm -f"
