4051404c69be8dc5464439dbd7d82225502366c3
Luke Shingles <luke.shingles@gmail.com>
2025-05-29 15:12:50 +0100

    Update ltepop.cc

--- ltepop.cc
+++ ltepop.cc
@@ -565,7 +565,7 @@ __host__ __device__ auto calculate_sahafact(const int element, const int ion, co
 
 // Use the ground level population and partition function to get an ion population
 [[nodiscard]] __host__ __device__ auto get_nnion(const int nonemptymgi, const int element, const int ion) -> double {
-  const auto nnion = static_cast<double>(get_groundlevelpop(nonemptymgi, element, ion)) *
+  const auto nnion = get_groundlevelpop(nonemptymgi, element, ion) *
                      grid::ion_partfuncts_allcells[(static_cast<ptrdiff_t>(nonemptymgi) * get_includedions()) +
                                                    get_uniqueionindex(element, ion)] /
                      stat_weight(element, ion, 0);
