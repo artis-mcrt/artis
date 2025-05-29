7030326c9697c2f30fe395c9656b93b874a8645a
Luke Shingles <luke.shingles@gmail.com>
2025-05-29 16:24:12 +0100

    Update thermalbalance.cc

--- thermalbalance.cc
+++ thermalbalance.cc
@@ -195,7 +195,7 @@ void calculate_heating_rates(const int nonemptymgi, const double T_e, const doub
 
 // Thermal balance equation on which we have to iterate to get T_e
 auto T_e_eqn_heating_minus_cooling(const double T_e, void *paras) -> double {
-  const TeSolutionParams *const params = static_cast<TeSolutionParams *>(paras);
+  const auto *const params = static_cast<const TeSolutionParams *>(paras);
 
   const auto nonemptymgi = params->nonemptymgi;
   const double t_current = params->t_current;
