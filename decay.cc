b1631f0589dc37de92cb1e2874614e736fc9e16c
Luke Shingles <luke.shingles@gmail.com>
2025-06-02 14:39:42 +0100

    Update decay.cc

--- decay.cc
+++ decay.cc
@@ -38,7 +38,7 @@ namespace decay {
 
 namespace {
 
-const std::array<const std::string, 119> elsymbols{
+constexpr auto elsymbols = std::array<const std::string, 119>{
     "n",  "H",  "He", "Li", "Be", "B",  "C",  "N",  "O",  "F",  "Ne", "Na",  "Mg", "Al",  "Si", "P",   "S",
     "Cl", "Ar", "K",  "Ca", "Sc", "Ti", "V",  "Cr", "Mn", "Fe", "Co", "Ni",  "Cu", "Zn",  "Ga", "Ge",  "As",
     "Se", "Br", "Kr", "Rb", "Sr", "Y",  "Zr", "Nb", "Mo", "Tc", "Ru", "Rh",  "Pd", "Ag",  "Cd", "In",  "Sn",
