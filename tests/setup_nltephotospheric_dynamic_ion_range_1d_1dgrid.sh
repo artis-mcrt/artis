e8ced3d63af6ce1b4b088d7ee1af8c94502feaaf
Fionntan Callan <56071719+fionntancallan@users.noreply.github.com>
2025-06-11 15:20:57 +0100

    Allow removing unimportant ion stages from the NLTE population solver (#187)
    Add functionality that allows the range of ions that are used in the
    NLTE solution to be dynamically changed if the NLTE solution fails due
    to either negative populations or large population inversions:
    
    -Previously when the NLTE population was negative for a level it was
    directly replaced with the LTE level population. This resulted in cases
    where the negative excited population was replaced with a value much
    greater than the NLTE ground pop resulting in extremely large partition
    function values which in some cases then overflowed the float limit
    resulting in the simulation failing. Additionally, negative NLTE ground
    populations were just replaced with the LTE value. However, if the
    ground population is negative this means the entire solution for that
    ion is unlikely to be sensible.
    
    -There were previously no checks on population inversions. Large
    population inversions can lead to partition function values which
    overflow the float limit causing the simulation to fail.
    
    The functionality added in this pull request should solve issues related
    to the NLTE solver giving negative populations or large populations
    inversions for ions which have very low populations (e.g for neutral ion
    states when the dominant ionisation state is a highly ionised state and
    vice versa) by allowing the NLTE solution to be recalculated when the
    solver fails but with the individual ion that is causing the issues
    excluded from the solution for that element. Previously the NLTE
    solution could only be calculated including all ions included in the
    simulation for a given element and there was only functionality to set
    the populations of the entire element to LTE values of the NLTE solver
    failed.
    
    ---------
    
    Co-authored-by: Luke Shingles <luke.shingles@gmail.com>

---
+++ tests/setup_nltephotospheric_dynamic_ion_range_1d_1dgrid.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env zsh
+
+set -x
+
+runfolder=nltephotospheric_dynamic_ion_range_1d_1dgrid_testrun
+
+mkdir -p $runfolder
+
+if [ ! -f atomicdata_feconi.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/atomicdata_feconi.tar.xz; fi
+
+tar -xf atomicdata_feconi.tar.xz --directory $runfolder
+
+rsync -av --ignore-times nltephotospheric_dynamic_ion_range_1d_1dgrid_inputfiles/ $runfolder
+
+rsync -av nebular_1d_3dgrid_inputfiles/abundances.txt nebular_1d_3dgrid_inputfiles/model.txt nebular_1d_3dgrid_inputfiles/recombrates.txt $runfolder
+
+cp ../data/* $runfolder
+
+cp ../artisoptions_nltephotospheric_dynamic_ion_range.h $runfolder/artisoptions.h
+
+cd $runfolder
+
+sed -i'' -e 's/constexpr int MPKTS.*/constexpr int MPKTS = 250;/g' artisoptions.h
+
+sed -i'' -e 's/constexpr auto GRID_TYPE.*/constexpr auto GRID_TYPE = GridType::SPHERICAL1D;/g' artisoptions.h
+
+sed -i'' -e 's|constexpr int NLEVELS_REQUIRETRANSITIONS(int Z, int ionstage) {.*}|constexpr int NLEVELS_REQUIRETRANSITIONS(int Z, int ionstage) { return (Z < 20) ? 20 : 40; }|g' artisoptions.h
+
+
+sed -i'' -e 's/constexpr int TABLESIZE.*/constexpr int TABLESIZE = 20;/g' artisoptions.h
+sed -i'' -e 's/constexpr double MINTEMP.*/constexpr double MINTEMP = 3500.;/g' artisoptions.h
+sed -i'' -e 's/constexpr double MAXTEMP.*/constexpr double MAXTEMP = 140000.;/g' artisoptions.h
+
+sed -i'' -e 's/constexpr int FIRST_NLTE_RADFIELD_TIMESTEP.*/constexpr int FIRST_NLTE_RADFIELD_TIMESTEP = 7;/g' artisoptions.h
+
+sed -i'' -e 's/constexpr int DETAILED_BF_ESTIMATORS_USEFROMTIMESTEP.*/constexpr int DETAILED_BF_ESTIMATORS_USEFROMTIMESTEP = 7;/g' artisoptions.h
+
+sed -i'' -e 's/constexpr float STRICT_POPULATION_CHECKING_INVERSION_FACTOR_SOLVER_FAIL.*/constexpr float STRICT_POPULATION_CHECKING_INVERSION_FACTOR_SOLVER_FAIL = 50.0;/g' artisoptions.h
+
+sed -i'' -e 's/constexpr float STRICT_POPULATION_CHECKING_INVERSION_FACTOR_PRINTOUT_WARNING.*/constexpr float STRICT_POPULATION_CHECKING_INVERSION_FACTOR_PRINTOUT_WARNING = 2.0;/g' artisoptions.h
+
+sed -i'' -e 's/constexpr double NLTE_LIMIT_ION_STAGES_MAX_LEVELPOP_OVER_ELEMENTPOP_REMOVE_ION.*/constexpr double NLTE_LIMIT_ION_STAGES_MAX_LEVELPOP_OVER_ELEMENTPOP_REMOVE_ION = 1e-9;/g' artisoptions.h
+
+sed -i'' -e 's/constexpr bool WRITE_PARTIAL_EMISSIONABSORPTIONSPEC.*/constexpr bool WRITE_PARTIAL_EMISSIONABSORPTIONSPEC = true;/g' artisoptions.h
+
+cd -
+
+set +x
