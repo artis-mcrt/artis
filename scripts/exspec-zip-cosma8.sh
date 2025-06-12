fd026c58347aef4825a0e06e1705c8681c4da596
Luke Shingles <luke.shingles@gmail.com>
2025-06-12 11:10:29 +0100

    Revert cosma8 jobscripts to rome queue

--- scripts/exspec-zip-cosma8.sh
+++ scripts/exspec-zip-cosma8.sh
@@ -2,9 +2,11 @@
 
 #SBATCH --ntasks 128
 #SBATCH --time=48:00:00
-#SBATCH --partition=cosma8-milan
+#SBATCH --partition=cosma8-rome
 #SBATCH --account=dp033
 #SBATCH --exclusive
+#SBATCH -o slurm-%J.out
+#SBATCH -e slurm-%J.out
 #SBATCH --mail-type=ALL
 ##SBATCH --mail-user=f.callan@qub.ac.uk
 ##SBATCH --mail-user=luke.shingles@gmail.com
