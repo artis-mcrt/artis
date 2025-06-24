c28c4465afa06477b654906da91c56d1cb48249d
Luke Shingles <luke.shingles@gmail.com>
2025-06-24 11:22:28 +0100

    Update sn3d.h

--- sn3d.h
+++ sn3d.h
@@ -427,11 +427,7 @@ template <typename T, typename Op, typename Comm>
   requires(!std::ranges::random_access_range<T> &&
            (std::is_same_v<T, double> || std::is_same_v<T, float> || std::is_same_v<T, int> || std::is_same_v<T, bool>))
 inline void MPI_Allreduce_safe(T &data, Op &&op, Comm &&comm) {
-  assert_always(op != MPI_OP_NULL);
-  assert_always(comm != MPI_COMM_NULL);
-  const auto ret =
-      MPI_Allreduce(MPI_IN_PLACE, &data, 1, GET_MPI_TYPE<T>(), std::forward<Op>(op), std::forward<Comm>(comm));
-  assert_always(ret == MPI_SUCCESS);
+  MPI_Allreduce_safe(std::span(&data, 1), std::forward<Op>(op), std::forward<Comm>(comm));
 }
 
 template <typename T>
