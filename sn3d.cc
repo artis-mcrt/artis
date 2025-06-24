fbd510b79c96c9cd7274f82782e584924b8c9f9b
Luke Shingles <luke.shingles@gmail.com>
2025-06-24 11:51:34 +0100

    Update sn3d.cc

--- sn3d.cc
+++ sn3d.cc
@@ -249,16 +249,21 @@ void mpi_communicate_grid_properties() {
 
     MPI_Barrier(MPI_COMM_WORLD);
     if (globals::rank_in_node == 0) {
-      MPI_Bcast(&grid::elem_meanweight_allcells[root_nstart_nonempty * nelements], root_ndo_nonempty * nelements,
-                MPI_FLOAT, root_node_id, globals::mpi_comm_internode);
-      MPI_Bcast(&grid::elem_massfracs_allcells[root_nstart_nonempty * nelements], root_ndo_nonempty * nelements,
-                MPI_FLOAT, root_node_id, globals::mpi_comm_internode);
-      MPI_Bcast(&grid::ion_groundlevelpops_allcells[root_nstart_nonempty * nincludedions],
-                root_ndo_nonempty * nincludedions, MPI_FLOAT, root_node_id, globals::mpi_comm_internode);
-      MPI_Bcast(&grid::ion_partfuncts_allcells[root_nstart_nonempty * nincludedions], root_ndo_nonempty * nincludedions,
-                MPI_FLOAT, root_node_id, globals::mpi_comm_internode);
-      MPI_Bcast(&grid::ion_cooling_contribs_allcells[root_nstart_nonempty * nincludedions],
-                root_ndo_nonempty * nincludedions, MPI_DOUBLE, root_node_id, globals::mpi_comm_internode);
+      MPI_Bcast_safe(
+          grid::elem_meanweight_allcells.subspan(root_nstart_nonempty * nelements, root_ndo_nonempty * nelements),
+          root_node_id, globals::mpi_comm_internode);
+      MPI_Bcast_safe(
+          grid::elem_massfracs_allcells.subspan(root_nstart_nonempty * nelements, root_ndo_nonempty * nelements),
+          root_node_id, globals::mpi_comm_internode);
+      MPI_Bcast_safe(grid::ion_groundlevelpops_allcells.subspan(root_nstart_nonempty * nincludedions,
+                                                                root_ndo_nonempty * nincludedions),
+                     root_node_id, globals::mpi_comm_internode);
+      MPI_Bcast_safe(grid::ion_partfuncts_allcells.subspan(root_nstart_nonempty * nincludedions,
+                                                           root_ndo_nonempty * nincludedions),
+                     root_node_id, globals::mpi_comm_internode);
+      MPI_Bcast_safe(grid::ion_cooling_contribs_allcells.subspan(root_nstart_nonempty * nincludedions,
+                                                                 root_ndo_nonempty * nincludedions),
+                     root_node_id, globals::mpi_comm_internode);
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
