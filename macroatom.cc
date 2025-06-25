a15e3ce0df07f629c913b440fd7c8ed8bf4aee1b
Luke Shingles <luke.shingles@gmail.com>
2025-06-25 22:58:21 +0100

    Update macroatom.cc

--- macroatom.cc
+++ macroatom.cc
@@ -467,8 +467,7 @@ __host__ __device__ void do_macroatom(Packet &pkt, const MacroAtomState &pktmast
         const double targetval = rng_uniform() * processrates[MA_ACTION_INTERNALDOWNSAME];
 
         // first sum_internal_down_same[i] such that sum_internal_down_same[i] > targetval
-        const auto downtransindex = std::upper_bound(sum_internal_down_same_exceptlast.begin(),
-                                                     sum_internal_down_same_exceptlast.end(), targetval) -
+        const auto downtransindex = std::ranges::upper_bound(sum_internal_down_same_exceptlast, targetval) -
                                     sum_internal_down_same_exceptlast.begin();
         const auto alltrans_startdown = get_alltrans_startdown(uniquelevelindex);
 
