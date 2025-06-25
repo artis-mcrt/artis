bf5222c80b836222ac0a529aa0169f93a3bfc337
Luke Shingles <luke.shingles@gmail.com>
2025-06-25 22:34:04 +0100

    Update macroatom.cc

--- macroatom.cc
+++ macroatom.cc
@@ -450,16 +450,17 @@ __host__ __device__ void do_macroatom(Packet &pkt, const MacroAtomState &pktmast
 
       case MA_ACTION_INTERNALDOWNSAME: {
         stats::increment(stats::COUNTER_INTERACTIONS);
-        const double *sum_internal_down_same = chlevel.sum_internal_down_same;
         const int ndowntrans = get_ndowntrans(uniquelevelindex);
+        const std::span<const double> sum_internal_down_same_exceptlast =
+            std::span(chlevel.sum_internal_down_same, ndowntrans - 1);
 
         // Randomly select the occurring transition
         const double targetval = rng_uniform() * processrates[MA_ACTION_INTERNALDOWNSAME];
 
         // first sum_internal_down_same[i] such that sum_internal_down_same[i] > targetval
-        const auto downtransindex =
-            std::upper_bound(sum_internal_down_same, sum_internal_down_same + ndowntrans - 1, targetval) -
-            sum_internal_down_same;
+        const auto downtransindex = std::upper_bound(sum_internal_down_same_exceptlast.begin(),
+                                                     sum_internal_down_same_exceptlast.end(), targetval) -
+                                    sum_internal_down_same_exceptlast.begin();
         const auto alltrans_startdown = get_alltrans_startdown(uniquelevelindex);
 
         level = globals::alltrans.targetlevelindex[alltrans_startdown + downtransindex];
@@ -562,7 +563,8 @@ __host__ __device__ void do_macroatom(Packet &pkt, const MacroAtomState &pktmast
         stats::increment(stats::COUNTER_INTERACTIONS);
 
         // randomly select the occurring transition
-        const auto sum_internal_up_same_exceptlast = std::span(chlevel.sum_internal_up_same, nuptrans - 1);
+        const std::span<const double> sum_internal_up_same_exceptlast =
+            std::span(chlevel.sum_internal_up_same, nuptrans - 1);
 
         const double targetval = rng_uniform() * processrates[MA_ACTION_INTERNALUPSAME];
 
