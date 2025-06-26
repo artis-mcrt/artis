4fe97f4fdef22942e385bad5fb5cfcd3d6d954ca
Fionntan Callan <56071719+fionntancallan@users.noreply.github.com>
2025-06-26 19:02:56 +0100

    Update nltephotospheric tests to include Fe VI and VII and He in atomic data (#203)
    Previously none of the automated tests included He or instates higher
    than V for any of the IGEs. Including He ensures there is a test to
    ensure we can handle the case when a species has zero bound electrons.
    Including Fe VI and VII provides a more robust test of the dynamic ion
    treatment in the photospheric phase when the ionisation state tends to
    be higher.
    
    ---------
    
    Co-authored-by: Luke Shingles <luke.shingles@gmail.com>

--- tests/setup_nltephotospheric_dynamic_ion_range_1d_1dgrid.sh
+++ tests/setup_nltephotospheric_dynamic_ion_range_1d_1dgrid.sh
@@ -6,13 +6,13 @@ runfolder=nltephotospheric_dynamic_ion_range_1d_1dgrid_testrun
 
 mkdir -p $runfolder
 
-if [ ! -f atomicdata_feconi.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/atomicdata_feconi.tar.xz; fi
+if [ ! -f atomicdata_hefeconi_fe_I_to_VII.tar.xz ]; then curl -O https://theory.gsi.de/~lshingle/artis_http_public/artis/atomicdata_hefeconi_fe_i_to_vii.tar.xz; fi
 
-tar -xf atomicdata_feconi.tar.xz --directory $runfolder
+tar -xf atomicdata_hefeconi_fe_i_to_vii.tar.xz --directory $runfolder
 
-rsync -av --ignore-times nltephotospheric_dynamic_ion_range_1d_1dgrid_inputfiles/ $runfolder
+rsync -av --exclude="recombrates.txt" nebular_1d_3dgrid_inputfiles/ $runfolder/
 
-rsync -av nebular_1d_3dgrid_inputfiles/abundances.txt nebular_1d_3dgrid_inputfiles/model.txt nebular_1d_3dgrid_inputfiles/recombrates.txt $runfolder
+rsync -av --ignore-times nltephotospheric_dynamic_ion_range_1d_1dgrid_inputfiles/ $runfolder/
 
 cp ../data/* $runfolder
 
@@ -20,26 +20,23 @@ cp ../artisoptions_nltephotospheric_dynamic_ion_range.h $runfolder/artisoptions.
 
 cd $runfolder
 
-sed -i'' -e 's/constexpr int MPKTS.*/constexpr int MPKTS = 250;/g' artisoptions.h
+sed -i'' -e 's/constexpr int MPKTS.*/constexpr int MPKTS = 400;/g' artisoptions.h
 
 sed -i'' -e 's/constexpr auto GRID_TYPE.*/constexpr auto GRID_TYPE = GridType::SPHERICAL1D;/g' artisoptions.h
 
+sed -i'' -e 's/constexpr int NLTEITER.*/constexpr int NLTEITER = 2;/g' artisoptions.h
+
+perl -0777 -i -pe 's|constexpr bool LEVEL_IS_NLTE\(int element_z, int ionstage, int level\) \{.*?\n\}|constexpr bool LEVEL_IS_NLTE(int element_z, int ionstage, int level) {\n  if (element_z == 26 && ionstage == 2) {\n    return (level <= 100);\n  }\n  return (level <= 50);\n}\n|s' artisoptions.h
+
 sed -i'' -e 's|constexpr int NLEVELS_REQUIRETRANSITIONS(int Z, int ionstage) {.*}|constexpr int NLEVELS_REQUIRETRANSITIONS(int Z, int ionstage) { return (Z < 20) ? 20 : 40; }|g' artisoptions.h
 
+sed -i'' -e 's/constexpr int TABLESIZE.*/constexpr int TABLESIZE = 40;/g' artisoptions.h
 
-sed -i'' -e 's/constexpr int TABLESIZE.*/constexpr int TABLESIZE = 20;/g' artisoptions.h
-sed -i'' -e 's/constexpr double MINTEMP.*/constexpr double MINTEMP = 3500.;/g' artisoptions.h
-sed -i'' -e 's/constexpr double MAXTEMP.*/constexpr double MAXTEMP = 140000.;/g' artisoptions.h
+sed -i'' -e 's/constexpr int FIRST_NLTE_RADFIELD_TIMESTEP.*/constexpr int FIRST_NLTE_RADFIELD_TIMESTEP = 4;/g' artisoptions.h
 
-sed -i'' -e 's/constexpr int FIRST_NLTE_RADFIELD_TIMESTEP.*/constexpr int FIRST_NLTE_RADFIELD_TIMESTEP = 7;/g' artisoptions.h
+sed -i'' -e 's/constexpr int RADFIELDBINCOUNT.*/constexpr int RADFIELDBINCOUNT = 24;/g' artisoptions.h
 
-sed -i'' -e 's/constexpr int DETAILED_BF_ESTIMATORS_USEFROMTIMESTEP.*/constexpr int DETAILED_BF_ESTIMATORS_USEFROMTIMESTEP = 7;/g' artisoptions.h
-
-sed -i'' -e 's/constexpr float STRICT_POPULATION_CHECKING_INVERSION_FACTOR_SOLVER_FAIL.*/constexpr float STRICT_POPULATION_CHECKING_INVERSION_FACTOR_SOLVER_FAIL = 50.0;/g' artisoptions.h
-
-sed -i'' -e 's/constexpr float STRICT_POPULATION_CHECKING_INVERSION_FACTOR_PRINTOUT_WARNING.*/constexpr float STRICT_POPULATION_CHECKING_INVERSION_FACTOR_PRINTOUT_WARNING = 2.0;/g' artisoptions.h
-
-sed -i'' -e 's/constexpr double NLTE_LIMIT_ION_STAGES_MAX_LEVELPOP_OVER_ELEMENTPOP_REMOVE_ION.*/constexpr double NLTE_LIMIT_ION_STAGES_MAX_LEVELPOP_OVER_ELEMENTPOP_REMOVE_ION = 1e-9;/g' artisoptions.h
+sed -i'' -e 's/constexpr int DETAILED_BF_ESTIMATORS_USEFROMTIMESTEP.*/constexpr int DETAILED_BF_ESTIMATORS_USEFROMTIMESTEP = 4;/g' artisoptions.h
 
 sed -i'' -e 's/constexpr bool WRITE_PARTIAL_EMISSIONABSORPTIONSPEC.*/constexpr bool WRITE_PARTIAL_EMISSIONABSORPTIONSPEC = true;/g' artisoptions.h
 
