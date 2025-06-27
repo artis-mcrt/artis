9b23daf61c6c634b188cafb2c7af3ffe88407627
Fionntan Callan <fcallan02@qub.ac.uk>
2025-06-27 10:39:16 +0100

    Update nltepop.cc

--- nltepop.cc
+++ nltepop.cc
@@ -1241,7 +1241,6 @@ void solve_nlte_pops_element(const int element, const int nonemptymgi, const int
           first_ion_used++;  // increment the first ion used to the next ion
         } else {
           printout("  WARNING: can't remove bottom ion stage from NLTE solution, returning matrix solve fail\n");
-          return;
         }
       }
       if (!remove_bottom_ion_from_solution && !remove_top_ion_from_solution) {
