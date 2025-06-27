b850ec232dd171fe6a6e062a7f5301b97efc177b
Fionntan Callan <fcallan02@qub.ac.uk>
2025-06-27 12:05:35 +0100

    Update artisoptions_nltephotospheric_dynamic_ion_range.h

--- artisoptions_nltephotospheric_dynamic_ion_range.h
+++ artisoptions_nltephotospheric_dynamic_ion_range.h
@@ -35,7 +35,7 @@ constexpr bool single_level_top_ion = false;
 
 constexpr bool single_ground_level = false;
 
-constexpr int NLEVELS_REQUIRETRANSITIONS(int Z, int ionstage) { return ION_NLEVELS_NLTE(Z, ionstage); }
+constexpr int NLEVELS_REQUIRETRANSITIONS(int Z, int ionstage) { return (Z < 20) ? 100 : 200; }
 
 constexpr bool UNIFORM_PELLET_ENERGIES = true;
 
