1f4de45e3ab6dd021f03cc725b2523452dec4cac
Luke Shingles <luke.shingles@gmail.com>
2025-07-04 12:18:33 +0100

    Update exspec-after.sh

--- scripts/exspec-after.sh
+++ scripts/exspec-after.sh
@@ -6,8 +6,12 @@ if [[ -f emission.out || -f emission.out.zst || -f emissionpol.out ]]; then
   # zstd does decent compression at high speeds
   cmdcompress="zstd -T0 -13 -v --rm -f"
 
+  mkdir -p speclc_angle_res
+  mv *_res_*.out* speclc_angle_res/ || true
   # join 3D direction files, if they exist
+  cd speclc_angle_res
   python3 ./artis/scripts/mergeangleres.py
+  cd ..
 
   mkdir -p packets
   mv packets*.out* packets/ || true
@@ -33,9 +37,6 @@ if [[ -f emission.out || -f emission.out.zst || -f emissionpol.out ]]; then
   find . -maxdepth 2 -name '*.out' ! -name "slurm-*.out" -size +1M -print0 | sort -z | xargs -r0 $cmdcompress
 
   ./artis/scripts/tar_rm_logs.sh
-
-  mkdir -p speclc_angle_res
-  mv *_res_*.out* speclc_angle_res/ || true
 
   export PATH="$(pwd)/../uv/bin:$PATH"
   export PATH="$HOME/.local/bin/:$PATH"
