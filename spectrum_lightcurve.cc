e1011bb6de9de7c1251716e8a3916779d53896be
Luke Shingles <luke.shingles@gmail.com>
2025-07-14 11:38:08 +0100

    Update spectrum_lightcurve.cc

--- spectrum_lightcurve.cc
+++ spectrum_lightcurve.cc
@@ -246,9 +246,14 @@ void write_partial_lightcurve_spectra_dirbin(const int nts, std::span<const Pack
   init_spectra(rpkt_spectra, NU_MIN_R, NU_MAX_R, do_emission_absorption);
 
   MPI_Barrier(globals::mpi_comm_node);
+#if defined REPRODUCIBLE && REPRODUCIBLE
   for (int node_rank = 0; node_rank < globals::node_nprocs; node_rank++) {
     // do one rank at a time to keep the results reproducible (instead of simultaneous atomic adds to shared memory)
     if (node_rank == globals::rank_in_node) {
+#else
+  {
+    {
+#endif
       for (int ii = 0; ii < globals::npkts; ii++) {
         if (pkts[ii].type == TYPE_ESCAPE) {
           if (pkts[ii].escape_type == TYPE_RPKT) {
