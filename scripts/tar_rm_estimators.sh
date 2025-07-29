efecee12dd702e634b98b13db65780d4925bdb51
Luke Shingles <luke.shingles@gmail.com>
2025-07-29 22:42:12 +0100

    Don't compress estimators_allranks.tar

--- scripts/tar_rm_estimators.sh
+++ scripts/tar_rm_estimators.sh
@@ -23,10 +23,8 @@ find . -type d -name "*.slurm" -print0 | while IFS= read -r -d '' dir; do
         else
             if [ -e estimbatch00_*.parquet* -a -e estimators_0001.out* ]; then
                 find . -mindepth 0 -name "estimators_*.out*" -print | sort > $tmpdir/estimatorfilelist.txt
-                echo "  Creating tarball of estimators_allranks.tar.zst"
-                tar -cf $tmpdir/estimators_allranks.tar --files-from $tmpdir/estimatorfilelist.txt && zstd -v -T0 -15 $tmpdir/estimators_allranks.tar && mv -v $tmpdir/estimators_allranks.tar.zst . && rm -f $tmpdir/* && find . -mindepth 0 -name "estimators_*.out*" -delete
-                # copy the timestamp of the first estimator file to the tarball
-                #touch -m -r estimators_0000.out* estimators_allranks.tar.zst
+                echo "  Creating tarball of estimators_allranks.tar"
+                tar -cf $tmpdir/estimators_allranks.tar --files-from $tmpdir/estimatorfilelist.txt && mv -v $tmpdir/estimators_allranks.tar . && rm -f $tmpdir/* && find . -mindepth 0 -name "estimators_*.out*" -delete
             else
                 echo "  no parquet batches or estimators_0001.out* file found, skipping tar creation"
             fi
