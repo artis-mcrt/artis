04c6c919d52f21ea60104458fd96b818cdf6bef5
Luke Shingles <luke.shingles@gmail.com>
2025-08-07 11:20:19 +0100

    Create aliases.sh

---
+++ scripts/aliases.sh
@@ -0,0 +1,13 @@
+# check mtime of symlinks and compile in parallel
+export MAKEFLAGS="--check-symlink-times -j$(nproc --all)"
+
+alias squeueu="squeue --user=$USER --format=\"%.9i %.10P %.8T %.12M %22N %5D %4C %j\""
+alias qstatu="squeueu"
+
+# enqueue with a job name set to the current directory name
+alias qsub='sbatch --job-name="${PWD##*/}"'
+
+alias qdel="scancel"
+alias qstat="squeue"
+alias jobinfo="scontrol show jobid"
+alias rsyncnooutput="rsync -av --exclude='speclc_angle_res' --exclude='exspec*.txt' --exclude="*.pdf" --exclude="out*.txt" --exclude='*.slurm' --exclude='*.out*' --exclude='packets*' --exclude='logfiles*' --exclude='*.tmp'"
