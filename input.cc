85189deaad86d2b1f698cd32c95d66b2590b9beb
ssim <s.sim@qub.ac.uk>
2025-09-04 13:51:02 +0100

    Minor bug fix

--- input.cc
+++ input.cc
@@ -899,7 +899,7 @@ void read_autoion_data() {
 
         if (globals::alllevels.allautoion_start[get_uniquelevelindex(element, lowerion, lowerlevel)] < 0) {
           assert_always(nautoiondowntrans == 1);
-          assert_always(nautoionuptrans == 1);
+          //assert_always(nautoionuptrans == 1);
           // this is the first autoionizing transition for this level, so set the start index
           globals::alllevels.allautoion_start[get_uniquelevelindex(element, lowerion, lowerlevel)] =
               static_cast<int>(temp_allautoion.size());
