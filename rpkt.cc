d6f71a33c05efef8a39a7cbcff5f5f7b0b19d3ee
Luke Shingles <luke.shingles@gmail.com>
2025-10-27 10:44:21 +0000

    Update rpkt.cc

--- rpkt.cc
+++ rpkt.cc
@@ -776,10 +776,10 @@ auto calculate_chi_bf_gammacontr(const int nonemptymgi, const double nu, Phixsli
   const int allcontend = static_cast<int>(std::ranges::upper_bound(allcont_nu_edge, nu) - allcont_nu_edge.begin());
 
   // require that nu > nu_edge * last_phixs_nuovernuedge, which can exclude some low-nu edges
-  const int allcontbegin = std::lower_bound(allcont_nu_edge.begin(), allcont_nu_edge.begin() + allcontend, nu,
-                                            [](const double nu_edge, const double nu_cmf) {
-                                              return nu_edge * last_phixs_nuovernuedge < nu_cmf;
-                                            }) -
+  const int allcontbegin = std::ranges::lower_bound(allcont_nu_edge.first(allcontend), nu,
+                                                    [](const double nu_edge, const double nu_cmf) {
+                                                      return nu_edge * last_phixs_nuovernuedge < nu_cmf;
+                                                    }) -
                            allcont_nu_edge.begin();
 
   assert_testmodeonly(allcontbegin >= 0);
