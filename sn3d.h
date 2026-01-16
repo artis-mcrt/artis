0a60dbfbc4e62003a975d2c8f0f931bec3074861
Luke Shingles <luke.shingles@gmail.com>
2026-01-15 23:34:52 -0700

    Update sn3d.h

--- sn3d.h
+++ sn3d.h
@@ -268,7 +268,7 @@ inline void gsl_error_handler_printout(const char* reason, const char* file, int
       auto datafolderfilename = std::string(datadir) + filename;
       auto file2 = std::fstream(datafolderfilename.c_str(), mode);
       if (file2.is_open()) {
-        return file;
+        return file2;
       }
     }
   }
