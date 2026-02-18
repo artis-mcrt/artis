107c5b80c8e30e7251e8d645be828f2b31bd231a
Luke Shingles <luke.shingles@gmail.com>
2026-02-18 10:22:12 +0000

    Update ratecoeff.h

--- ratecoeff.h
+++ ratecoeff.h
@@ -115,7 +115,6 @@ auto integrator(auto params, const double a, const double b, const double epsrel
 #else
 #ifdef BOOST_OFF
 
-  constexpr double epsrelwarning = 1e-2;  // fractional error to emit a warning
   // GSL's QAG adaptive integrator
   constexpr auto key = []() {
     switch (GKNPOINTS) {
@@ -139,7 +138,7 @@ auto integrator(auto params, const double a, const double b, const double epsrel
   const auto status =
       gsl_integration_qag(&gslfunc, a, b, 0., epsrel, GSLWSIZE, key, gslworkspace.get(), &result, abserr);
   gsl_set_error_handler(previous_handler);
-  if (status != 0 && (status != 18 || (*abserr / std::abs(result)) > epsrelwarning)) {
+  if (status != 0 && (status != 18 || (*abserr / std::abs(result)) > 0.01)) {
     printlnlog("[warning] integrator status {}. Integral value {:9.3e} +/- {:9.3e}", status, result, *abserr);
   }
 
@@ -148,7 +147,7 @@ auto integrator(auto params, const double a, const double b, const double epsrel
   // Boost's Gauss-Kronrod integrator
   result = boost::math::quadrature::gauss_kronrod<double, GKNPOINTS>::integrate(
       [&](double x) { return func_integrand(x, &params); }, a, b, 15, epsrel, abserr);
-  assert_testmodeonly((*abserr / std::abs(result)) <= epsrel);
+  assert_testmodeonly((*abserr / std::abs(result)) <= 0.01);
 
 #endif
 #endif
