f78499f1ada9a4205e981b616b99b1b38d969c46
Luke Shingles <luke.shingles@gmail.com>
2026-02-19 00:46:28 +0000

    Update macroatom.h

--- macroatom.h
+++ macroatom.h
@@ -40,12 +40,12 @@ __host__ __device__ void do_macroatom(Packet& pkt, const MacroAtomState& pktmast
 
 // radiative deexcitation rate: paperII 3.5.2
 // multiply by upper level population to get a rate per second
-[[gnu::const]] [[nodiscard]] inline auto rad_deexcitation_ratecoeff(
+[[gnu::const]] [[nodiscard]] constexpr auto rad_deexcitation_ratecoeff(
     const double epsilon_trans, const float A_ul, const double upperstatweight, const double lowerstatweight,
     const double nnlevelupper, const double nnlevellower, const double t_current) -> double {
   const double nu_trans = epsilon_trans / H;
 
-  const double B_ul = CLIGHTSQUAREDOVERTWOH / std::pow(nu_trans, 3) * A_ul;
+  const double B_ul = CLIGHTSQUAREDOVERTWOH / pow3(nu_trans) * A_ul;
   const double B_lu = upperstatweight / lowerstatweight * B_ul;
 
   const double tau_sobolev = ((B_lu * nnlevellower) - (B_ul * nnlevelupper)) * HCLIGHTOVERFOURPI * t_current;
