887c364d34c33c06b7d8d39ddadbdd1be57c4e72
Luke Shingles <luke.shingles@gmail.com>
2026-02-20 09:27:18 +0000

    Update update_packets.cc

--- update_packets.cc
+++ update_packets.cc
@@ -405,14 +405,13 @@ void update_packets(const int nts, std::span<Packet> packets) {
   stats::pkt_action_counters_printout(nts);
 
   const auto time_update_packets_end_thisrank = std::time(nullptr);
-  printlnlog("timestep {}: finished update_packets for rank {} (took {}s)", nts, globals::my_rank,
+  printlnlog("timestep {}: finished update_packets for rank {} (took {} seconds)", nts, globals::my_rank,
              time_update_packets_end_thisrank - time_update_packets_start);
 
   MPI_Barrier(MPI_COMM_WORLD);  // hold all processes once the packets are updated
   const auto time_update_packets_end_allranks = std::time(nullptr);
-  printlnlog("timestep {}: time after update packets for all processes {} (rank {} took {}s, waited {}s, total {}s)",
-             nts, time_update_packets_end_allranks, globals::my_rank,
-             time_update_packets_end_thisrank - time_update_packets_start,
+  printlnlog("timestep {}: time after update packets for all processes (rank {} took {}, waited {}, total {} seconds)",
+             nts, globals::my_rank, time_update_packets_end_thisrank - time_update_packets_start,
              time_update_packets_end_allranks - time_update_packets_end_thisrank,
              time_update_packets_end_allranks - time_update_packets_start);
 }
