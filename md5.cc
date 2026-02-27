6c9e0574e705b1a7f8127485459077ab516e8997
Luke Shingles <luke.shingles@gmail.com>
2026-02-27 15:55:23 +0000

    Update md5.cc

--- md5.cc
+++ md5.cc
@@ -246,7 +246,7 @@ constexpr auto md5_test() -> bool {
   return (bytes_to_hex(hashbytes) == "4b0cff9625b0501c7b9ccc6569113ddf");
 }
 
-#if __cplusplus >= 202302L
+#if (defined(__clang__) && __clang_major__ >= 21) || defined(__GNUC__)
 static_assert(md5_test(), "MD5 test failed!");
 #endif
 
