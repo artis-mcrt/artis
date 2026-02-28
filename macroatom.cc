9b5463908d38f1deee22cbdb1cd434e12bb0facd
Adam McGarrity <67506477+AMG3141@users.noreply.github.com>
2026-02-28 13:09:33 +0000

    Remove unused variable

--- macroatom.cc
+++ macroatom.cc
@@ -647,7 +647,6 @@ void macroatom_close_file() {
       const double sigma_bf = (get_phixs_table(lowerionlower_uniquelevelindex)[0] *
                                get_phixsprobability(lowerionlower_uniquelevelindex, phixstargetindex));
       const double statw_upper = stat_weight(element, upperion, upper);
-      const double modified_sahafact = SAHACONST * statw_lower / statw_upper * std::pow(T_e, -1.5);
 
       // Expanded and simplified from
       // nne * nne * sf * 1.55e13 * std::pow(T_e, -0.5) * g * sigma_bf * std::exp(-fac1) / fac1
