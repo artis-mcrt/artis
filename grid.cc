e35b58a4b7003011a89757cdec644ff30921a88c
Luke Shingles <luke.shingles@gmail.com>
2026-03-24 22:43:00 +0000

    Update grid.cc

--- grid.cc
+++ grid.cc
@@ -212,7 +212,7 @@ auto get_cell_r_inner(const int cellindex, const GridType prop_gridtype) -> doub
 }
 
 [[gnu::pure]] DEVICE_FUNC auto get_propgridtype() -> GridType {
-  if (GRID_TYPE.has_value()) {
+  if constexpr (GRID_TYPE.has_value()) {
     return GRID_TYPE.value();
   }
   return get_modelgridtype();
