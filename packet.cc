921fb5452f134cdf266840533e3cb1ec0287d2d0
Luke Shingles <luke.shingles@gmail.com>
2026-03-24 16:35:51 +0000

    Update packet.cc

--- packet.cc
+++ packet.cc
@@ -48,7 +48,8 @@ void place_pellet(const double e_cmf_per_packet, const std::span<const double> e
   pkt.prop_time = globals::tmin;
   pkt.originated_from_particlenotgamma = false;
 
-  if (grid::get_prop_gridtype() == GridType::SPHERICAL1D) {
+  const auto prop_gridtype = grid::get_prop_gridtype();
+  if (prop_gridtype == GridType::SPHERICAL1D) {
     const double zrand = rng_uniform();
     const double r_inner = grid::get_cellcoordmin(cellindex, 0);
     const double r_outer = grid::get_cellcoordmax(cellindex, 0);
@@ -59,7 +60,7 @@ void place_pellet(const double e_cmf_per_packet, const std::span<const double> e
 
     pkt.pos = vec_scale(get_rand_isotropic_unitvec(), radius);
 
-  } else if (grid::get_prop_gridtype() == GridType::CYLINDRICAL2D) {
+  } else if (prop_gridtype == GridType::CYLINDRICAL2D) {
     const double zrand = rng_uniform_pos();
     const double rcyl_inner = grid::get_cellcoordmin(cellindex, 0);
     const double rcyl_outer = grid::get_cellcoordmax(cellindex, 0);
@@ -69,7 +70,7 @@ void place_pellet(const double e_cmf_per_packet, const std::span<const double> e
     pkt.pos = {std::cos(theta_rand) * rcyl_rand, std::sin(theta_rand) * rcyl_rand,
                grid::get_cellcoordmin(cellindex, 1) + (rng_uniform_pos() * grid::wid_init(cellindex, 1))};
 
-  } else if (grid::get_prop_gridtype() == GridType::CARTESIAN3D) {
+  } else if (prop_gridtype == GridType::CARTESIAN3D) {
     for (int axis = 0; axis < 3; axis++) {
       pkt.pos[axis] = grid::get_cellcoordmin(cellindex, axis) + (rng_uniform_pos() * grid::wid_init(cellindex, axis));
     }
