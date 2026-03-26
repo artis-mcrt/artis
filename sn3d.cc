6ca94a1cf815ef148dff3a6ef2ceec4402c6423f
Luke Shingles <luke.shingles@gmail.com>
2026-03-26 15:06:15 +0000

    Update sn3d.cc

--- sn3d.cc
+++ sn3d.cc
@@ -443,8 +443,7 @@ void write_temp_packetsfile(const int timestep, const int my_rank, std::span<con
       printlnlog("ERROR: Could not open file '{}' for mode 'wb'.", filename);
       write_success = false;
     } else {
-      write_success =
-          (std::fwrite(pkt.data(), sizeof(Packet), pkt.size(), packets_file) == static_cast<size_t>(globals::npkts));
+      write_success = (std::fwrite(pkt.data(), sizeof(Packet), pkt.size(), packets_file) == pkt.size());
       if (!write_success) {
         printlnlog("fwrite() FAILED! will retry...");
       }
