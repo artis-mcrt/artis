8c03d316759c44965aa15c4e163a3a0f85b38553
Luke Shingles <luke.shingles@gmail.com>
2026-03-30 15:49:58 +0100

    Update vpkt.cc

--- vpkt.cc
+++ vpkt.cc
@@ -29,6 +29,9 @@
 namespace vpkt {
 namespace {
 
+static_assert(!VPKT_ON || POL_ON,
+              "POL_ON must be true if VPKT_ON is true because vpkt needs stokes parameters to be tracked");
+
 struct StokesParams {
   double i = 0.;
   double q = 0.;
@@ -187,7 +190,7 @@ auto rlc_emiss_vpkt(const Packet& pkt, const double t_current, const double t_ar
   double pn{1 / (4 * PI)};
   double Q{0.};
   double U{0.};
-  if (type_before_rpkt == TYPE_RPKT && POL_ON) {
+  if (type_before_rpkt == TYPE_RPKT) {
     // Transform Stokes Parameters from the RF to the CMF
     const auto [old_dir_cmf, Qi, Ui] = frame_transform(pkt.dir, vpkt.stokes[1], vpkt.stokes[2], vel_vec);
 
