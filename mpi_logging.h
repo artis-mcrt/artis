e7681884ba0e08421938f5f2bfedc186ea52e647
Luke Shingles <luke.shingles@gmail.com>
2026-04-02 22:37:25 +0100

    Update mpi_logging.h

--- mpi_logging.h
+++ mpi_logging.h
@@ -292,8 +292,9 @@ class MPI_shared_array {
   explicit MPI_shared_array(const ptrdiff_t num_allranks, const T& initval = {}) { allocate(num_allranks, initval); }
 
   // copy constructor is deleted to avoid avoid multiple owners of the same MPI window, but move constructor is allowed
-  MPI_shared_array(const MPI_shared_array&) = delete;
-  MPI_shared_array(MPI_shared_array&& other) noexcept : _win(other._win), _span(other._span) {
+  MPI_shared_array(const MPI_shared_array<std::remove_cv_t<T>>&) = delete;
+  explicit MPI_shared_array(MPI_shared_array<std::remove_cv_t<T>>&& other) noexcept
+      : _win(other._win), _span(other._span) {
     // prevent the other object from freeing the window in its destructor
     other._win = MPI_WIN_NULL;
     other._span = {};
