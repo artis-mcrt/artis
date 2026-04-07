e666835ceb5e57d2219bbf16fe7ecfc1a08ecb70
Luke Shingles <luke.shingles@gmail.com>
2026-04-07 18:12:42 +0100

    clean.sh: Pass -y arg to bypass confirmation

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,19 +2,17 @@
 
 paths="gridsave*.tmp packets*.tmp *.out *.out.* out.txt output_*-*.txt* exspec*.txt* machine.file.* core.* *.slurm packets vspecpol speclc_angle_res bflist.dat logfiles.tar*"
 
-# if [[ "$1" == "-d" ]]; then
-#   echo 1
-# else
-#   echo 1>&2 "ADD -d TO CONFIRM DELETION OF THE FOLLOWING FILES"
-#   ls $paths 2>/dev/null
-# fi
-
 if [ 0 -lt $(ls $paths 2>/dev/null | wc -w) ]; then
   echo "The following ARTIS run files will be deleted:"
   ls -d -- $paths 2>/dev/null
 
-  read -p "Are you sure you want to delete these ARTIS run files? [y/n]" -n 1 -r
-  echo    # (optional) move to a new line
+  if [[ "$1" == "-y" ]]; then
+    REPLY="y"
+  else
+    read -p "Are you sure you want to delete these ARTIS run files? [y/n]" -n 1 -r
+    echo    # new line
+  fi
+
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Deleting:"
     rm -v -rf -- $paths 2>/dev/null
@@ -25,5 +23,13 @@ if [ 0 -lt $(ls $paths 2>/dev/null | wc -w) ]; then
   fi
 else
   echo "No ARTIS run files to delete"
-  cp -i -v input-newrun.txt input.txt
+  if [[ -f "input-newrun.txt" ]] && ! cmp -s input-newrun.txt input.txt; then
+    if [[ "$1" == "-y" ]]; then
+      cp -v input-newrun.txt input.txt
+    else
+      cp -i -v input-newrun.txt input.txt
+    fi
+  else
+    echo "input-newrun.txt is the same as input.txt"
+  fi
 fi
