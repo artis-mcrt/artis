2d2a07e8d71e6ad61e75e00bd56cda337fe66b01
Luke Shingles <luke.shingles@gmail.com>
2026-04-25 16:50:31 +0100

    Update exspec-zip-virgo-submit.sh

--- scripts/exspec-zip-virgo-submit.sh
+++ scripts/exspec-zip-virgo-submit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -x
-sbatch -J exspec_$(basename $(pwd)) --ntasks=1 --cpus-per-task 48 --mem-per-cpu=4000MB --partition=long --time=48:00:00 --constraint="[9654,9555]" --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/exspec-zip-virgo-slurmjob.sh
+sbatch -J exspec_$(basename $(pwd)) --ntasks=1 --cpus-per-task 64 --mem-per-cpu=4000MB --partition=long --time=48:00:00 --constraint="[9654|9555]" --mail-type=ALL --mail-user=${USER}@gsi.de -- artis/scripts/exspec-zip-virgo-slurmjob.sh
