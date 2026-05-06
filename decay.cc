6e95549e76cdbedb34f9b40bc65466e7de293ea7
Luke Shingles <luke.shingles@gmail.com>
2026-05-06 13:49:28 +0100

    Update decay.cc

--- decay.cc
+++ decay.cc
@@ -679,9 +679,9 @@ auto get_endecay_per_ejectamass_between_times(const int mgi, const int decaypath
 auto write_nuclides_list() {
   auto nuclides_file = std::fstream("nuclides.out", std::ofstream::out | std::ofstream::trunc);
   assert_always(nuclides_file.is_open());
-  nuclides_file << "#nucindex Z A\n";
+  std::println("#nucindex Z A");
   for (int nucindex = 0; nucindex < std::ssize(nuclides); nucindex++) {
-    nuclides_file << nucindex << ' ' << get_nuc_z(nucindex) << ' ' << get_nuc_a(nucindex) << '\n';
+    std::println(nuclides_file, "{} {} {}", nucindex, get_nuc_z(nucindex), get_nuc_a(nucindex));
   }
 }
 
