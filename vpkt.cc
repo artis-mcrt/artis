94cf9cafd2b3445b042b313fbd9a849bc3ba76ea
Luke Shingles <luke.shingles@gmail.com>
2026-05-09 11:34:48 +0100

    Update vpkt.cc

--- vpkt.cc
+++ vpkt.cc
@@ -954,10 +954,8 @@ auto trace_vpkts(const Packet& pkt, const enum packet_type type_before_rpkt) -> 
     }
   }
   if (VPKT_WRITE_CONTRIBS && any_dir_escaped) {
-    std::print(vpkt_contrib_file, "{} {} {} {:g}", pkt.emissiontype, pkt.trueemissiontype, pkt.absorptiontype,
-               pkt.absorptionfreq);
-    std::println(vpkt_contrib_file, "{}", vpkt_contrib_row);
-    vpkt_contrib_file.flush();
+    std::println(vpkt_contrib_file, "{} {} {} {:g}{}", pkt.emissiontype, pkt.trueemissiontype, pkt.absorptiontype,
+                 pkt.absorptionfreq, vpkt_contrib_row);
   }
 }
 }  // namespace vpkt
