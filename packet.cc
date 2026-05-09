131e836d65c68bccc69f2869b2fa5abeac6d01e8
Luke Shingles <luke.shingles@gmail.com>
2026-05-09 12:33:10 +0100

    Update packet.cc

--- packet.cc
+++ packet.cc
@@ -221,14 +221,14 @@ void write_text_packets(const std::string& filename, const std::span<const Packe
     if (!KEEP_ESCAPED_GAMMAS && pkt.type == TYPE_ESCAPE && pkt.escape_type == TYPE_GAMMA) {
       continue;
     }
-    std::print(packets_file, "{} {} {} ", pkt.number, pkt.cellindex, std::to_underlying(pkt.type));
-    std::print(packets_file, "{:g} {:g} {:g} ", pkt.pos[0], pkt.pos[1], pkt.pos[2]);
-    std::print(packets_file, "{:g} {:g} {:g} ", pkt.dir[0], pkt.dir[1], pkt.dir[2]);
-    std::print(packets_file, "{:g} ", pkt.tdecay);
-    std::print(packets_file, "{:g} {:g} {:g} {:g} ", pkt.e_cmf, pkt.e_rf, pkt.nu_cmf, pkt.nu_rf);
-    std::print(packets_file, "{} {:g} ", std::to_underlying(pkt.escape_type), pkt.escape_time);
-    std::print(packets_file, "{} {} ", pkt.emissiontype, pkt.trueemissiontype);
-    std::print(packets_file, "{:g} {:g} {:g}", pkt.em_pos[0], pkt.em_pos[1], pkt.em_pos[2]);
+    std::print(packets_file, "{} {} {}", pkt.number, pkt.cellindex, std::to_underlying(pkt.type));
+    std::print(packets_file, " {:g} {:g} {:g}", pkt.pos[0], pkt.pos[1], pkt.pos[2]);
+    std::print(packets_file, " {:g} {:g} {:g}", pkt.dir[0], pkt.dir[1], pkt.dir[2]);
+    std::print(packets_file, " {:g} ", pkt.tdecay);
+    std::print(packets_file, " {:g} {:g} {:g} {:g}", pkt.e_cmf, pkt.e_rf, pkt.nu_cmf, pkt.nu_rf);
+    std::print(packets_file, " {} {:g}", std::to_underlying(pkt.escape_type), pkt.escape_time);
+    std::print(packets_file, " {} {}", pkt.emissiontype, pkt.trueemissiontype);
+    std::print(packets_file, " {:g} {:g} {:g}", pkt.em_pos[0], pkt.em_pos[1], pkt.em_pos[2]);
     std::print(packets_file, " {} {:g} {} {:g}", pkt.absorptiontype, pkt.absorptionfreq, pkt.nscatterings, pkt.em_time);
     if constexpr (POL_ON) {
       std::print(packets_file, " {:g} {:g} {:g}", pkt.stokes[0], pkt.stokes[1], pkt.stokes[2]);
