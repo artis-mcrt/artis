4b34b8d6d22d9132beee59ae7a9bd20bf065bc32
Luke Shingles <luke.shingles@gmail.com>
2026-05-09 12:29:42 +0100

    Update sn3d.h

--- sn3d.h
+++ sn3d.h
@@ -55,8 +55,8 @@ inline void check_already_running() {
     }
 
     auto pidfile = std::fstream("artis.pid", std::ofstream::out | std::ofstream::trunc);
-    pidfile << artispid << '\n';
-    pidfile << std::filesystem::current_path().generic_string() << '\n';
+    std::println(pidfile, "{}", artispid);
+    std::println(pidfile, "{}", std::filesystem::current_path().generic_string());
   }
 
   // make sure rank 0 checked for a pid file before we proceed
