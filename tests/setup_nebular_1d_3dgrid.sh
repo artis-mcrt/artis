12bc82f2f03a3464e67e91859eb54796726c1db4
Luke Shingles <luke.shingles@gmail.com>
2026-05-16 21:36:46 +0100

    Update setup_nebular_1d_3dgrid.sh

--- tests/setup_nebular_1d_3dgrid.sh
+++ tests/setup_nebular_1d_3dgrid.sh
@@ -6,7 +6,6 @@ runfolder=nebular_1d_3dgrid_testrun
 
 if [ ! -f atomicdata_feconi.tar.xz ]; then curl -O -L https://github.com/artis-mcrt/artis/releases/download/v2026.5.15/atomicdata_feconi.tar.xz; fi
 
-
 mkdir -p $runfolder
 
 cd $runfolder
