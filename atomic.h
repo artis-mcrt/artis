819c2b51c9c62d18eef3997b1fc3c1ce9a4966bb
Luke Shingles <luke.shingles@gmail.com>
2026-05-18 12:42:00 +0100

    Update atomic.h

--- atomic.h
+++ atomic.h
@@ -233,7 +233,7 @@ DEVICE_FUNC inline auto get_nphixstargets(const int element, const int ion, cons
     const double sigma_bf_a = photoion_xs[i];
     const double sigma_bf_b = photoion_xs[i + 1];
     const double factor_b = ireal - i;
-    sigma_bf = static_cast<float>(((1. - factor_b) * sigma_bf_a) + (factor_b * sigma_bf_b));
+    sigma_bf = static_cast<float>(std::lerp(sigma_bf_a, sigma_bf_b, factor_b));
   } else {
     // use a parameterization of sigma_bf by the Kramers formula
     // which anchor point should we take ??? the cross-section at the edge or at the highest grid point ???
