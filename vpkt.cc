de7af41fccfd5069a844c18287dc939f40ab5180
Luke Shingles <luke.shingles@gmail.com>
2026-05-20 15:35:47 +0100

    vpkt: use scatter_polarisation_frame_transform()

--- vpkt.cc
+++ vpkt.cc
@@ -204,46 +204,7 @@ auto trace_vpkt_direction(const Packet& rpkt, const double t_arrive, const doubl
     // Need to rotate Stokes Parameters in the scattering plane
 
     const auto obs_cmf = angle_ab(obsdir, vel_vec);
-
-    const auto [ref1_old, ref2_old] = meridian(old_dir_cmf);
-
-    // This is the i1 angle of Bulla+2015, obtained by computing the angle between the
-    // reference axes ref1 and ref2 in the meridian frame and the corresponding axes
-    // ref1_sc and ref2_sc in the scattering plane.
-    const double i1 = get_rot_angle(old_dir_cmf, obs_cmf, ref1_old, ref2_old);
-    const double cos2i1 = cos(2 * i1);
-    const double sin2i1 = sin(2 * i1);
-
-    const double Qold = (Qi * cos2i1) - (Ui * sin2i1);
-    const double Uold = (Qi * sin2i1) + (Ui * cos2i1);
-
-    // Scattering
-
-    const double mu = dot(old_dir_cmf, obs_cmf);
-
-    pn = 3. / (16. * PI) * (1 + pow2(mu) + ((pow2(mu) - 1) * Qold));
-
-    const double Inew = 0.75 * (((mu * mu) + 1.0) + (Qold * ((mu * mu) - 1.0)));
-    const double Qnew = (0.75 * (((mu * mu) - 1.0) + (Qold * ((mu * mu) + 1.0)))) / Inew;
-    const double Unew = (1.5 * mu * Uold) / Inew;
-
-    // Need to rotate Stokes Parameters out of the scattering plane to the meridian frame
-
-    const auto [ref1, ref2] = meridian(obs_cmf);
-
-    // This is the i2 angle of Bulla+2015, obtained from the angle THETA between the
-    // reference axes ref1_sc and ref2_sc in the scattering plane and ref1 and ref2 in the
-    // meridian frame. NB: we need to add PI to transform THETA to i2
-    const double i2 = PI + get_rot_angle(obs_cmf, old_dir_cmf, ref1, ref2);
-    const double cos2i2 = cos(2 * i2);
-    const double sin2i2 = sin(2 * i2);
-
-    const double Q_cmf = (Qnew * cos2i2) + (Unew * sin2i2);
-    const double U_cmf = (-Qnew * sin2i2) + (Unew * cos2i2);
-
-    // Transform Stokes Parameters from the CMF to the RF
-
-    std::tie(std::ignore, Q, U) = frame_transform(obs_cmf, Q_cmf, U_cmf, Vec3d{-vel_vec[0], -vel_vec[1], -vel_vec[2]});
+    std::tie(std::ignore, Q, U, pn) = scatter_polarisation_frame_transform(old_dir_cmf, obs_cmf, Qi, Ui, vel_vec);
 
   } else {
     assert_testmodeonly(type_before_rpkt == TYPE_KPKT || type_before_rpkt == TYPE_MA);
