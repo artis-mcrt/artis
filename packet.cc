29de9911411be766ea0dd8f3ae51b4cb09697956
Luke Shingles <luke.shingles@gmail.com>
2026-05-20 21:16:18 +0100

    Update packet.cc

--- packet.cc
+++ packet.cc
@@ -231,7 +231,7 @@ void write_text_packets(const std::string& filename, const std::span<const Packe
     std::print(packets_file, " {:g} {:g} {:g}", pkt.em_pos[0], pkt.em_pos[1], pkt.em_pos[2]);
     std::print(packets_file, " {} {:g} {} {:g}", pkt.absorptiontype, pkt.absorptionfreq, pkt.nscatterings, pkt.em_time);
     if constexpr (POL_ON) {
-      std::print(packets_file, " {:g} {:g} {:g}", pkt.stokes[0], pkt.stokes[1], pkt.stokes[2]);
+      std::print(packets_file, " {:g} {:g} {:g}", 1., pkt.stokes[1], pkt.stokes[2]);
     }
     std::print(packets_file, " {}", static_cast<int>(pkt.originated_from_particlenotgamma));
     std::print(packets_file, " {:g} {:g} {:g}", pkt.trueem_pos[0], pkt.trueem_pos[1], pkt.trueem_pos[2]);
