d670d058488328ac12146f46cec6f3d284c9c331
Luke Shingles <luke.shingles@gmail.com>
2026-05-21 09:05:11 +0100

    Remove redundant stoke_i = 1. from packet format

--- packet.cc
+++ packet.cc
@@ -40,7 +40,7 @@ constexpr auto get_packets_text_header() -> std::string {
       " em_posx em_posy em_posz"
       " absorption_type absorption_freq nscatterings em_time";
   if constexpr (POL_ON) {
-    header += " stokes1 stokes2 stokes3";
+    header += " stokes_q stokes_u";
   }
   header +=
       " originated_from_particlenotgamma"
@@ -193,8 +193,7 @@ auto read_text_packets(const std::string& filename) -> std::vector<Packet> {
     ssline >> pkt.em_time;
 
     if constexpr (POL_ON) {
-      double stokes1 = 0.;
-      ssline >> stokes1 >> pkt.stokes_q >> pkt.stokes_u;
+      ssline >> pkt.stokes_q >> pkt.stokes_u;
     }
 
     int int_originated_from_particlenotgamma = 0;
@@ -232,7 +231,7 @@ void write_text_packets(const std::string& filename, const std::span<const Packe
     std::print(packets_file, " {:g} {:g} {:g}", pkt.em_pos[0], pkt.em_pos[1], pkt.em_pos[2]);
     std::print(packets_file, " {} {:g} {} {:g}", pkt.absorptiontype, pkt.absorptionfreq, pkt.nscatterings, pkt.em_time);
     if constexpr (POL_ON) {
-      std::print(packets_file, " {:g} {:g} {:g}", 1., pkt.stokes_q, pkt.stokes_u);
+      std::print(packets_file, " {:g} {:g}", pkt.stokes_q, pkt.stokes_u);
     }
     std::print(packets_file, " {}", static_cast<int>(pkt.originated_from_particlenotgamma));
     std::print(packets_file, " {:g} {:g} {:g}", pkt.trueem_pos[0], pkt.trueem_pos[1], pkt.trueem_pos[2]);
