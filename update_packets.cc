a715ca435f3fc73f9cf1022dfeff4b43e23d26a4
Luke Shingles <luke.shingles@gmail.com>
2026-06-26 13:02:17 +0100

    Update update_packets.cc

--- update_packets.cc
+++ update_packets.cc
@@ -499,7 +499,7 @@ void update_packets(const int nts, std::span<Packet> packets) {
     for (int nonemptymgi = 0; nonemptymgi < nonempty_npts_model; nonemptymgi++) {
       cellcache_change_cell(globals::cellcache[nonemptymgi], nonemptymgi);
     }
-    printlnlog("timestep {}: all {} cell caches set (took {} s)", nts, nonempty_npts_model,
+    printlnlog("timestep {}: all {} cell caches set (took {:.1f} s)", nts, nonempty_npts_model,
                std::chrono::duration<double>(std::chrono::steady_clock::now() - time_update_packets_start).count());
   }
   int prevpkt_cellcache_nonemptymgi = -2;
