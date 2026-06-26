e8e4e418f7340f0bf092b62508df8f18e95f477c
Luke Shingles <luke.shingles@gmail.com>
2026-06-26 16:46:35 +0100

    Update update_packets.cc

--- update_packets.cc
+++ update_packets.cc
@@ -413,7 +413,7 @@ void cellcacheslot_populate(globals::CellCache& cacheslot, const int nonemptymgi
   // prepopulate every cell's rates here, up front, while still on the host.
   const double t_mid = globals::timesteps[globals::timestep].mid;
   const auto alllevelindices = std::ranges::iota_view{0, get_includedlevels()};
-  std::for_each(EXEC_PAR alllevelindices.begin(), alllevelindices.end(),
+  std::for_each(EXEC_PAR_UNSEQ alllevelindices.begin(), alllevelindices.end(),
                 [nonemptymgi, t_mid](const int uniquelevelindex) {
                   calculate_cellcache_macroatom_transitionrates(nonemptymgi, uniquelevelindex, t_mid);
                 });
