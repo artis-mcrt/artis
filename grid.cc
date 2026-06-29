8e1af9e9461ed9fba231fa99dd6b3444125907e5
Luke Shingles <luke.shingles@gmail.com>
2026-06-29 14:10:22 +0100

    Revert "Update grid.cc"
    This reverts commit 887a3b6eaca01b7af9018fed2ba13492b1885831.

--- grid.cc
+++ grid.cc
@@ -1480,8 +1480,8 @@ template <BoundaryType boundarytype>
     case GridType::CARTESIAN3D: {
       Vec3d pos;
       for (int axis = 0; axis < 3; axis++) {
-        pos[axis] = std::lerp(get_cellcoordmin(cellindex, axis), get_cellcoordmax(cellindex, axis),
-                              rng_uniform_pos(packetnumber));
+        pos[axis] =
+            get_cellcoordmin(cellindex, axis) + (rng_uniform_pos(packetnumber) * propcell_width_tmin(cellindex, axis));
       }
       return pos;
     }
