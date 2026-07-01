3175e29da88e7cff02b30f5653b79b37c2e0d858
Luke Shingles <luke.shingles@gmail.com>
2026-07-01 13:51:36 +0100

    Update packet.h

--- packet.h
+++ packet.h
@@ -78,7 +78,7 @@ struct Packet {
   // per-packet RNG state so that GPU threads (which can't use thread_local)
   // don't share and race on a single global generator
   utlrandom::Xoshiro128PP rng;
-  auto rngstate() -> utlrandom::Xoshiro128PP& { return rng; }
+  auto rngstate() -> utlrandom::Xoshiro128PP& { return rng; }  // cppcheck-suppress functionStatic
 #else
   // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
   auto rngstate() -> std::mt19937& {
