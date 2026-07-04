1cc9b2acf7c5ef7f6b66a0def039aac84d91f030
Luke Shingles <luke.shingles@gmail.com>
2026-07-04 16:55:39 +0100

    Revert "Update mpi_logging.h"
    This reverts commit 0eb5c52a74f89db81e6438e6cdfcb270f7f4c097.

--- mpi_logging.h
+++ mpi_logging.h
@@ -533,7 +533,22 @@ inline void MPI_Reduce_safe(R&& data, MPI_Op op, const int root, MPI_Comm comm) 
   std::abort();
 }
 
-class alignas(std::hardware_destructive_interference_size) PaddedMutex {
+namespace scoped_mutex_detail {
+#if defined(__cpp_lib_hardware_interference_size) && __cpp_lib_hardware_interference_size >= 201603
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Winterference-size"
+#endif
+inline constexpr std::size_t hardware_destructive_interference_size = std::hardware_destructive_interference_size;
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic pop
+#endif
+#else
+inline constexpr std::size_t hardware_destructive_interference_size = 64;
+#endif
+}  // namespace scoped_mutex_detail
+
+class alignas(scoped_mutex_detail::hardware_destructive_interference_size) PaddedMutex {
  private:
   int lock_{0};
 
@@ -549,10 +564,10 @@ class alignas(std::hardware_destructive_interference_size) PaddedMutex {
   }
 };
 
-static_assert(alignof(PaddedMutex) >= std::hardware_destructive_interference_size);
-static_assert(alignof(PaddedMutex) % std::hardware_destructive_interference_size == 0);
-static_assert(sizeof(PaddedMutex) >= std::hardware_destructive_interference_size);
-static_assert(sizeof(PaddedMutex) % std::hardware_destructive_interference_size == 0);
+static_assert(alignof(PaddedMutex) >= scoped_mutex_detail::hardware_destructive_interference_size);
+static_assert(alignof(PaddedMutex) % scoped_mutex_detail::hardware_destructive_interference_size == 0);
+static_assert(sizeof(PaddedMutex) >= scoped_mutex_detail::hardware_destructive_interference_size);
+static_assert(sizeof(PaddedMutex) % scoped_mutex_detail::hardware_destructive_interference_size == 0);
 
 class ScopedMutex {
  private:
