beddf6ef02ea03b8c18a53bd5a49880d17221afa
Luke Shingles <luke.shingles@gmail.com>
2026-07-12 21:07:27 +0100

    Update thermalbalance.cc

--- thermalbalance.cc
+++ thermalbalance.cc
@@ -153,12 +153,10 @@ auto T_e_eqn_heating_minus_cooling(const double T_e, int nonemptymgi, const doub
 
   const auto ntlepton_frac_heating = nonthermal::get_nt_frac_heating(nonemptymgi);
   const auto ntlepton_dep = nonthermal::get_deposition_rate_density(nonemptymgi);
-  const auto ntalpha_frac_heating = 1.;
   const auto ntalpha_dep = heatingcoolingrates.dep_alpha;
   // spontaneous fission fragments deposit as pure heating via k-packets (like alpha particles)
   const auto ntspfission_dep = heatingcoolingrates.dep_spfission;
-  heatingcoolingrates.heating_dep =
-      (ntlepton_dep * ntlepton_frac_heating) + (ntalpha_dep * ntalpha_frac_heating) + ntspfission_dep;
+  heatingcoolingrates.heating_dep = (ntlepton_dep * ntlepton_frac_heating) + ntalpha_dep + ntspfission_dep;
   heatingcoolingrates.dep_frac_heating =
       ((ntalpha_dep + ntspfission_dep) > 0)
           ? heatingcoolingrates.heating_dep / (ntlepton_dep + ntalpha_dep + ntspfission_dep)
