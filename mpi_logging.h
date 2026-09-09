4e5e55f1948c9d38bca9ab3ee0a353955d5b9710
Luke Shingles <luke.shingles@gmail.com>
2026-09-09 13:29:25 +0100

    Update mpi_logging.h

--- mpi_logging.h
+++ mpi_logging.h
@@ -291,6 +291,7 @@ template <typename T>
   assert_always(MPI_Info_create(&info) == MPI_SUCCESS);
   // Request alignment (AVX-512 requires 64b, and 128b is Apple Silicon cache line size).
   assert_always(MPI_Info_set(info, "mpi_minimum_memory_alignment", "128") == MPI_SUCCESS);
+  assert_always(MPI_Info_set(info, "alloc_shared_noncontig", "true") == MPI_SUCCESS);
   assert_always(MPI_Win_allocate_shared(size, disp_unit, info, globals::mpi_comm_node, static_cast<void*>(&ptr),
                                         &mpiwin) == MPI_SUCCESS);
   assert_always(MPI_Info_free(&info) == MPI_SUCCESS);
